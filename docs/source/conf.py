781cedf1752c4a995a1fbd19298c5ff37ba6f4d7
philippmwirth <65946090+philippmwirth@users.noreply.github.com>
2020-10-13 15:46:07 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,8 @@ copyright = '2020, Lightly AG'
 author = 'Philipp Wirth, Igor Susmelj'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
+master_doc = 'index'
 
 
 # -- General configuration ---------------------------------------------------
