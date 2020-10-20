c01e573d1c9ab9edb6a5dfda675d9e1dfaff5acc
philippmwirth <philipp.m.wirth@gmail.com>
2020-10-20 12:22:33 +0000

    Add MoCo

---
+++ lightly/models/moco.py
@@ -0,0 +1,128 @@
+""" ResNet architecture with projection head and momentum encoder. """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+import torch
+import torch.nn as nn
+from lightly.models.resnet import ResNetGenerator
+
+
+class ResNetMoCo(nn.Module):
+    """ Implementation of a momentum encoder with a ResNet backbone.
+
+    Attributes:
+        name:
+            ResNet version, choose from resnet-{9, 18, 34, 50, 101, 152}.
+        width:
+            Width of the ResNet.
+        num_ftrs:
+            Dimension of the embedding (before the projection head).
+        out_dim:
+            Dimension of the output (after the projection head).
+        m:
+            Momentum for momentum update of the key-encoder.
+
+    """
+
+    def __init__(self,
+                 name: str ='resnet-18',
+                 width: float = 1.,
+                 num_ftrs: int = 16,
+                 out_dim: int = 128,
+                 m: float = 0.999):
+
+        self.num_ftrs = num_ftrs
+        self.out_dim = out_dim
+        self.m = m
+
+        super(ResNetMoCo, self).__init__()
+        resnet = ResNetGenerator(name=name, width=width)
+        last_conv_channels = list(resnet.children())[-1].in_features
+
+        self.features = nn.Sequential(
+            nn.BatchNorm2d(3),
+            *list(resnet.children())[:-1],
+            nn.Conv2d(last_conv_channels, num_ftrs, 1),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        self.projection_head = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.ReLU(),
+            nn.Linear(num_ftrs, out_dim)
+        )
+
+        self.key_features = nn.Sequential(
+            nn.BatchNorm2d(3),
+            *list(resnet.children())[:-1],
+            nn.Conv2d(last_conv_channels, num_ftrs, 1),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        self.key_projection_head = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.ReLU(),
+            nn.Linear(num_ftrs, out_dim)
+        )
+
+        # set key-encoder weights to query-encoder weights
+        self._momentum_update_key_encoder(0.)
+
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self, m=0.):
+        """Momentum update of the key-encoder
+
+        Args:
+            m:
+                Floating point in [0, 1] which determines update rate.
+
+        """
+        # zip feature weights
+        feature_weight_generator = zip(
+            self.features.parameters(),
+            self.key_features.parameters())
+        # zip projection head weights
+        head_weight_generator = zip(
+            self.projection_head.parameters(),
+            self.key_projection_head.parameters())
+        # update weights
+        for param_q, param_k in feature_weight_generator:
+            param_k.data = param_k.data * m + param_q.data * (1. - m)
+        for param_q, param_k in head_weight_generator:
+            param_k.data = param_k.data * m + param_q.data * (1. - m)
+
+
+    def forward(self, x: torch.Tensor):
+        """Forward pass through ResNetMoCo
+
+        Splits the input batch into q and k following the notation of MoCo.
+        Extracts features with the ResNet backbone and applies the projection
+        head to the output space.
+
+        Args:
+            x:
+                Tensor of shape bsz x channels x W x H
+
+        Returns:
+            Tensor of shape bsz x out_dim
+
+        """
+        self._momentum_update_key_encoder(self.m)
+
+        # adopting the notation of the moco paper
+        batch_size = x.shape[0] // 2
+        q = x[:batch_size]
+        k = x[batch_size:]
+        
+        # embed queries
+        emb_q = self.features(q).squeeze()
+        out_q = self.projection_head(emb_q)
+
+        # embed keys
+        with torch.no_grad():
+            emb_k = self.key_features(k).squeeze()
+            out_k = self.key_projection_head(emb_k)
+
+        return torch.cat([out_q, out_k], axis=0)
