7cc3f3c0260aa406d9da75a8903ea2f8faca53ce
philippmwirth <philipp.m.wirth@gmail.com>
2020-10-22 07:13:44 +0000

    Move helpers to uploads and downloads

--- lightly/api/__init__.py
+++ lightly/api/__init__.py
@@ -3,9 +3,8 @@
 # Copyright (c) 2020. Lightly AG and its affiliates.
 # All Rights Reserved
 
-from lightly.api.communication import get_presigned_upload_url
-from lightly.api.communication import get_latest_version
-from lightly.api.helpers import upload_images_from_folder
-from lightly.api.helpers import upload_file_with_signed_url
-from lightly.api.helpers import upload_embeddings_from_csv
-from lightly.api.helpers import get_samples_by_tag
+from lightly.api.routes.pip import get_version              # noqa: F401
+from lightly.api.upload import upload_images_from_folder    # noqa: F401
+from lightly.api.upload import upload_embeddings_from_csv   # noqa: F401
+from lightly.api.upload import upload_file_with_signed_url  # noqa: F401
+from lightly.api.download import get_samples_by_tag         # noqa: F401
