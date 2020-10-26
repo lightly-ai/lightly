b8591666382863dd28b2189c989cc46122fc68f7
IgorSusmelj <igor_7779@hotmail.com>
2020-10-26 15:14:38 +0000

    Update min python version to 3.6. Add pipy classifiers

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 
     long_description = load_description()
 
-    python_requires = '>=3.7'
+    python_requires = '>=3.6'
     install_requires = load_requirements()
     dev_requires = load_requirements(filename='dev.txt')
 
@@ -89,7 +89,21 @@ if __name__ == '__main__':
     }
 
     classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License"
     ]
 
