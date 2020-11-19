e78e1f79eaa07d03646072b9fe1e67b43cf7890d
philippmwirth <philipp.m.wirth@gmail.com>
2020-11-19 11:08:30 +0000

    Move checkpoint loading to models

--- lightly/models/moco.py
+++ lightly/models/moco.py
@@ -6,6 +6,7 @@
 import torch
 import torch.nn as nn
 from lightly.models.resnet import ResNetGenerator
+from lightly.models._helpers import filter_state_dict
 
 
 def _get_features_and_projections(resnet, num_ftrs, out_dim):
@@ -83,6 +84,56 @@ class ResNetMoCo(nn.Module):
             param_k.requires_grad = False
         self._momentum_update_key_encoder(0.)
 
+    @classmethod
+    def from_state_dict(cls,
+                        state_dict: dict,
+                        name: str = 'resnet-18',
+                        width: float = 1.,
+                        num_ftrs: int = 32,
+                        out_dim: int = 128,
+                        m: float = 0.999,
+                        strict: bool = True,
+                        apply_filter: bool = True):
+        """Initializes a ResNetMoCo and loads weights from a checkpoint.
+
+        Args:
+            state_dict:
+                State dictionary with layer weights.
+            name:
+                ResNet version, choose from resnet-{9, 18, 34, 50, 101, 152}.
+            width:
+                Width of the ResNet.
+            num_ftrs:
+                Dimension of the embedding (before the projection head).
+            out_dim:
+                Dimension of the output (after the projection head).
+            m:
+                Momentum for momentum update of the key-encoder.
+            strict:
+                Set to False when loading from a partial state_dict.
+            apply_filter:
+                If True, removes the `model.` prefix from keys in the state_dict.
+
+        """
+        model = cls(
+            name=name,
+            width=width,
+            num_ftrs=num_ftrs,
+            out_dim=out_dim,
+            m=m,
+        )
+
+        # remove the model. prefix which is caused by the pytorch-lightning
+        # checkpoint saver and load the model from the "filtered" state dict
+        # this approach is compatible with pytorch_lightning 0.7.1 - 0.8.4 (latest)
+        if apply_filter:
+            state_dict_ = filter_state_dict(state_dict)
+        else:
+            state_dict_ = state_dict
+
+        model.load_state_dict(state_dict_, strict=strict)
+
+        return model
 
     @torch.no_grad()
     def _momentum_update_key_encoder(self, m=0.):
