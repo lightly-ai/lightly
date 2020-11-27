3fc562c145ba0555dfb7479d0850407b95689aea
IgorSusmelj <igor_7779@hotmail.com>
2020-11-27 09:57:29 +0000

    Minor changes. Removed unused images.

--- docs/source/tutorials_source/package/tutorial_moco_memory_bank.py
+++ docs/source/tutorials_source/package/tutorial_moco_memory_bank.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 
+.. _lightly-moco-tutorial-2:
+
 Tutorial 2: Train MoCo on CIFAR-10
 ==============================================
 
@@ -16,6 +18,17 @@ Some of them use a memory bank to store old negative examples we can query
 to compensate for the smaller batch size. MoCo takes this approach
 one step further by including a momentum encoder.
 
+We use the **CIFAR-10** dataset for this tutorial.
+
+In this tutorial you will learn:
+
+- How to use lightly to load a dataset and train a model
+
+- How to create a MoCo model with a memory bank
+
+- How to use the pre-trained model after self-supervised learning for a 
+  transfer learning task
+
 """
 
 # %%
@@ -23,7 +36,7 @@ one step further by including a momentum encoder.
 # -------
 #
 # Import the Python frameworks we need for this tutorial.
-# You can install all of them using pip.
+# Make sure you have lightly installed.
 #
 # .. code-block:: console
 #
@@ -46,7 +59,7 @@ num_workers = 8
 batch_size = 256
 memory_bank_size = 4096
 seed = 1
-max_epochs = 1 #100
+max_epochs = 50
 
 # %%
 # Replace the path with the location of your CIFAR-10 dataset.
@@ -83,7 +96,7 @@ pl.seed_everything(seed)
 # ------------------------------------
 #
 # We start with our data preprocessing pipeline. We can implement augmentations
-# from the MOCO paper using the following collate function provided by lightly.
+# from the MOCO paper using the collate function provided by lightly.
 # We can overwrite any default parameter such as in this example the input_size.
 # Images from the CIFAR-10 dataset have a resolution of 32x32 pixels. Let's use
 # this resolution to train our model. 
@@ -99,9 +112,22 @@ collate_fn = lightly.data.MoCoCollateFunction(
 
 # %%
 # We don't want any augmentation for our test data. Therefore,
-# we create custom torchvision based data transformations.
+# we create custom, torchvision based data transformations.
 # Let's ensure the size is correct and we normalize the data in
 # the same way as we do with the training data.
+
+# Augmentations typically used to train on cifar-10
+train_classifier_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.RandomCrop(32, padding=4),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+# No additional augmentations for the test set
 test_transforms = torchvision.transforms.Compose([
     torchvision.transforms.Resize((32, 32)),
     torchvision.transforms.ToTensor(),
@@ -111,13 +137,19 @@ test_transforms = torchvision.transforms.Compose([
     )
 ])
 
+# We use the moco augmentations for training moco
 dataset_train_moco = lightly.data.LightlyDataset(
     from_folder=path_to_train
 )
 
+# Since we also train a linear classifier on the pre-trained moco model we
+# reuse the test augmentations here (MoCo augmentations are very strong and 
+# usually reduce accuracy of models which are not used for contrastive learning.
+# Our linear layer will be trained using cross entropy loss and labels provided
+# by the dataset. Therefore we chose light augmentations.)
 dataset_train_classifier = lightly.data.LightlyDataset(
     from_folder=path_to_train,
-    transform=test_transforms
+    transform=train_classifier_transforms
 )
 
 dataset_test = lightly.data.LightlyDataset(
@@ -146,7 +178,6 @@ dataloader_train_classifier = torch.utils.data.DataLoader(
     num_workers=num_workers
 )
 
-
 dataloader_test = torch.utils.data.DataLoader(
     dataset_test,
     batch_size=batch_size,
@@ -158,8 +189,11 @@ dataloader_test = torch.utils.data.DataLoader(
 # %%
 # Create the MoCo Lightning Module
 # ---------------------------
-# Now we create our model. We use PyTorch Lightning to train
+# Now we create our MoCo model. We use PyTorch Lightning to train
 # our model. We follow the specification of the lightning module.
+# In this example we set the number of feature for the hidden dimension to 128.
+# The momentum for the Momentum Encoder is set to 0.99 (default is 0.999) since
+# other reports show that this works better for Cifar-10.
 
 class MocoModel(pl.LightningModule):
     def __init__(self):
@@ -268,14 +302,23 @@ class Classifier(pl.LightningModule):
 gpus = 1 if torch.cuda.is_available() else 0
 
 model = MocoModel()
-trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus)
-trainer.fit(model, dataloader_train_moco)
+trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus,
+                     progress_bar_refresh_rate=100)
+trainer.fit(
+    model,
+    dataloader_train_moco
+)
 
 # %%
 # Train the Classifier
 classifier = Classifier(model.resnet_moco)
-trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus)
-trainer.fit(classifier, dataloader_train_classifier, dataloader_test)
+trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus,
+                     progress_bar_refresh_rate=100)
+trainer.fit(
+    classifier,
+    dataloader_train_classifier,
+    dataloader_test
+)
 
 # %%
 # Checkout the tensorboard logs while the model is training.
@@ -292,12 +335,3 @@ trainer.fit(classifier, dataloader_train_classifier, dataloader_test)
 #   6006 from your remote machine to your local machine:
 #
 #   `ssh username:host -N -L localhost:6006:localhost:6006`
-
-# %%
-# The plots in tensorboard should look similar to the ones here:
-#
-# .. image:: images/moco_memory_bank/training_loss.png
-#   :alt: Training loss for MoCo with different memory banks
-#
-# .. image:: images/moco_memory_bank/validation_accuracy.png
-#   :alt: Validation accuracy for MoCo with different memory banks
