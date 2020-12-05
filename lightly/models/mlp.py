c561d29692f2d74e71422781a630b1eb1bc66ad4
Lauren Moos <lauren.a.moos@gmail.com>
2020-12-05 13:04:01 +0100

    byol loss & MLP implementation under models

---
+++ lightly/models/mlp.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+
+class MLP(nn.Module):
+    def __init__(self, dim, projection_size, hidden_size = 4096):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_size, projection_size)
+        )
+
+    def forward(self, x):
+        return self.net(x)
