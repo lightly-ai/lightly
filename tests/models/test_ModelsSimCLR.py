5a6b0f51da12e0ef3974abe5e956d3bb993f1215
philippmwirth <philipp.m.wirth@gmail.com>
2020-12-07 19:39:45 +0000

    Fix SimCLR test

--- tests/models/test_ModelsSimCLR.py
+++ tests/models/test_ModelsSimCLR.py
@@ -1,8 +1,22 @@
 import unittest
 
 import torch
+import torch.nn as nn
 
-from lightly.models import ResNetSimCLR
+import lightly
+from lightly.models import ResNetGenerator
+from lightly.models import SimCLR
+
+
+def get_backbone(resnet, num_ftrs=64):
+    last_conv_channels = list(resnet.children())[-1].in_features
+    backbone = nn.Sequential(
+        lightly.models.batchnorm.get_norm_layer(3, 0),
+        *list(resnet.children())[:-1],
+        nn.Conv2d(last_conv_channels, num_ftrs, 1),
+        nn.AdaptiveAvgPool2d(1),
+    )
+    return backbone
 
 
 class TestModelsSimCLR(unittest.TestCase):
@@ -20,14 +34,16 @@ class TestModelsSimCLR(unittest.TestCase):
 
     def test_create_variations_cpu(self):
         for model_name in self.resnet_variants:
-            model = ResNetSimCLR(name=model_name)
+            resnet = ResNetGenerator(model_name)
+            model = SimCLR(get_backbone(resnet))
             self.assertIsNotNone(model)
 
     def test_create_variations_gpu(self):
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         if device == 'cuda':
             for model_name in self.resnet_variants:
-                model = ResNetSimCLR(name=model_name).to(device)
+                resnet = ResNetGenerator(model_name)
+                model = SimCLR(get_backbone(resnet)).to(device)
                 self.assertIsNotNone(model)
         else:
             pass
@@ -37,13 +53,14 @@ class TestModelsSimCLR(unittest.TestCase):
         for model_name in self.resnet_variants:
             for num_ftrs in [2, 16, 32, 64]:
                 for out_dim in [64, 128, 256]:
-                    model = ResNetSimCLR(name=model_name,
-                                         num_ftrs=num_ftrs,
-                                         out_dim=out_dim).to(device)
+                    resnet = ResNetGenerator(model_name)
+                    model = SimCLR(get_backbone(resnet,  num_ftrs=num_ftrs),
+                                   num_ftrs=num_ftrs,
+                                   out_dim=out_dim).to(device)
 
                     # check that feature vector has correct dimension
                     with torch.no_grad():
-                        out_features = model.features(
+                        out_features = model.backbone(
                             self.input_tensor.to(device)
                         )
                     self.assertEqual(out_features.shape[1], num_ftrs)
@@ -61,7 +78,8 @@ class TestModelsSimCLR(unittest.TestCase):
         for model_name in self.resnet_variants:
             for input_width in [16, 32, 64, 128]:
                 for input_height in [16, 32, 64, 128]:
-                    model = ResNetSimCLR(name=model_name).to(device)
+                    resnet = ResNetGenerator(model_name)
+                    model = SimCLR(get_backbone(resnet, num_ftrs=32)).to(device)
 
                     input_tensor = torch.rand((self.batch_size,
                                                3,
