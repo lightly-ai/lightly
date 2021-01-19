f20acfa5b9292f4ced156763e848bf7b37cd94b8
MalteEbner <malte.ebner@gmail.com>
2021-01-19 10:23:49 +0100

    wrote unittests and made according bugfixes

--- lightly/active_learning/interface_to_client/agent.py
+++ lightly/active_learning/interface_to_client/agent.py
@@ -2,10 +2,10 @@ from typing import *
 
 import numpy as np
 
-from active_learning.interface_to_client.sampler_config import SamplerConfig
-from active_learning.scorers.scorer import ALScorer
+from lightly.active_learning.interface_to_client.sampler_config import SamplerConfig
+from lightly.active_learning.scorers.scorer import Scorer
 
-from active_learning.interface_to_api.active_learning_step import sampling_step_to_api
+from lightly.active_learning.interface_to_api.active_learning_step import sampling_step_to_api
 
 
 class ActiveLearningAgent:
@@ -14,9 +14,10 @@ class ActiveLearningAgent:
         self.dataset_id = dataset_id
         self.token = token
 
-    def sample(self, sampler_config: SamplerConfig, al_scorer: ALScorer = None, labelled_ids: List[int] = []):
+    def sample(self, sampler_config: SamplerConfig, al_scorer: Scorer = None, labelled_ids: List[int] = []):
         if al_scorer is not None:
             scores = al_scorer._calculate_scores()
         else:
             scores = dict()
-        sampling_step_to_api(sampler_config, scores, labelled_ids)
+        chosen_samples = sampling_step_to_api(sampler_config, scores, labelled_ids)
+        return chosen_samples
--- lightly/active_learning/scorers/scorer_classification.py
+++ lightly/active_learning/scorers/scorer_classification.py
@@ -3,19 +3,17 @@ from typing import *
 import numpy as np
 from scipy.stats import entropy
 
-from active_learning.scorers.scorer import ALScorer
+from lightly.active_learning.scorers.scorer import Scorer
 
 
-class ALScorerClassification(ALScorer):
+class ScorerClassification(Scorer):
     def __init__(self, model_output: np.ndarray):
-        assert self.model_output is np.ndarray
-        assert self.model_output.shape.__len__() == 2
         self.model_output = model_output
 
     def _calculate_scores(self) -> Dict[str, np.ndarray]:
         scores = dict()
-        scores["prediction_margin"] = self.get_prediction_margin_score()
-        scores["prediction_entropy"] = self.get_prediction_entropy_score()
+        scores["prediction_margin"] = self._get_prediction_margin_score()
+        scores["prediction_entropy"] = self._get_prediction_entropy_score()
         return scores
 
     def _get_prediction_margin_score(self):
