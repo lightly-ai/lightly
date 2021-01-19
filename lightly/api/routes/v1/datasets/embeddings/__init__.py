94e400d6b16f21cd8c0319a34e1b2e435c6a0a9d
philippmwirth <philipp.m.wirth@gmail.com>
2021-01-19 12:24:02 +0000

    Add routes for csv file upload

---
+++ lightly/api/routes/v1/datasets/embeddings/__init__.py
@@ -0,0 +1,8 @@
+""" Embeddings Routes """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+
+# provided functions
+from lightly.api.routes.v1.datasets.embeddings.service import get_presigned_upload_url  # noqa: F401, E402
