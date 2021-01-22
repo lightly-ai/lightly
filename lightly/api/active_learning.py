b27b68e49919d3f1af7ad717294fc4f102c265f5
MalteEbner <malte.ebner@gmail.com>
2021-01-22 15:57:38 +0100

    reworked lightly_active_learning.sampling_request_to_api to have better params

--- lightly/api/active_learning.py
+++ lightly/api/active_learning.py
@@ -6,8 +6,16 @@
 from typing import *
 import requests
 
-from lightly.active_learning.config.sampler_config import SamplerConfig
 from lightly.api.utils import getenv
+
+from lightly.openapi_generated.swagger_client.configuration import Configuration
+from lightly.openapi_generated.swagger_client.api_client import ApiClient
+from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
+
+from lightly.openapi_generated.swagger_client.models.sampling_method import SamplingMethod
+from lightly.openapi_generated.swagger_client.models.sampling_create_request import SamplingCreateRequest
+from lightly.openapi_generated.swagger_client.models.sampling_config import SamplingConfig
+from lightly.openapi_generated.swagger_client.models.sampling_config_stopping_condition import SamplingConfigStoppingCondition
 
 
 def upload_scores_to_api(scores: Dict[str, Iterable[float]]):
@@ -21,7 +29,8 @@ def sampling_request_to_server(token: str,
                                name: str,
                                method: str,
                                config: dict,
-                               ) -> str :
+                               ) -> str:
+    raise DeprecationWarning
     """ Makes the sampling request to the server.
 
     Args:
@@ -68,30 +77,51 @@ def sampling_request_to_server(token: str,
     if response.status_code == 200:
         response_json = response.json()
         return response_json['jobId']
-    
+
     raise RuntimeError(response.status_code)
 
 
-from lightly.openapi_generated.swagger_client.configuration import Configuration
-from lightly.openapi_generated.swagger_client.api_client import ApiClient
-from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
-from lightly.openapi_generated.swagger_client.models import \
-    sampling_config, sampling_method, sampling_create_request, sampling_config_stopping_condition
+def sampling_request_to_api(token: str,
+                            dataset_id: str,
+                            tag_id: str,
+                            embedding_id: str,
+                            sampling_request_name: str,
+                            sampling_method: SamplingMethod = SamplingMethod.RANDOM,
+                            stopping_condition_n_samples: int = None,
+                            stopping_condition_min_distance: float = None
+                            ) -> str:
+    """ Makes the sampling request to the server using the generated openAPI
 
+    Args:
+        token (str):
+            The token for authenticating the request.
+        dataset_id (str):
+            Unique identifier for the dataset.
+        tag_id (str):
+            Unique identifier for the current state (labelled_indices).
+        embedding_id (str):
+            Unique identifier for previosuly uploaded embeddings.
+        sampling_request_name (str):
+            Name describing the sampling request
+        sampling_method (sampling_method.SamplingMethod):
+            Choose from ['CORESET', 'RANDOM', 'BIT']
+        stopping_condition_n_samples (int):
+            the number of samples to sample
+        stopping_condition_min_distance (float):
+            the minimum distance chosen samples should have
 
-def sampling_request_to_server_with_openapi(
-        token: str,
-        dataset_id: str,
-        tag_id: str,
-        embedding_id: str,
-) -> str:
+    Returns:
+        jobId if the request was successful.
 
-    sampling_config_stopping_condition_ = sampling_config_stopping_condition.SamplingConfigStoppingCondition(
-        n_samples=10, min_distance=5)
-    sampling_config_ = sampling_config.SamplingConfig(stopping_condition=sampling_config_stopping_condition_)
-    sampling_method_ = sampling_method.SamplingMethod.RANDOM
-    sampling_create_request_ = sampling_create_request.SamplingCreateRequest(name='test', method=sampling_method_,
-                                                                             config=sampling_config_)
+    Raises:
+        Runtime error response status.
+    """
+
+    sampling_config_stopping_condition_ = SamplingConfigStoppingCondition(
+        n_samples=stopping_condition_n_samples, min_distance=stopping_condition_min_distance)
+    sampling_config_ = SamplingConfig(stopping_condition=sampling_config_stopping_condition_)
+    sampling_create_request_ = SamplingCreateRequest(
+        name=sampling_request_name, method=sampling_method, config=sampling_config_)
 
     configuration = Configuration()
     configuration.host = 'https://app-dev.lightly.ai'
@@ -109,13 +139,10 @@ if __name__ == '__main__':
     datasetId = '6006f54aab0cd9000ad7914c'
     tagId = 'initial-tag'
     embeddingId = '0'
-    name = 'sampling-test'
-    method = 'RANDOM'
-    config = {
-            'stoppingCondition': {
-                'nSamples': 10,
-                'minDistance': 5,
-            }
-        }
-    #print(sampling_request_to_server(token, datasetId, tagId,embeddingId, name, method,config))
-    print(sampling_request_to_server_with_openapi(token, datasetId, tagId, embeddingId))
+    sampling_name = 'sampling-test'
+    sampling_method = SamplingMethod.RANDOM
+    n_samples = 100
+    min_distance = 0.1
+    response_job_id = sampling_request_to_api(token, datasetId, tagId, embeddingId,
+                                              sampling_name, sampling_method, n_samples, min_distance)
+    print(response_job_id)
