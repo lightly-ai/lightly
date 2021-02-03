6fab6634b474e52af0fff84dd7f33153e8e04706
MalteEbner <malte.ebner@gmail.com>
2021-02-03 15:59:16 +0100

    Tests: include data/_utils.py:check_images() (#121)
    * Unittests: include data/_utils.py: check_images
    
    * Unittests: test_check_images: also checks for corrupt images

--- tests/data/test_LightlyDataset.py
+++ tests/data/test_LightlyDataset.py
@@ -9,6 +9,7 @@ import numpy as np
 from lightly.data import LightlyDataset
 import pytest
 
+from lightly.data._utils import check_images
 
 try:
     from lightly.data._video import VideoDataset
@@ -134,6 +135,31 @@ class TestLightlyDataset(unittest.TestCase):
 
         for i in range(n_tot):
             sample, target, fname = dataset[i]
+
+    def test_check_images(self):
+
+        # create a dataset
+        tmp_dir = tempfile.mkdtemp()
+        n_healthy = 100
+        n_corrupt = 20
+
+        dataset = torchvision.datasets.FakeData(size=n_healthy,
+                                                image_size=(3, 32, 32))
+        sample_names = [f'img_{i}.jpg' for i in range(n_healthy)]
+        for sample_name, data in zip(sample_names, dataset):
+            path = os.path.join(tmp_dir, sample_name)
+            data[0].save(path)
+
+        corrupt_sample_names = [f'img_{i}.jpg' for i in range(n_healthy,n_healthy+n_corrupt)]
+        for sample_name in corrupt_sample_names:
+            path = os.path.join(tmp_dir, sample_name)
+            with open(path, 'a') as f:
+                f.write('this_is_not_an_image')
+
+        # tests
+        healthy_images, corrupt_images = check_images(tmp_dir)
+        assert(len(healthy_images) == n_healthy)
+        assert(len(corrupt_images) == n_corrupt)
 
     def test_not_existing_folder_dataset(self):
         with self.assertRaises(ValueError):
