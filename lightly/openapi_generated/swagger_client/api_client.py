c58ebe7fdd12020afb7a8dcc4f9f9a4820e0117f
MalteEbner <malte.ebner@gmail.com>
2021-02-08 14:01:56 +0100

    Bugfix: generated client had wrong imports

--- lightly/openapi_generated/swagger_client/api_client.py
+++ lightly/openapi_generated/swagger_client/api_client.py
@@ -23,9 +23,9 @@ import tempfile
 import six
 from six.moves.urllib.parse import quote
 
-from swagger_client.configuration import Configuration
-import swagger_client.models
-from swagger_client import rest
+from lightly.openapi_generated.swagger_client.configuration import Configuration
+import lightly.openapi_generated.swagger_client.models
+from lightly.openapi_generated.swagger_client import rest
 
 
 class ApiClient(object):
@@ -267,7 +267,7 @@ class ApiClient(object):
             if klass in self.NATIVE_TYPES_MAPPING:
                 klass = self.NATIVE_TYPES_MAPPING[klass]
             else:
-                klass = getattr(swagger_client.models, klass)
+                klass = getattr(lightly.openapi_generated.swagger_client.models, klass)
 
         if klass in self.PRIMITIVE_TYPES:
             return self.__deserialize_primitive(data, klass)
