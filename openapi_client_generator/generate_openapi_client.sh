a25f859cf628c033563891d9934cf4bd3776af1a
MalteEbner <malte.ebner@gmail.com>
2021-02-08 17:50:19 +0100

    update generate_openapi_client.sh: use template

--- openapi_client_generator/generate_openapi_client.sh
+++ openapi_client_generator/generate_openapi_client.sh
@@ -2,7 +2,7 @@ cd ../
 PATH_TO_SPEC="../lightly-core/openapi/spec/api.yml"
 
 #npm install -g swagger-codegen
-swagger-codegen generate -l python -i $PATH_TO_SPEC  -o lightly/openapi_generated
+swagger-codegen generate -l python -i $PATH_TO_SPEC  -o lightly/openapi_generated  -t openapi_client_generator/python
 
 cd lightly/openapi_generated
 rm -R docs
