c0ad5ad572b040f6e5af34d2063fac01d7356ee8
MalteEbner <malte.ebner@gmail.com>
2021-02-08 14:01:56 +0100

    Bugfix: generated client had wrong imports

--- lightly/openapi_generated/swagger_client/api/tags_api.py
+++ lightly/openapi_generated/swagger_client/api/tags_api.py
@@ -17,7 +17,7 @@ import re  # noqa: F401
 # python 2 and python 3 compatibility library
 import six
 
-from swagger_client.api_client import ApiClient
+from lightly.openapi_generated.swagger_client.api_client import ApiClient
 
 
 class TagsApi(object):
