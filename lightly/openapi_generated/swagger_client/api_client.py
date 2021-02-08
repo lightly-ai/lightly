96a29643218cbb84ece11594d87a61cca14a653b
MalteEbner <malte.ebner@gmail.com>
2021-02-08 11:15:26 +0100

    generated new swagger_client

--- lightly/openapi_generated/swagger_client/api_client.py
+++ lightly/openapi_generated/swagger_client/api_client.py
@@ -22,9 +22,9 @@ import tempfile
 import six
 from six.moves.urllib.parse import quote
 
-from lightly.openapi_generated.swagger_client.configuration import Configuration
-import lightly.openapi_generated.swagger_client.models
-from lightly.openapi_generated.swagger_client import rest
+from swagger_client.configuration import Configuration
+import swagger_client.models
+from swagger_client import rest
 
 
 class ApiClient(object):
@@ -257,7 +257,7 @@ class ApiClient(object):
             if klass in self.NATIVE_TYPES_MAPPING:
                 klass = self.NATIVE_TYPES_MAPPING[klass]
             else:
-                klass = getattr(lightly.openapi_generated.swagger_client.models, klass)
+                klass = getattr(swagger_client.models, klass)
 
         if klass in self.PRIMITIVE_TYPES:
             return self.__deserialize_primitive(data, klass)
