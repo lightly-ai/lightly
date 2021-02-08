554b109a5414934fd185e7eb10f764f5db2b1624
MalteEbner <malte.ebner@gmail.com>
2021-02-08 16:54:26 +0100

    added template for generating python client

--- openapi_client_generator/generate_openapi_client.sh
+++ openapi_client_generator/generate_openapi_client.sh
@@ -1,14 +1,14 @@
-cd ../
-PATH_TO_SPEC="../lightly-core/openapi/spec/api.yml"
-
-# install swagger-codegen following the instructions at https://github.com/swagger-api/swagger-codegen#compatibility
-swagger-codegen generate -l python -i $PATH_TO_SPEC  -o lightly/openapi_generated  --template-dir openapi_client_generator/python --template-engine mustache
-
-cd lightly/openapi_generated
+PATH_TO_SPEC="../../lightly-core/openapi/spec/api.yml"
+PATH_FROM_BASE="lightly/openapi_generated"
+PATH_TO_OUTPUT="../$PATH_FROM_BASE"
+#npm install -g swagger-codegen
+swagger-codegen generate -l python -i $PATH_TO_SPEC  -o $PATH_TO_OUTPUT
+cd $PATH_TO_OUTPUT
 rm -R docs
 rm -R test
 
-# replaces of imports
-find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/swagger_client/lightly.openapi_generated.swagger_client/g'
-find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/from api./from lightly.openapi_generated.swagger_client.api./g'
+PATTERN_TO_REPLACE="swagger_client"
+PATTERN_REPLACING="${PATH_FROM_BASE/"/"/"."}/$PATTERN_TO_REPLACE"
+find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/PATTERN_TO_REPLACE/PATTERN_REPLACING/g'
+
 
