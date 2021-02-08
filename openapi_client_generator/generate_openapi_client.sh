50b7ba653b22e4a107638df552029ca616f12cdf
MalteEbner <malte.ebner@gmail.com>
2021-02-08 18:00:59 +0100

    genearte_openapi_client now running with script with template

--- openapi_client_generator/generate_openapi_client.sh
+++ openapi_client_generator/generate_openapi_client.sh
@@ -2,11 +2,11 @@ cd ../
 PATH_TO_SPEC="../lightly-core/openapi/spec/api.yml"
 
 #npm install -g swagger-codegen
-swagger-codegen generate -l python -i $PATH_TO_SPEC  -o lightly/openapi_generated  -t openapi_client_generator/python
+swagger-codegen generate -l python -i $PATH_TO_SPEC  -o lightly/openapi_generated  --template-dir openapi_client_generator/python --template-engine mustache
 
 cd lightly/openapi_generated
 rm -R docs
 rm -R test
 find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/swagger_client/lightly.openapi_generated.swagger_client/g'
-
+find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/from api./from lightly.openapi_generated.swagger_client.api./g'
 
