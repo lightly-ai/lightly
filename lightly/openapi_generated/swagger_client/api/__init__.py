e7ef18c5558df00b5b296e4ca41d661138b9b176
MalteEbner <malte.ebner@gmail.com>
2021-02-11 14:23:56 +0100

    openapi_client: add generated files

---
+++ lightly/openapi_generated/swagger_client/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lightly.openapi_generated.swagger_client.api.annotations_api import AnnotationsApi
+from lightly.openapi_generated.swagger_client.api.auth_api import AuthApi
+from lightly.openapi_generated.swagger_client.api.datasets_api import DatasetsApi
+from lightly.openapi_generated.swagger_client.api.embeddings_api import EmbeddingsApi
+from lightly.openapi_generated.swagger_client.api.internal_api import InternalApi
+from lightly.openapi_generated.swagger_client.api.jobs_api import JobsApi
+from lightly.openapi_generated.swagger_client.api.mappings_api import MappingsApi
+from lightly.openapi_generated.swagger_client.api.samples_api import SamplesApi
+from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
+from lightly.openapi_generated.swagger_client.api.tags_api import TagsApi
