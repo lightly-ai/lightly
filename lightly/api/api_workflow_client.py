90741ff6e723633c64eeba069c31dc54229c55ee
MalteEbner <malte.ebner@gmail.com>
2021-02-16 13:24:53 +0100

    175 create api call structure (#130)

---
+++ lightly/api/api_workflow_client.py
@@ -0,0 +1,93 @@
+from typing import *
+
+from lightly.data.dataset import LightlyDataset
+from lightly.api.upload import upload_images_from_folder, upload_dataset
+from lightly.openapi_generated.swagger_client.api.embeddings_api import EmbeddingsApi
+from lightly.openapi_generated.swagger_client.api.jobs_api import JobsApi
+from lightly.openapi_generated.swagger_client.api.mappings_api import MappingsApi
+from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
+from lightly.openapi_generated.swagger_client.api.tags_api import TagsApi
+from lightly.openapi_generated.swagger_client.api_client import ApiClient
+from lightly.openapi_generated.swagger_client.configuration import Configuration
+from lightly.api.api_workflow_upload_embeddings import _UploadEmbeddingsMixin
+
+
+class ApiWorkflowClient(_UploadEmbeddingsMixin):
+    """Provides a uniform interface to communicate with the api and run workflows including multiple API calls
+
+    Args:
+        host:
+            the url of the server, e.g. https://api-dev.lightly.ai
+        token:
+            the token of the user, provided in webapp
+        dataset_id:
+            the id of the dataset, provided in webapp
+        embedding_id:
+            the id of the embedding to use. If it is not set, but used by a workflow, the newest embedding is taken by default
+    """
+
+    def __init__(self, host: str, token: str, dataset_id: str, embedding_id: str = None):
+
+        configuration = Configuration()
+        configuration.host = host
+        configuration.api_key = {'token': token}
+        api_client = ApiClient(configuration=configuration)
+        self.api_client = api_client
+
+        self.dataset_id = dataset_id
+        if embedding_id is not None:
+            self.embedding_id = embedding_id
+
+        self.samplings_api = SamplingsApi(api_client=self.api_client)
+        self.jobs_api = JobsApi(api_client=self.api_client)
+        self.tags_api = TagsApi(api_client=self.api_client)
+        self.embeddings_api = EmbeddingsApi(api_client=api_client)
+        self.mappings_api = MappingsApi(api_client=api_client)
+
+    def _order_list_by_filenames(self, filenames_for_list: List[str], list_to_order: List[object]) -> List[object]:
+        """Orders a list such that it is in the order of the filenames specified on the server.
+
+        Args:
+            filenames_for_list:
+                The filenames of samples in a specific order
+            list_to_order:
+                Some values belonging to the samples
+
+        Returns:
+            The list reorderd. The same reorder applied on the filenames_for_list
+            would put them in the order of the filenames in self.filenames_on_server
+
+        """
+        assert len(filenames_for_list) == len(list_to_order)
+        dict_by_filenames = dict(zip(filenames_for_list, list_to_order))
+        list_ordered = [dict_by_filenames[filename] for filename in self.filenames_on_server
+                        if filename in filenames_for_list]
+        return list_ordered
+
+    @property
+    def filenames_on_server(self):
+        if not hasattr(self, "_filenames"):
+            self._filenames_on_server = self.mappings_api. \
+                get_sample_mappings_by_dataset_id(dataset_id=self.dataset_id, field="fileName")
+        return self._filenames_on_server
+
+    def upload_dataset(self, input: Union[str, LightlyDataset], **kwargs):
+        """Uploads a dataset to the server and creates the initial tag.
+
+        Args:
+            input:
+                one of the following:
+                    - the path to the dataset, e.g. "path/to/dataset"
+                    - the dataset in form of a LightlyDataset
+            **kwargs:
+                see specification of the called functions
+        """
+        if isinstance(input, str):
+            path_to_dataset = input
+            upload_images_from_folder(path_to_dataset, self.dataset_id, self.token, **kwargs)
+        elif isinstance(input, LightlyDataset):
+            dataset = input
+            upload_dataset(dataset, self.dataset_id, self.token, **kwargs)
+        else:
+            raise ValueError(f"input must either be a LightlyDataset or the path to the dataset as str, "
+                             f"but is of type {type(input)}")
---
+++ tests/api_workflow/mocked_api_workflow_client.py
@@ -0,0 +1,101 @@
+import lightly
+
+from lightly.api.api_workflow_client import ApiWorkflowClient
+
+from io import BufferedReader
+from typing import *
+
+from lightly.openapi_generated.swagger_client.api.embeddings_api import EmbeddingsApi
+from lightly.openapi_generated.swagger_client.api.jobs_api import JobsApi
+from lightly.openapi_generated.swagger_client.api.mappings_api import MappingsApi
+from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
+from lightly.openapi_generated.swagger_client.api.tags_api import TagsApi
+from lightly.openapi_generated.swagger_client.models.async_task_data import AsyncTaskData
+from lightly.openapi_generated.swagger_client.models.dataset_embedding_data import DatasetEmbeddingData
+from lightly.openapi_generated.swagger_client.models.job_result_type import JobResultType
+from lightly.openapi_generated.swagger_client.models.job_state import JobState
+from lightly.openapi_generated.swagger_client.models.job_status_data import JobStatusData
+from lightly.openapi_generated.swagger_client.models.job_status_data_result import JobStatusDataResult
+from lightly.openapi_generated.swagger_client.models.sampling_create_request import SamplingCreateRequest
+from lightly.openapi_generated.swagger_client.models.tag_data import TagData
+from lightly.openapi_generated.swagger_client.models.write_csv_url_data import WriteCSVUrlData
+
+
+class MockedEmbeddingsApi(EmbeddingsApi):
+    def get_embeddings_csv_write_url_by_id(self, dataset_id: str, **kwargs):
+        assert isinstance(dataset_id, str)
+        response_ = WriteCSVUrlData(signed_write_url="signed_write_url_valid", embedding_id="embedding_id_xyz")
+        return response_
+
+    def get_embeddings_by_dataset_id(self, dataset_id, **kwargs) -> List[DatasetEmbeddingData]:
+        assert isinstance(dataset_id,str)
+        return [DatasetEmbeddingData(id="embedding_id_xyz", name="embedding_name_xxyyzz",
+                                     is_processed=True, created_at=0)]
+
+
+class MockedSamplingsApi(SamplingsApi):
+    def trigger_sampling_by_id(self, body, dataset_id, embedding_id, **kwargs):
+        assert isinstance(body, SamplingCreateRequest)
+        assert isinstance(dataset_id, str)
+        assert isinstance(embedding_id, str)
+        response_ = AsyncTaskData(job_id="155")
+        return response_
+
+
+class MockedJobsApi(JobsApi):
+    def __init__(self, *args, **kwargs):
+        self.no_calls = 0
+        JobsApi.__init__(self, *args, **kwargs)
+
+    def get_job_status_by_id(self, job_id, **kwargs):
+        assert isinstance(job_id, str)
+        self.no_calls += 1
+        if self.no_calls > 3:
+            result = JobStatusDataResult(type=JobResultType.SAMPLING, data="tag_id_xyz")
+            response_ = JobStatusData(id="id_", status=JobState.FINISHED, wait_time_till_next_poll=0,
+                                      created_at=1234, finished_at=1357, result=result)
+        else:
+            result = None
+            response_ = JobStatusData(id="id_", status=JobState.RUNNING, wait_time_till_next_poll=0.5,
+                                      created_at=1234, result=result)
+        return response_
+
+
+class MockedTagsApi(TagsApi):
+    def get_tag_by_tag_id(self, dataset_id, tag_id, **kwargs):
+        assert isinstance(dataset_id, str)
+        assert isinstance(tag_id, str)
+        response_ = TagData(id=tag_id, dataset_id=dataset_id, prev_tag_id="initial-tag", bit_mask_data="0x80bda23e9",
+                            name='second-tag', tot_size=0, created_at=1577836800, changes=dict())
+        return response_
+
+
+class MockedMappingsApi(MappingsApi):
+    def __init__(self, *args, **kwargs):
+        sample_names = [f'img_{i}.jpg' for i in range(100)]
+        sample_names.reverse()
+        self.sample_names = sample_names
+        MappingsApi.__init__(self, *args, **kwargs)
+
+    def get_sample_mappings_by_dataset_id(self, dataset_id, field, **kwargs):
+        return self.sample_names
+
+
+def mocked_upload_file_with_signed_url(file: str, url: str, mocked_return_value=True) -> bool:
+    assert isinstance(file, BufferedReader)
+    assert isinstance(url, str)
+    return mocked_return_value
+
+
+class MockedApiWorkflowClient(ApiWorkflowClient):
+    def __init__(self, *args, **kwargs):
+        ApiWorkflowClient.__init__(self, *args, **kwargs)
+
+        self.samplings_api = MockedSamplingsApi(api_client=self.api_client)
+        self.jobs_api = MockedJobsApi(api_client=self.api_client)
+        self.tags_api = MockedTagsApi(api_client=self.api_client)
+        self.embeddings_api = MockedEmbeddingsApi(api_client=self.api_client)
+        self.mappings_api = MockedMappingsApi(api_client=self.api_client)
+        lightly.api.api_workflow_upload_embeddings.upload_file_with_signed_url = mocked_upload_file_with_signed_url
+
+
