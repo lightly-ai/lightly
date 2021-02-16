b802daefd1f22757deb0003410b32458868ef3ce
MalteEbner <malte.ebner@gmail.com>
2021-02-16 16:59:06 +0100

    Bugfix in _SamplingMixin: Wait 5s till job_status_data exists on the server

--- lightly/api/api_workflow_sampling.py
+++ lightly/api/api_workflow_sampling.py
@@ -5,6 +5,7 @@ from lightly.active_learning.config.sampler_config import SamplerConfig
 from lightly.openapi_generated.swagger_client.models.job_state import JobState
 from lightly.openapi_generated.swagger_client.models.job_status_data import JobStatusData
 from lightly.openapi_generated.swagger_client.models.tag_data import TagData
+from lightly.openapi_generated.swagger_client.rest import ApiException
 
 
 class _SamplingMixin:
@@ -20,21 +21,28 @@ class _SamplingMixin:
         payload.row_count = 15  # TODO: remove after solving https://github.com/lightly-ai/lightly-core/issues/150
         response = self.samplings_api.trigger_sampling_by_id(payload, self.dataset_id, self.embedding_id)
         job_id = response.job_id
-        print(f"job_id: {job_id}")
+
 
         # poll the job status till the job is finished
+        exception_counter = 0
         while True:
             try:
                 job_status_data: JobStatusData = self.jobs_api.get_job_status_by_id(job_id=job_id)
-                if job_status_data.status == JobState.FINISHED:
-                    new_tag = job_status_data.result.data
-                    break
-                elif job_status_data.status == JobState.FAILED:
-                    raise ValueError(f"Sampling job failed with error {job_status_data.error}")
             except Exception as e:
-                time.sleep(2)
+                exception_counter += 1
+                if exception_counter == 10:
+                    print(f"Sampling job with job_id {job_id} could not be started because of error: {e}")
+                time.sleep(1)
+                continue
 
+            if job_status_data.status == JobState.FINISHED:
+                new_tag = job_status_data.result.data
+                break
+            elif job_status_data.status == JobState.FAILED:
+                raise ValueError(f"Sampling job with job_id {job_id} failed with error {job_status_data.error}")
             time.sleep(job_status_data.wait_time_till_next_poll)
+
+
 
         # get the new tag from the job status
         new_tag_data = self.tags_api.get_tag_by_tag_id(self.dataset_id, tag_id=new_tag)
