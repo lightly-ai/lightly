9ece91809921ec388f09631e053654cbf5577cf1
MalteEbner <malte.ebner@gmail.com>
2021-02-17 17:17:22 +0100

    deleted scores api (not needed yet)

--- lightly/openapi_generated/swagger_client/api/__init__.py
+++ lightly/openapi_generated/swagger_client/api/__init__.py
@@ -9,5 +9,4 @@ from lightly.openapi_generated.swagger_client.api.jobs_api import JobsApi
 from lightly.openapi_generated.swagger_client.api.mappings_api import MappingsApi
 from lightly.openapi_generated.swagger_client.api.samples_api import SamplesApi
 from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
-from lightly.openapi_generated.swagger_client.api.scores_api import ScoresApi
 from lightly.openapi_generated.swagger_client.api.tags_api import TagsApi
