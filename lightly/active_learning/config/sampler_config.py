ea4a08f2821139ca69f3937bc0825f10bc033e83
MalteEbner <malte.ebner@gmail.com>
2021-02-24 09:36:40 +0100

    Changed sampler_config.n_samples to include the number of samples in the preselected tag

--- lightly/active_learning/config/sampler_config.py
+++ lightly/active_learning/config/sampler_config.py
@@ -11,22 +11,22 @@ class SamplerConfig:
     Attributes:
         method:
             The method to use for sampling, e.g. RANDOM.
-        batch_size:
-            The maximum number of samples to be chosen by the sampler.
+        n_samples:
+            The maximum number of samples to be chosen by the sampler including the samples in the preselected tag.
         min_distance:
             The minimum distance of samples in the chosen set, one of stopping conditions.
         name:
             The name of this sampling, defaults to a name consisting of all other attributes and the datetime
 
     """
-    def __init__(self, method: SamplingMethod = SamplingMethod.CORESET, batch_size: int = 32, min_distance: float = -1,
+    def __init__(self, method: SamplingMethod = SamplingMethod.CORESET, n_samples: int = 32, min_distance: float = -1,
                  name: str = None):
 
         self.method = method
-        self.batch_size = batch_size
+        self.n_samples = n_samples
         self.min_distance = min_distance
         if name is None:
             date_time = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")
-            name = f"{self.method}_{self.batch_size}_{self.min_distance}_{date_time}"
+            name = f"{self.method}_{self.n_samples}_{self.min_distance}_{date_time}"
         self.name = name
 
