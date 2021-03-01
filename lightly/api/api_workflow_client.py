c64b5be090fc1f17103d3a02c4ab784375d8901a
MalteEbner <malte.ebner@gmail.com>
2021-03-01 10:30:39 +0100

    put ApiWorkflowClient.UploadDataset in own Mixin

--- lightly/api/api_workflow_client.py
+++ lightly/api/api_workflow_client.py
@@ -1,10 +1,9 @@
 import os
 from typing import *
 
+from lightly.api.api_workflow_upload_dataset import _UploadDatasetMixin
 from lightly.api.api_workflow_upload_embeddings import _UploadEmbeddingsMixin
 from lightly.api.api_workflow_sampling import _SamplingMixin
-from lightly.data.dataset import LightlyDataset
-from lightly.api.upload import upload_images_from_folder, upload_dataset
 from lightly.openapi_generated.swagger_client import TagData, ScoresApi
 from lightly.openapi_generated.swagger_client.api.embeddings_api import EmbeddingsApi
 from lightly.openapi_generated.swagger_client.api.jobs_api import JobsApi
@@ -15,7 +14,7 @@ from lightly.openapi_generated.swagger_client.api_client import ApiClient
 from lightly.openapi_generated.swagger_client.configuration import Configuration
 
 
-class ApiWorkflowClient(_UploadEmbeddingsMixin, _SamplingMixin):
+class ApiWorkflowClient(_UploadEmbeddingsMixin, _SamplingMixin, _UploadDatasetMixin):
     """Provides a uniform interface to communicate with the api and run workflows including multiple API calls
 
     Args:
@@ -79,24 +78,3 @@ class ApiWorkflowClient(_UploadEmbeddingsMixin, _SamplingMixin):
             self._filenames_on_server = self.mappings_api. \
                 get_sample_mappings_by_dataset_id(dataset_id=self.dataset_id, field="fileName")
         return self._filenames_on_server
-
-    def upload_dataset(self, input: Union[str, LightlyDataset], **kwargs):
-        """Uploads a dataset to the server and creates the initial tag.
-
-        Args:
-            input:
-                one of the following:
-                    - the path to the dataset, e.g. "path/to/dataset"
-                    - the dataset in form of a LightlyDataset
-            **kwargs:
-                see specification of the called functions
-        """
-        if isinstance(input, str):
-            path_to_dataset = input
-            upload_images_from_folder(path_to_dataset, self.dataset_id, self.token, **kwargs)
-        elif isinstance(input, LightlyDataset):
-            dataset = input
-            upload_dataset(dataset, self.dataset_id, self.token, **kwargs)
-        else:
-            raise ValueError(f"input must either be a LightlyDataset or the path to the dataset as str, "
-                             f"but is of type {type(input)}")
---
+++ lightly/api/api_workflow_upload_dataset.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Union
+
+from build.lib.lightly.api.upload import upload_dataset
+from lightly.api.upload import upload_images_from_folder
+
+from lightly.data.dataset import LightlyDataset
+
+if TYPE_CHECKING:
+    from lightly.api.api_workflow_client import ApiWorkflowClient
+
+import csv
+from typing import List
+
+
+class _UploadDatasetMixin:
+    def upload_dataset(self: ApiWorkflowClient, input: Union[str, LightlyDataset], **kwargs):
+        """Uploads a dataset to the server and creates the initial tag.
+
+        Args:
+            input:
+                one of the following:
+                    - the path to the dataset, e.g. "path/to/dataset"
+                    - the dataset in form of a LightlyDataset
+            **kwargs:
+                see specification of the called functions
+        """
+        if isinstance(input, str):
+            path_to_dataset = input
+            upload_images_from_folder(path_to_dataset, self.dataset_id, self.token, **kwargs)
+        elif isinstance(input, LightlyDataset):
+            dataset = input
+            upload_dataset(dataset, self.dataset_id, self.token, **kwargs)
+        else:
+            raise ValueError(f"input must either be a LightlyDataset or the path to the dataset as str, "
+                             f"but is of type {type(input)}")
