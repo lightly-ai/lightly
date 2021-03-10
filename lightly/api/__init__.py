ad9fdc78e464e8547f44296986612294d4492135
MalteEbner <malte.ebner@gmail.com>
2021-03-10 17:31:54 +0100

    deleted old routes and tests needed for getting pip version

--- lightly/api/__init__.py
+++
@@ -1,8 +0,0 @@
-""" The lightly.api module provides access to the Lightly web-app. """
-
-# Copyright (c) 2020. Lightly AG and its affiliates.
-# All Rights Reserved
-
-from lightly.api import routes
-from lightly.api.routes.pip import get_version              # noqa: F401
-from lightly.api.download import get_samples_by_tag         # noqa: F401
