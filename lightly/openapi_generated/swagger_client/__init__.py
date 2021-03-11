7bf2fd5169c7d768f2e5c9e6d59ef46ce09782a0
MalteEbner <malte.ebner@gmail.com>
2021-03-11 18:07:38 +0100

    add quota api

--- lightly/openapi_generated/swagger_client/__init__.py
+++ lightly/openapi_generated/swagger_client/__init__.py
@@ -20,6 +20,7 @@ from lightly.openapi_generated.swagger_client.api.datasets_api import DatasetsAp
 from lightly.openapi_generated.swagger_client.api.embeddings_api import EmbeddingsApi
 from lightly.openapi_generated.swagger_client.api.jobs_api import JobsApi
 from lightly.openapi_generated.swagger_client.api.mappings_api import MappingsApi
+from lightly.openapi_generated.swagger_client.api.quota_api import QuotaApi
 from lightly.openapi_generated.swagger_client.api.samples_api import SamplesApi
 from lightly.openapi_generated.swagger_client.api.samplings_api import SamplingsApi
 from lightly.openapi_generated.swagger_client.api.scores_api import ScoresApi
