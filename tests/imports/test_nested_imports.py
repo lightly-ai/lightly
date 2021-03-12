0e9006630f3e8f69c6e0816e24b63d30b9c38c7a
MalteEbner <malte.ebner@gmail.com>
2021-03-12 16:37:58 +0100

    deleted imports of routes

--- tests/imports/test_nested_imports.py
+++ tests/imports/test_nested_imports.py
@@ -14,10 +14,6 @@ class TestNestedImports(unittest.TestCase):
         #lightly.active_learning.scorers.classification.ScorerClassification
 
         # api imports
-        lightly.api.routes.users.get_quota
-        lightly.api.routes.users.docker.get_authorization
-        lightly.api.routes.users.docker.get_soft_authorization
-        lightly.api.routes.users.docker.post_diagnostics
         lightly.api.api_workflow_client.ApiWorkflowClient
         lightly.api.bitmask.BitMask
 
