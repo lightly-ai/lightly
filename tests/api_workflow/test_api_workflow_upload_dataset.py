2f28ee503b3c959be5896aea1885fe59aad614fd
MalteEbner <malte.ebner@gmail.com>
2021-03-12 14:54:06 +0100

    unittests for using new get_quota endpoint

--- tests/api_workflow/test_api_workflow_upload_dataset.py
+++ tests/api_workflow/test_api_workflow_upload_dataset.py
@@ -1,23 +1,21 @@
 import os
 import tempfile
-from typing import Tuple
 
 import torchvision
 from lightly.data.dataset import LightlyDataset
 
-import lightly
-
-from tests.api_workflow.mocked_api_workflow_client import MockedApiWorkflowSetup, MockedSamplesApi, mocked_get_quota, \
-    mocked_put_request
+from tests.api_workflow.mocked_api_workflow_client import MockedApiWorkflowSetup
 
 
 class TestApiWorkflowUploadDataset(MockedApiWorkflowSetup):
     def setUp(self) -> None:
         MockedApiWorkflowSetup.setUp(self)
+        self.n_data = 100
         self.create_fake_dataset()
         self.api_workflow_client.tags_api.no_tags = 0
 
-    def create_fake_dataset(self, n_data=1000):
+    def create_fake_dataset(self):
+        n_data = self.n_data
         self.dataset = torchvision.datasets.FakeData(size=n_data,
                                                      image_size=(3, 32, 32))
 
@@ -28,6 +26,15 @@ class TestApiWorkflowUploadDataset(MockedApiWorkflowSetup):
             data = self.dataset[sample_idx]
             path = os.path.join(self.folder_path, sample_names[sample_idx])
             data[0].save(path)
+
+    def test_upload_dataset_over_quota(self):
+        quota = self.n_data-1
+        def get_quota_reduced():
+            return str(quota)
+        self.api_workflow_client.quota_api.get_quota = get_quota_reduced
+        with self.assertRaises(ValueError):
+            self.api_workflow_client.upload_dataset(input=self.folder_path)
+
 
     def test_upload_dataset_from_folder(self):
         self.api_workflow_client.upload_dataset(input=self.folder_path)
