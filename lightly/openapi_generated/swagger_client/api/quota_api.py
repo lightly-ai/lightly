81a7093a2e09f2ec610abbec3413d1d70ea6ff85
MalteEbner <malte.ebner@gmail.com>
2021-03-12 13:39:06 +0100

    quota_api from new api specs

--- lightly/openapi_generated/swagger_client/api/quota_api.py
+++ lightly/openapi_generated/swagger_client/api/quota_api.py
@@ -43,7 +43,7 @@ class QuotaApi(object):
         >>> result = thread.get()
 
         :param async_req bool
-        :return: int
+        :return: str
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -64,7 +64,7 @@ class QuotaApi(object):
         >>> result = thread.get()
 
         :param async_req bool
-        :return: int
+        :return: str
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -112,7 +112,7 @@ class QuotaApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='int',  # noqa: E501
+            response_type='str',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
