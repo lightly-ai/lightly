e141c2399bc0e2ad7ab94c251d19659a99c7173e
IgorSusmelj <igor_7779@hotmail.com>
2021-03-14 12:26:50 +0000

    Update benchmark to use new BenchmarkModule from lighlty

--- docs/source/getting_started/benchmarks/cifar10_benchmark.py
+++ docs/source/getting_started/benchmarks/cifar10_benchmark.py
@@ -23,6 +23,7 @@ import torchvision
 import numpy as np
 import pytorch_lightning as pl
 import lightly
+from lightly.utils import BenchmarkModule
 
 num_workers = 8
 memory_bank_size = 4096
@@ -128,107 +129,11 @@ def get_data_loaders(batch_size: int):
     )
 
     return dataloader_train_ssl, dataloader_train_kNN, dataloader_test
-
-# code for kNN prediction from here:
-# https://colab.research.google.com/github/facebookresearch/moco/blob/colab-notebook/colab/moco_cifar10_demo.ipynb
-def knn_predict(feature, feature_bank, feature_labels, classes: int, knn_k: int, knn_t: float):
-    """Helper method to run kNN predictions on features based on a feature bank
-
-    Args:
-        feature: Tensor of shape [N, D] consisting of N D-dimensional features
-        feature_bank: Tensor of a database of features used for kNN
-        feature_labels: Labels for the features in our feature_bank
-        classes: Number of classes (e.g. 10 for CIFAR-10)
-        knn_k: Number of k neighbors used for kNN
-        knn_t: 
-
-    """
-
-    # compute cos similarity between each feature vector and feature bank ---> [B, N]
-    sim_matrix = torch.mm(feature, feature_bank)
-    # [B, K]
-    sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
-    # [B, K]
-    sim_labels = torch.gather(feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices)
-
-    # we do a reweighting of the similarities 
-    sim_weight = (sim_weight / knn_t).exp()
-
-    # counts for each class
-    one_hot_label = torch.zeros(feature.size(0) * knn_k, classes, device=sim_labels.device)
-    # [B*K, C]
-    one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
-    # weighted score ---> [B, C]
-    pred_scores = torch.sum(one_hot_label.view(feature.size(0), -1, classes) * sim_weight.unsqueeze(dim=-1), dim=1)
-
-    pred_labels = pred_scores.argsort(dim=-1, descending=True)
-    return pred_labels
-
-
-class BenchmarkModule(pl.LightningModule):
-    """A PyTorch Lightning Module for automated kNN callback
     
-    At the end of every training epoch we create a feature bank by inferencing
-    the backbone on the dataloader passed to the module. 
-    At every validation step we predict features on the validation data.
-    After all predictions on validation data (validation_epoch_end) we evaluate
-    the predictions on a kNN classifier on the validation data using the 
-    feature_bank features from the train data.
-    We can access the highest accuracy during a kNN prediction using the 
-    max_accuracy attribute.
-    """
-    def __init__(self, dataloader_kNN):
-        super().__init__()
-        self.backbone = nn.Module()
-        self.max_accuracy = 0.0
-        self.dataloader_kNN = dataloader_kNN
-
-    def training_epoch_end(self, outputs):
-        # update feature bank at the end of each training epoch
-        self.backbone.eval()
-        self.feature_bank = []
-        self.targets_bank = []
-        with torch.no_grad():
-            for data in self.dataloader_kNN:
-                img, target, _ = data
-                if gpus > 0:
-                    img = img.cuda()
-                    target = target.cuda()
-                feature = self.backbone(img).squeeze()
-                feature = F.normalize(feature, dim=1)
-                self.feature_bank.append(feature)
-                self.targets_bank.append(target)
-        self.feature_bank = torch.cat(self.feature_bank, dim=0).t().contiguous()
-        self.targets_bank = torch.cat(self.targets_bank, dim=0).t().contiguous()
-        self.backbone.train()
-
-    def validation_step(self, batch, batch_idx):
-        # we can only do kNN predictions once we have a feature bank
-        if hasattr(self, 'feature_bank') and hasattr(self, 'targets_bank'):
-            images, targets, _ = batch
-            feature = self.backbone(images).squeeze()
-            feature = F.normalize(feature, dim=1)
-            pred_labels = knn_predict(feature, self.feature_bank, self.targets_bank, classes, knn_k, knn_t)
-            num = images.size(0)
-            top1 = (pred_labels[:, 0] == targets).float().sum().item()
-            return (num, top1)
-    
-    def validation_epoch_end(self, outputs):
-        if outputs:
-            total_num = 0
-            total_top1 = 0.
-            for (num, top1) in outputs:
-                total_num += num
-                total_top1 += top1
-            acc = float(total_top1 / total_num)
-            if acc > self.max_accuracy:
-                self.max_accuracy = acc
-            self.log('kNN_accuracy', acc * 100.0)
-
 
 class MocoModel(BenchmarkModule):
-    def __init__(self, dataloader_kNN):
-        super().__init__(dataloader_kNN)
+    def __init__(self, dataloader_kNN, num_classes):
+        super().__init__(dataloader_kNN, num_classes)
         # create a ResNet backbone and remove the classification head
         resnet = lightly.models.ResNetGenerator('resnet-18', num_splits=8)
         self.backbone = nn.Sequential(
@@ -264,8 +169,8 @@ class MocoModel(BenchmarkModule):
 
 
 class SimCLRModel(BenchmarkModule):
-    def __init__(self, dataloader_kNN):
-        super().__init__(dataloader_kNN)
+    def __init__(self, dataloader_kNN, num_classes):
+        super().__init__(dataloader_kNN, num_classes)
         # create a ResNet backbone and remove the classification head
         resnet = lightly.models.ResNetGenerator('resnet-18')
         self.backbone = nn.Sequential(
@@ -295,8 +200,8 @@ class SimCLRModel(BenchmarkModule):
 
 
 class SimSiamModel(BenchmarkModule):
-    def __init__(self, dataloader_kNN):
-        super().__init__(dataloader_kNN)
+    def __init__(self, dataloader_kNN, num_classes):
+        super().__init__(dataloader_kNN, num_classes)
         # create a ResNet backbone and remove the classification head
         resnet = lightly.models.ResNetGenerator('resnet-18')
         self.backbone = nn.Sequential(
@@ -338,7 +243,7 @@ for batch_size in batch_sizes:
         for seed in range(n_runs):
             pl.seed_everything(seed)
             dataloader_train_ssl, dataloader_train_kNN, dataloader_test = get_data_loaders(batch_size)
-            model = Model(dataloader_train_kNN)
+            model = Model(dataloader_train_kNN, classes)
             trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus,
                                 progress_bar_refresh_rate=100)
             trainer.fit(
