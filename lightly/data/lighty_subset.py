bd89ca085b17933b57450cbd7fda109977d143a4
MalteEbner <malte.ebner@gmail.com>
2021-03-16 10:36:25 +0100

    LightlySubset: followed suggestions from PR

--- lightly/data/lighty_subset.py
+++ lightly/data/lighty_subset.py
@@ -6,26 +6,26 @@ from lightly.data.dataset import LightlyDataset
 class LightlySubset(LightlyDataset):
     def __init__(self, base_dataset: LightlyDataset, filenames_subset: List[str]):
         self.base_dataset = base_dataset
+        self.filenames_subset = filenames_subset
 
-        dict_base_dataset_filename_index: Dict[str, int] = \
-            dict([
-                (base_dataset.index_to_filename(self.dataset, index), index)
-                for index in range(len(base_dataset))
-            ])
-        self.mapping_subset_index_baseset = \
+        dict_base_dataset_filename_index: Dict[str, int] = dict()
+        for index in range(len(base_dataset)):
+            fname = base_dataset.index_to_filename(self.dataset, index)
+            dict_base_dataset_filename_index[fname] = index
+
+        self.mapping_subset_index_to_baseset_index = \
             [dict_base_dataset_filename_index[filename] for filename in filenames_subset]
 
     def __getitem__(self, index_subset: int) -> Tuple[object, object, str]:
-        index_baseset = self.mapping_subset_index_baseset[index_subset]
+        index_baseset = self.mapping_subset_index_to_baseset_index[index_subset]
         sample, target, fname = self.base_dataset.__getitem__(index_baseset)
         return sample, target, fname
 
     def __len__(self) -> int:
-        return len(self.mapping_subset_index_baseset)
+        return len(self.mapping_subset_index_to_baseset_index)
 
     def index_to_filename(self, dataset, index_subset: int):
-        index_baseset = self.mapping_subset_index_baseset[index_subset]
-        fname = self.base_dataset.index_to_filename(dataset, index_baseset)
+        fname = self.filenames_subset[index_subset]
         return fname
 
     @property
