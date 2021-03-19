5b217d724ec11244efbaced1b30de7f72cb15344
IgorSusmelj <igor_7779@hotmail.com>
2021-03-19 23:26:27 +0000

    Fix typo. Add example to docstring.

--- lightly/active_learning/scorers/detection.py
+++ lightly/active_learning/scorers/detection.py
@@ -6,7 +6,7 @@ from lightly.active_learning.scorers.scorer import Scorer
 
 
 def _object_frequency(model_output: List[Dict],
-                      frequency_pentaly: float,
+                      frequency_penalty: float,
                       min_score: float) -> np.ndarray:
     """Scorer which prefers samples with many and diverse objects
 
@@ -30,7 +30,7 @@ def _object_frequency(model_output: List[Dict],
         for label_prob in output['class_probabilities']:
             label = np.argmax(label_prob)
             if label in objs:
-                objs[label] += frequency_pentaly
+                objs[label] += frequency_penalty
             else:
                 objs[label] = 1
         for k, v in objs.items():
@@ -59,6 +59,14 @@ class ScorerObjectDetection(Scorer):
             C classes. The class probabilities should all sum up to 1.
         config:
             A dictionary containing additional parameters for the scorers.
+
+    Examples:
+        >>> predictions = [{
+        >>>     'boxes': [[14, 16, 52, 85]],
+        >>>     'object_probabilities': [0.1024],
+        >>>     'class_probabilities': [[0.5, 0.41, 0.09]]
+        >>> }]
+        >>> scorer = ScorerObjectDetection(predictions)
     """
 
     def __init__(self, model_output: List[Dict], config: Dict=None):
