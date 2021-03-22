ba8466ce60b74461df32e690b8f8ec3f4c0c9471
philippmwirth <philipp.m.wirth@gmail.com>
2021-03-22 14:43:41 +0000

    Improve code coverage

--- tests/active_learning/test_BoundingBox.py
+++ tests/active_learning/test_BoundingBox.py
@@ -7,11 +7,27 @@ class TestBoundingBox(unittest.TestCase):
 
     def test_bounding_box(self):
         bbox = BoundingBox(0.2, 0.3, 0.5, 0.6)
+        self.assertEqual(bbox.x0, 0.2)
+        self.assertEqual(bbox.y0, 0.3)
+        self.assertEqual(bbox.x1, 0.5)
+        self.assertEqual(bbox.y1, 0.6)  
+
+    def test_bounding_box_2(self):
+        bbox = BoundingBox.from_x_y_w_h(0.2, 0.3, 0.3, 0.3)
+        self.assertEqual(bbox.x0, 0.2)
+        self.assertEqual(bbox.y0, 0.3)
+        self.assertEqual(bbox.x1, 0.5)
+        self.assertEqual(bbox.y1, 0.6)
 
     def test_bounding_box_illogical_argument(self):
         with self.assertRaises(ValueError):
             # let x1 < x0
             bbox = BoundingBox(0.5, 0.3, 0.1, 0.6)
+
+    def test_bounding_box_illogical_argument_2(self):
+        with self.assertRaises(ValueError):
+            # let y1 < y0
+            bbox = BoundingBox(0.2, 0.6, 0.5, 0.3)
 
     def test_bounding_box_oob_arguments(self):
         with self.assertRaises(ValueError):
