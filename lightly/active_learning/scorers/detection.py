7a7f32839b75a3c1c0bd72bacd0a135412894d4a
philippmwirth <philipp.m.wirth@gmail.com>
2021-03-22 13:18:58 +0000

    Refactor OD scorer and add prediction-margin

--- lightly/active_learning/scorers/detection.py
+++ lightly/active_learning/scorers/detection.py
@@ -3,12 +3,13 @@ from typing import *
 import numpy as np
 
 from lightly.active_learning.scorers.scorer import Scorer
+from lightly.active_learning.utils.object_detection_output import ObjectDetectionOutput
 
 
-def _object_frequency(model_output: List[Dict],
+def _object_frequency(model_output: List[ObjectDetectionOutput],
                       frequency_penalty: float,
                       min_score: float) -> np.ndarray:
-    """Scorer which prefers samples with many and diverse objects
+    """Score which prefers samples with many and diverse objects.
 
     Args:
         model_outputs:
@@ -22,13 +23,13 @@ def _object_frequency(model_output: List[Dict],
         
     Returns:
         Numpy array of length N with the computed scores
+
     """
     n_objs = []
     for output in model_output:
         val = 0
         objs = {}
-        for label_prob in output['class_probabilities']:
-            label = np.argmax(label_prob)
+        for label in output.labels:
             if label in objs:
                 objs[label] += frequency_penalty
             else:
@@ -43,20 +44,29 @@ def _object_frequency(model_output: List[Dict],
     return np.asarray(scores)
 
 
+def _prediction_margin(model_output: List[ObjectDetectionOutput]):
+    """TODO
+
+    """
+    scores = []
+    for output in model_output:
+        if len(output.scores) > 0:
+            # prediction margin is 1 - max(class probs), therefore the mean margin
+            # is mean(1 - max(class probs)) which is 1 - mean(max(class probs))
+            score = 1. - np.mean(output.scores)
+        else:
+            # set the score to 0 if there was no bounding box detected
+            score = 0.
+        scores.append(score)
+    return np.asarray(scores)
+
+
 class ScorerObjectDetection(Scorer):
-    """Class to compute active learning scores from the model_output of a object detection task.
+    """Class to compute active learning scores from the model_output of an object detection task.
 
     Attributes:
         model_output:
-            List of length N containing the predictions of an object detection
-            model. For each sample we expect a dictionary containing `boxes`,
-            `object_probabilities` and `class_probabilities`.
-            `boxes` has shape [B, 4] for B found bounding boxes and the 4 values
-            x1, y1, x2, y2.
-            `object_probabilities` has shape [B] with a objectness value for each
-            bounding box.
-            `class_probabilities` has shape [B, C] for the B bounding boxes and
-            C classes. The class probabilities should all sum up to 1.
+            TODO
         config:
             A dictionary containing additional parameters for the scorers.
 
@@ -69,16 +79,23 @@ class ScorerObjectDetection(Scorer):
         >>> scorer = ScorerObjectDetection(predictions)
     """
 
-    def __init__(self, model_output: List[Dict], config: Dict=None):
-        self.model_output = model_output
+    def __init__(self,
+                 model_output: List[ObjectDetectionOutput],
+                 config: Dict = None):
+        super(ScorerObjectDetection, self).__init__(model_output)
         self.config = config
 
     def _calculate_scores(self) -> Dict[str, np.ndarray]:
         scores = dict()
-        scores["object-frequency"] = self._get_object_frequency()
+        scores['object-frequency'] = self._get_object_frequency()
+        scores['prediction-margin'] = self._get_prediction_margin()
         return scores
 
     def _get_object_frequency(self):
         scores = _object_frequency(self.model_output, 0.25, 0.9)
+        return scores
+
+    def _get_prediction_margin(self):
+        scores = _prediction_margin(self.model_output)
         return scores
 
