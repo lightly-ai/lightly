1f6ef04a94a744515d9d89ee8d86e3a98a329736
Philipp Wirth <65946090+philippmwirth@users.noreply.github.com>
2021-03-31 16:05:01 +0200

    126 consistency regularization pw (#267)
    * Add CO2 regularizer
    
    * Add regularizer to docs
    
    * Add tests for co2 regularizer

---
+++ lightly/loss/regularizer/__init__.py
@@ -0,0 +1,7 @@
+"""The lightly.loss.regularizer package provides regularizers for self-supervised learning. """
+
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.loss.regularizer.co2 import CO2Regularizer
