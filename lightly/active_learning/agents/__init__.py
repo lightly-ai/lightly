fa8b010f4ec16dfb16b208a55bcde2f04374e4bc
Philipp Wirth <65946090+philippmwirth@users.noreply.github.com>
2021-03-31 16:29:41 +0200

    199 active learning docstring examples (#270)
    * Add more examples to docstrings
    
    * Fix al import statements

---
+++ lightly/active_learning/agents/__init__.py
@@ -0,0 +1,6 @@
+""" Collection of Active Lerning Agents """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.active_learning.agents.agent import ActiveLearningAgent
