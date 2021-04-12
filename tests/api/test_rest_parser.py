5b7af1926acf3dbed0e347057e91d605321b919c
MalteEbner <malte.ebner@gmail.com>
2021-04-12 11:52:04 +0200

    Update test_rest_parser.py

--- tests/api/test_rest_parser.py
+++ tests/api/test_rest_parser.py
@@ -9,7 +9,8 @@ from lightly.openapi_generated.swagger_client.rest import ApiException
 
 class TestRestParser(unittest.TestCase):
 
-    def test_parse_active_learning_scores_generator(self):
+    @unittest.skip("This test only shows the error, it does not ensure it is solved.")
+    def test_parse_active_learning_scores(self):
         score_value_tuple = (
             np.random.normal(0, 1, size=(999,)).astype(np.float32),
             np.random.normal(0, 1, size=(999,)).astype(np.float64),
@@ -20,7 +21,12 @@ class TestRestParser(unittest.TestCase):
         for i, score_values in enumerate(score_value_tuple):
             with self.subTest(i=i, msg=str(type(score_values))):
                 body = ActiveLearningScoreCreateRequest(score_type=SamplingMethod.CORESET, scores=list(score_values))
-                with self.assertRaises(ApiException):
-                    self.scores_api.create_or_update_active_learning_score_by_tag_id(
-                        body, dataset_id="dataset_id_xyz", tag_id="tag_id_xyz")
+                if isinstance(score_values[0], float):
+                    with self.assertRaises(ApiException):
+                        self.scores_api.create_or_update_active_learning_score_by_tag_id(
+                            body, dataset_id="dataset_id_xyz", tag_id="tag_id_xyz")
+                else:
+                    with self.assertRaises(AttributeError):
+                        self.scores_api.create_or_update_active_learning_score_by_tag_id(
+                            body, dataset_id="dataset_id_xyz", tag_id="tag_id_xyz")
 
