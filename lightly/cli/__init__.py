8c4546760a964171cecc25a2709d3d284060af6f
MalteEbner <malte.ebner@gmail.com>
2021-04-16 19:46:47 +0200

    created tests for download_cli

--- lightly/cli/__init__.py
+++ lightly/cli/__init__.py
@@ -10,4 +10,4 @@ from lightly.cli.lightly_cli import lightly_cli
 from lightly.cli.train_cli import train_cli
 from lightly.cli.embed_cli import embed_cli
 from lightly.cli.upload_cli import upload_cli
-from lightly.cli.download_cli import download_cli
+from lightly.cli.download_cli_file import download_cli
