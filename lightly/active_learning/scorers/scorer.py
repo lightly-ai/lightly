1e20e1b94287fdb1e0c406b4a2cc4aa005efa05c
MalteEbner <malte.ebner@gmail.com>
2021-04-19 16:28:21 +0200

    scorer.py: shorter docstring

--- lightly/active_learning/scorers/scorer.py
+++ lightly/active_learning/scorers/scorer.py
@@ -9,12 +9,6 @@ class Scorer():
         self.model_output = model_output
 
     def calculate_scores(self) -> Dict[str, np.ndarray]:
-        """Calculates and returns the active learning scores
-
-        Which scores are calculated depends on the implementation
-        of this parent class by the child classes.
-        Returns:
-            A dictionary mapping from the score name (as string)
-            to the scores (as a single-dimensional numpy array).
+        """Calculates and returns active learning scores in a dictionary.
         """
         raise NotImplementedError
