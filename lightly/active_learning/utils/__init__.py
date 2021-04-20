ceb12c0ffd95a7b0d30b28a12f9ba46a3d9a1c25
philippmwirth <philipp.m.wirth@gmail.com>
2021-04-20 05:47:08 +0000

    Fix imports for api workflow client and al utils

---
+++ lightly/active_learning/utils/__init__.py
@@ -0,0 +1,7 @@
+""" Collection of Utils for Active Learning """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.active_learning.utils.bounding_box import BoundingBox
+from lightly.active_learning.utils.object_detection_output import ObjectDetectionOutput
