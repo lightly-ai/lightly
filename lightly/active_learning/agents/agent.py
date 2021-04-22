34d5588317cffa1389e0a49254510a8fb261f9fd
philippmwirth <philipp.m.wirth@gmail.com>
2021-04-22 14:58:08 +0000

    Show query tag to user

--- lightly/active_learning/agents/agent.py
+++ lightly/active_learning/agents/agent.py
@@ -14,16 +14,10 @@ class ActiveLearningAgent:
     Attributes:
         api_workflow_client:
             The client to connect to the api.
-        preselected_tag_id:
-            The id of the tag containing the already labeled samples, default: None == no labeled samples yet.
-        query_tag_id:
-            The id of the tag defining where to sample from, default: None resolves to initial_tag
-        labeled_set:
-            The filenames of the samples in the labeled set, List[str]
-        unlabeled_set:
-            The filenames of the samples in the unlabeled set, List[str]
+        TODO
 
     Examples:
+        TODO: rework
         >>> # set the token and dataset id
         >>> token = '123'
         >>> dataset_id = 'XYZ'
@@ -49,63 +43,123 @@ class ActiveLearningAgent:
 
     """
 
-    def __init__(self, api_workflow_client: ApiWorkflowClient, query_tag_name: str = None,
+    def __init__(self,
+                 api_workflow_client: ApiWorkflowClient,
+                 query_tag_name: str = None,
                  preselected_tag_name: str = None):
 
         self.api_workflow_client = api_workflow_client
+
+        # set the query_tag_id and preselected_tag_id
+        self._query_tag_id = None
+        self._preselected_tag_id = None
         if query_tag_name is not None or preselected_tag_name is not None:
-            tag_name_id_dict = dict([tag.name, tag.id] for tag in self.api_workflow_client._get_all_tags())
-            if preselected_tag_name is not None:
-                self.preselected_tag_id = tag_name_id_dict[preselected_tag_name]
+            # build lookup table for tag_name to tag_id
+            tag_name_id_dict = {}
+            for tag in self.api_workflow_client._get_all_tags():
+                tag_name_id_dict[tag.name] = tag.id
+            # use lookup table to set ids
             if query_tag_name is not None:
-                self.query_tag_id = tag_name_id_dict[query_tag_name]
+                self._query_tag_id = tag_name_id_dict[query_tag_name]
+            if preselected_tag_name is not None:
+                self._preselected_tag_id = tag_name_id_dict[preselected_tag_name]
 
-        if not hasattr(self, "preselected_tag_id"):
-            self.preselected_tag_id = None
-        if not hasattr(self, "query_tag_id"):
-            self.query_tag_id = None
-        self._set_labeled_and_unlabeled_set()
+        # set the filename lists based on preselected and query tag
+        self._query_tag_bitmask = self._get_query_tag_bitmask()
+        self._preselected_tag_bitmask = self._get_preselected_tag_bitmask()
+        # keep track of the last preselected tag to compute added samples
+        self._old_preselected_tag_bitmask = None
 
-    def _set_labeled_and_unlabeled_set(self, preselected_tag_data: TagData = None):
-        """Sets the labeled and unlabeled set based on the preselected and query tag id
 
-        It loads the bitmaks for the both tag_ids from the server and then
-        extracts the filenames from it given the mapping on the server.
-
-        Args:
-            preselected_tag_data:
-                optional param, then it must not be loaded from the API
+    def _get_query_tag_bitmask(self):
+        """Initializes the query tag bitmask.
 
         """
-
-        if not hasattr(self, "bitmask_labeled_set"):
-            self.bitmask_labeled_set = BitMask.from_hex("0x0")  # empty labeled set
-            self.bitmask_added_set = BitMask.from_hex("0x0")  # empty added set
-        if self.preselected_tag_id is not None:  # else the default values (empty labeled and added set) are kept
-            if preselected_tag_data is None:  # if it is not passed as argument, it must be loaded from the API
-                preselected_tag_data = self.api_workflow_client.tags_api.get_tag_by_tag_id(
-                    self.api_workflow_client.dataset_id, tag_id=self.preselected_tag_id)
-            new_bitmask_labeled_set = BitMask.from_hex(preselected_tag_data.bit_mask_data)
-            self.bitmask_added_set = new_bitmask_labeled_set - self.bitmask_labeled_set
-            self.bitmask_labeled_set = new_bitmask_labeled_set
-
-        if self.query_tag_id is None:
-            bitmask_query_tag = BitMask.from_length(len(self.api_workflow_client.filenames_on_server))
+        if self._query_tag_id is None:
+            # if not specified, all samples belong to the query tag
+            query_tag_bitmask = BitMask.from_length(
+                len(self.api_workflow_client.filenames_on_server)
+            )
         else:
+            # get query tag from api and set bitmask accordingly
             query_tag_data = self.api_workflow_client.tags_api.get_tag_by_tag_id(
-                self.api_workflow_client.dataset_id, tag_id=self.query_tag_id)
-            bitmask_query_tag = BitMask.from_hex(query_tag_data.bit_mask_data)
-        self.bitmask_unlabeled_set = bitmask_query_tag - self.bitmask_labeled_set
+                self.api_workflow_client.dataset_id,
+                tag_id=self._query_tag_id
+            )
+            query_tag_bitmask = BitMask.from_hex(query_tag_data.bit_mask_data)
 
-        self.labeled_set = self.bitmask_labeled_set.masked_select_from_list(self.api_workflow_client.filenames_on_server)
-        self.added_set = self.bitmask_added_set.masked_select_from_list(self.api_workflow_client.filenames_on_server)
-        self.unlabeled_set = self.bitmask_unlabeled_set.masked_select_from_list(self.api_workflow_client.filenames_on_server)
+        return query_tag_bitmask
 
-    def query(self, sampler_config: SamplerConfig, al_scorer: Scorer = None) -> Tuple[List[str], List[str]]:
+    def _get_preselected_tag_bitmask(self):
+        """Initializes the preselected tag bitmask.
+
+        """
+        if self._preselected_tag_id is None:
+            # if not specified, no samples belong to the preselected tag
+            preselected_tag_bitmask = BitMask.from_hex('0x0')
+        else:
+            # get preselected tag from api and set bitmask accordingly
+            preselected_tag_data = self.api_workflow_client.tags_api.get_tag_by_tag_id(
+                self.api_workflow_client.dataset_id,
+                tag_id=self._preselected_tag_id
+            )
+            preselected_tag_bitmask = BitMask.from_hex(preselected_tag_data.bit_mask_data)
+
+        return preselected_tag_bitmask
+
+    @property
+    def query_set(self):
+        """List of filenames for which to calculate active learning scores.
+
+        """
+        return self._query_tag_bitmask.masked_select_from_list(
+            self.api_workflow_client.filenames_on_server
+        )
+
+    @property
+    def labeled_set(self):
+        """List of filenames indicating selected samples.
+
+        """
+        return self._preselected_tag_bitmask.masked_select_from_list(
+            self.api_workflow_client.filenames_on_server
+        )
+
+    @property
+    def unlabeled_set(self):
+        """List of filenames which belong to the query set but are not selected.
+
+        """
+        # unlabeled set is the query set minus the preselected set
+        unlabeled_tag_bitmask = self._query_tag_bitmask - self._preselected_tag_bitmask
+        return unlabeled_tag_bitmask.masked_select_from_list(
+            self.api_workflow_client.filenames_on_server
+        )
+
+    @property
+    def added_set(self):
+        """List of filenames of newly added samples (in the last query).
+
+        Raises:
+            RuntimeError if executed before a query.
+
+        """
+        # the added set only exists after a query
+        if self._old_preselected_tag_bitmask is None:
+            raise RuntimeError('Cannot compute \"added set\" before querying.')
+        # added set is new preselected set minus the old one
+        added_tag_bitmask = self._preselected_tag_bitmask - self._old_preselected_tag_bitmask
+        return added_tag_bitmask.masked_select_from_list(
+            self.api_workflow_client.filenames_on_server
+        )
+
+
+    def query(self,
+              sampler_config: SamplerConfig,
+              al_scorer: Scorer = None) -> Tuple[List[str], List[str]]:
         """Performs an active learning query.
 
-        As part of it, the self.labeled_set and self.unlabeled_set are updated
-        and can be used for the next step.
+        TODO: what happens here?
 
         Args:
             sampler_config:
@@ -120,36 +174,40 @@ class ActiveLearningAgent:
             to form the new labeled_set.
 
         """
-        # check input
+
+        # handle illogical stopping condition
         if sampler_config.n_samples < len(self.labeled_set):
-            warnings.warn("ActiveLearningAgent.query: The number of samples which should be sampled "
-                          "including the current labeled set "
-                          "(sampler_config.n_samples) "
-                          "is smaller than the number of samples in the current labeled set."
-                          "Skipping the sampling and returning the previous labeled set.")
+            warnings.warn(
+                f'ActiveLearningAgent.query: The number of samples ({sampler_config.n_samples}) is '
+                f'smaller than the number of preselected samples ({len(self.labeled_set)}).'
+                'Skipping the active learning query and returning the previous labeled set.'
+            )
             return self.labeled_set, []
 
-        # calculate scores
+        # calculate active learning scores
+        scores_dict = None
         if al_scorer is not None:
-            no_unlabeled_samples = len(self.unlabeled_set)
-            no_samples_with_predictions = len(al_scorer.model_output)
-            if no_unlabeled_samples != no_samples_with_predictions:
-                raise ValueError(f"The scorer must have exactly as many samples as in the unlabeled set,"
-                                 f"but there are {no_samples_with_predictions} predictions in the scorer,"
-                                 f"but {no_unlabeled_samples} in the unlabeled set.")
+            no_query_samples = len(self.query_set)
+            no_query_samples_with_scores = len(al_scorer.model_output)
+            if no_query_samples != no_query_samples_with_scores:
+                raise ValueError(
+                    f'Number of query samples ({no_query_samples}) must match '
+                    f'the number of predictions ({no_query_samples_with_scores})!'
+                )
             scores_dict = al_scorer.calculate_scores()
-        else:
-            scores_dict = None
 
         # perform the sampling
         new_tag_data = self.api_workflow_client.sampling(
             sampler_config=sampler_config,
             al_scores=scores_dict,
-            preselected_tag_id=self.preselected_tag_id,
-            query_tag_id=self.query_tag_id)
+            preselected_tag_id=self._preselected_tag_id,
+            query_tag_id=self._query_tag_id
+        )
 
-        # set the newly chosen tag as the new preselected_tag_id and update the sets
-        self.preselected_tag_id = new_tag_data.id
-        self._set_labeled_and_unlabeled_set(new_tag_data)
+        # update the old preselected_tag
+        self._old_preselected_tag_bitmask = self._preselected_tag_bitmask
+        # set the newly chosen tag as the new preselected_tag
+        self._preselected_tag_id = new_tag_data.id
+        self._preselected_tag_bitmask = self._get_preselected_tag_bitmask()
 
         return self.labeled_set, self.added_set
