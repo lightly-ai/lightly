b301a137ac38a3c180fda16dabfb4fde19133ded
philippmwirth <philipp.m.wirth@gmail.com>
2021-04-23 06:16:40 +0000

    Add and adapt tests for active learning agent

--- tests/active_learning/test_active_learning_agent.py
+++ tests/active_learning/test_active_learning_agent.py
@@ -30,7 +30,7 @@ class TestActiveLearningAgent(MockedApiWorkflowSetup):
                         sampler_config = SamplerConfig(n_samples=n_samples, method=method)
 
                     if sampler_config.method == SamplingMethod.CORAL:
-                        predictions = np.random.rand(len(agent.unlabeled_set), 10).astype(np.float32)
+                        predictions = np.random.rand(len(agent.query_set), 10).astype(np.float32)
                         predictions_normalized = predictions / np.sum(predictions, axis=1)[:, np.newaxis]
                         al_scorer = ScorerClassification(predictions_normalized)
                         labeled_set, added_set = agent.query(sampler_config=sampler_config, al_scorer=al_scorer)
@@ -39,22 +39,54 @@ class TestActiveLearningAgent(MockedApiWorkflowSetup):
                         labeled_set, added_set = agent.query(sampler_config=sampler_config)
 
                     self.assertEqual(n_old_labeled + len(added_set), len(labeled_set))
-                    assert set(added_set).issubset(labeled_set)
+                    self.assertTrue(set(added_set).issubset(labeled_set))
                     self.assertEqual(len(list(set(agent.labeled_set) & set(agent.unlabeled_set))), 0)
                     self.assertEqual(n_old_unlabeled - len(added_set), len(agent.unlabeled_set))
 
-    def test_agent_wrong_scores(self):
+    def test_agent_wrong_number_of_scores(self):
         self.api_workflow_client.embedding_id = "embedding_id_xyz"
 
         agent = ActiveLearningAgent(self.api_workflow_client, preselected_tag_name="preselected_tag_name_xyz")
         method = SamplingMethod.CORAL
         n_samples = len(agent.labeled_set) + 2
 
-        n_predictions = len(agent.unlabeled_set) - 3  # the -3 should cause en error
+        n_predictions = len(agent.query_set) - 3 # the -3 should cause en error
         predictions = np.random.rand(n_predictions, 10).astype(np.float32)
         predictions_normalized = predictions / np.sum(predictions, axis=1)[:, np.newaxis]
         al_scorer = ScorerClassification(predictions_normalized)
 
         sampler_config = SamplerConfig(n_samples=n_samples, method=method)
         with self.assertRaises(ValueError):
-            labeled_set, added_set = agent.query(sampler_config=sampler_config, al_scorer=al_scorer)
+            agent.query(sampler_config=sampler_config, al_scorer=al_scorer)
+
+    def test_agent_added_set_before_query(self):
+
+        self.api_workflow_client.embedding_id = "embedding_id_xyz"
+        agent = ActiveLearningAgent(
+            self.api_workflow_client,
+            preselected_tag_name="preselected_tag_name_xyz"
+        )
+
+        agent.query_set
+        agent.labeled_set
+        agent.unlabeled_set
+        with self.assertRaises(RuntimeError):
+            agent.added_set
+
+    def test_agent_query_too_few(self):
+
+        self.api_workflow_client.embedding_id = "embedding_id_xyz"
+        agent = ActiveLearningAgent(
+            self.api_workflow_client,
+            preselected_tag_name="preselected_tag_name_xyz",
+        )
+
+        # sample 0 samples
+        sampler_config = SamplerConfig(
+            n_samples=0,
+            method=SamplingMethod.RANDOM
+        )
+
+        labeled, added = agent.query(sampler_config)
+
+        self.assertListEqual(added, [])
