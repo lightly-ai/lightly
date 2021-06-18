1c928ec0c2cd669485d880d81dc6e79f14232c33
MalteEbner <malte.ebner@gmail.com>
2021-06-18 17:26:30 +0200

    prevent test_api_latency from being called by github actions

--- tests/UNMOCKED_end2end_tests/test_api_latency.py
+++ tests/UNMOCKED_end2end_tests/test_api_latency.py
@@ -7,23 +7,24 @@ from tqdm import tqdm
 from lightly.api import ApiWorkflowClient
 from lightly.openapi_generated.swagger_client import Configuration, ApiClient, QuotaApi
 
-token = os.getenv("TOKEN")
-api_client = ApiWorkflowClient(token=token).api_client
-quota_api = QuotaApi(api_client)
+if __name__ == "__main__":
+    token = os.getenv("TOKEN")
+    api_client = ApiWorkflowClient(token=token).api_client
+    quota_api = QuotaApi(api_client)
 
-no_iters = 200
+    no_iters = 200
 
-latencies = np.zeros(no_iters)
-for i in tqdm(range(no_iters)):
-    start = time.time()
-    quota_api.get_quota_maximum_dataset_size()
-    duration = time.time()-start
-    latencies[i] = duration
+    latencies = np.zeros(no_iters)
+    for i in tqdm(range(no_iters)):
+        start = time.time()
+        quota_api.get_quota_maximum_dataset_size()
+        duration = time.time()-start
+        latencies[i] = duration
 
-def format_latency(latency: float):
-    return f"{latency*1000:.1f}ms"
+    def format_latency(latency: float):
+        return f"{latency*1000:.1f}ms"
 
-values = [('min')]
-print(f"Latencies: min: {format_latency(np.min(latencies))}, mean: {format_latency(np.mean(latencies))}, max: {format_latency(np.max(latencies))}")
-print(f"\nPINGING TO GOOGLE")
-response = os.system("ping -c 1 " + "google.com")
+    values = [('min')]
+    print(f"Latencies: min: {format_latency(np.min(latencies))}, mean: {format_latency(np.mean(latencies))}, max: {format_latency(np.max(latencies))}")
+    print(f"\nPINGING TO GOOGLE")
+    response = os.system("ping -c 1 " + "google.com")
