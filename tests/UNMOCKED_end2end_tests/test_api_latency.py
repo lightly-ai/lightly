bd73b9c4680069c4eee806f865e0588ffdfb0271
MalteEbner <malte.ebner@gmail.com>
2021-06-18 17:55:58 +0200

    created unmocked scripts to test the api latency and upload performance (#415)
    * created unmocked scripts to test the api latency and upload performance

---
+++ tests/UNMOCKED_end2end_tests/test_api_latency.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+import numpy as np
+from tqdm import tqdm
+
+from lightly.api import ApiWorkflowClient
+from lightly.openapi_generated.swagger_client import Configuration, ApiClient, QuotaApi
+
+if __name__ == "__main__":
+    token = os.getenv("TOKEN")
+    api_client = ApiWorkflowClient(token=token).api_client
+    quota_api = QuotaApi(api_client)
+
+    n_iters = 200
+
+    latencies = np.zeros(n_iters)
+    for i in tqdm(range(n_iters)):
+        start = time.time()
+        quota_api.get_quota_maximum_dataset_size()
+        duration = time.time()-start
+        latencies[i] = duration
+
+    def format_latency(latency: float):
+        return f"{latency*1000:.1f}ms"
+
+    values = [('min')]
+    print(f"Latencies: min: {format_latency(np.min(latencies))}, mean: {format_latency(np.mean(latencies))}, max: {format_latency(np.max(latencies))}")
+    print(f"\nPINGING TO GOOGLE")
+    response = os.system("ping -c 1 " + "google.com")
