e45827a0a797b37aa1a67b742a8ac770a31dc3f8
MalteEbner <malte.ebner@gmail.com>
2021-08-05 17:10:04 +0200

    refactored cropping code to work without yolo labels

--- lightly/cli/crop_cli.py
+++ lightly/cli/crop_cli.py
@@ -8,17 +8,16 @@ command-line interface.
 # Copyright (c) 2020. Lightly AG and its affiliates.
 # All Rights Reserved
 import os.path
-from pathlib import Path
 from typing import List
 
 import hydra
 import yaml
 from PIL.Image import Image
 from torch.utils.hipify.hipify_python import bcolors
-from tqdm import tqdm
 
+from lightly.active_learning.utils import BoundingBox
 from lightly.cli._helpers import fix_input_path
-from lightly.utils.cropping.crop_image_by_bounding_boxes import crop_image_by_bounding_boxes
+from lightly.utils.cropping.crop_image_by_bounding_boxes import crop_dataset_by_bounding_boxes_and_save
 from lightly.utils.cropping.read_yolo_label_file import read_yolo_label_file
 from lightly.data import LightlyDataset
 
@@ -45,31 +44,24 @@ def _crop_cli(cfg, is_cli_call=True):
 
 
     dataset = LightlyDataset(input_dir)
-    filenames_images = dataset.get_filenames()
-    cropped_images_list_list: List[List[Image]] = []
-    print(f"Cropping objects out of {len(filenames_images)} images...")
-    for filename_image in tqdm(filenames_images):
-        filepath_image = dataset.get_filepath_from_filename(filename_image)
-        filepath_image_base, image_extension = os.path.splitext(filepath_image)
+
+
+    class_indices_list_list: List[List[int]] = []
+    bounding_boxes_list_list: List[List[BoundingBox]] = []
+
+    # YOLO-Specific
+    for filename_image in dataset.get_filenames():
+        filepath_image_base, image_extension = os.path.splitext(filename_image)
         filepath_label = os.path.join(label_dir, filename_image).replace(image_extension, '.txt')
-        filepath_out_dir = os.path.join(output_dir, filename_image).replace(image_extension, '')
-        Path(filepath_out_dir).mkdir(parents=True, exist_ok=True)
-
         class_indices, bounding_boxes = read_yolo_label_file(filepath_label, float(cfg['crop_padding']))
-        cropped_images = crop_image_by_bounding_boxes(filepath_image, bounding_boxes)
-        cropped_images_list_list.append(cropped_images)
-        for index, (class_index, cropped_image) in enumerate((zip(class_indices, cropped_images))):
-            if class_names:
-                class_name = class_names[class_index]
-            else:
-                class_name = f"class{class_index}"
-            cropped_image_filename = os.path.join(filepath_out_dir, f'{index}_{class_name}{image_extension}')
-            cropped_image.save(cropped_image_filename)
+        class_indices_list_list.append(class_indices)
+        bounding_boxes_list_list.append(bounding_boxes)
 
+    cropped_images_list_list = \
+        crop_dataset_by_bounding_boxes_and_save(dataset, output_dir, bounding_boxes_list_list, class_indices_list_list, class_names)
 
     print(f'Cropped images are stored at: {bcolors.OKBLUE}{output_dir}{bcolors.ENDC}')
     return cropped_images_list_list
-
 
 
 @hydra.main(config_path="config", config_name="config")
--- lightly/utils/cropping/crop_image_by_bounding_boxes.py
+++ lightly/utils/cropping/crop_image_by_bounding_boxes.py
@@ -1,8 +1,13 @@
+import os.path
+import warnings
+from pathlib import Path
 from typing import List
 
 from PIL import Image
+from tqdm import tqdm
 
 from lightly.active_learning.utils import BoundingBox
+from lightly.data import LightlyDataset
 
 
 def crop_image_by_bounding_boxes(image_filepath: str, bounding_boxes: List[BoundingBox]) -> List[Image.Image]:
@@ -15,3 +20,71 @@ def crop_image_by_bounding_boxes(image_filepath: str, bounding_boxes: List[Bound
         cropped_image = image.crop(crop_box)
         cropped_images.append(cropped_image)
     return cropped_images
+
+
+def crop_dataset_by_bounding_boxes_and_save(dataset: LightlyDataset,
+                                            output_dir: str,
+                                            bounding_boxes_list_list: List[List[BoundingBox]],
+                                            class_indices_list_list: List[List[int]],
+                                            class_names: List[str] = None
+                                            ) -> List[List[str]]:
+    """Crops all images in a dataset by the bounding boxes and saves them in the output dir
+
+    Args:
+        dataset:
+            The dataset with the images to be cropped. Must contain M images.
+        output_dir:
+            The output directory to saved the cropped images to.
+        bounding_boxes_list_list:
+            The bounding boxes of the detections for each image. Must have M sublists, one for each image.
+            Each sublist contains the bounding boxes for each detection, thus N_m elements.
+        class_indices_list_list:
+            The object class ids of the detections for each image. Must have M sublists, one for each image.
+            Each sublist contains the bounding boxes for each detection, thus N_m elements.
+        class_names:
+            The names of the classes, used to map the class id to the class name.
+
+
+    Returns:
+        The filepaths to all saved cropped images. Has M sublists, one for each image.
+        Each sublist contains the filepath of the crop each detection, thus N_m elements.
+
+    """
+    filenames_images = dataset.get_filenames()
+    if len(filenames_images) != len(bounding_boxes_list_list) or len(filenames_images) != len(class_indices_list_list):
+        raise ValueError("There must be one bounding box and class index list for each image in the datasets,"
+                         "but the lengths dont align.")
+
+    cropped_image_filepath_list_list: List[List[Image]] = []
+
+
+    print(f"Cropping objects out of {len(filenames_images)} images...")
+    for filename_image, class_indices, bounding_boxes in \
+            tqdm(zip(filenames_images, class_indices_list_list, bounding_boxes_list_list)):
+
+        if not len(class_indices) == len(bounding_boxes):
+            warnings.warn(f"Length of class indices ({len(class_indices)} does not equal length of bounding boxes"
+                          f"({len(bounding_boxes)}. This is an error in the input arguments. "
+                          f"Skipping this image {filename_image}.")
+            continue
+
+        filepath_image = dataset.get_filepath_from_filename(filename_image)
+        filepath_image_base, image_extension = os.path.splitext(filepath_image)
+
+        filepath_out_dir = os.path.join(output_dir, filename_image).replace(image_extension, '')
+        Path(filepath_out_dir).mkdir(parents=True, exist_ok=True)
+
+        cropped_images = crop_image_by_bounding_boxes(filepath_image, bounding_boxes)
+        cropped_images_filepaths = []
+        for index, (class_index, cropped_image) in enumerate((zip(class_indices, cropped_images))):
+            if class_names:
+                class_name = class_names[class_index]
+            else:
+                class_name = f"class{class_index}"
+            cropped_image_filepath = os.path.join(filepath_out_dir, f'{index}_{class_name}{image_extension}')
+            cropped_images_filepaths.append(cropped_image_filepath)
+            cropped_image.save(cropped_image_filepath)
+
+        cropped_image_filepath_list_list.append(cropped_images_filepaths)
+
+    return cropped_image_filepath_list_list
