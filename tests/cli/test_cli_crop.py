60a97a8263d5aa7503f08b6dadc7372357bba2a9
MalteEbner <malte.ebner@gmail.com>
2021-08-09 18:54:28 +0200

    more docstrings and tests

--- tests/cli/test_cli_crop.py
+++ tests/cli/test_cli_crop.py
@@ -9,6 +9,10 @@ import yaml
 from hydra.experimental import compose, initialize
 
 import lightly
+from lightly.active_learning.utils import BoundingBox
+from lightly.data import LightlyDataset
+from lightly.utils.cropping.crop_image_by_bounding_boxes import crop_dataset_by_bounding_boxes_and_save
+from lightly.utils.cropping.read_yolo_label_file import read_yolo_label_file
 from tests.api_workflow.mocked_api_workflow_client import MockedApiWorkflowSetup, MockedApiWorkflowClient
 
 
@@ -54,7 +58,7 @@ class TestCLICrop(MockedApiWorkflowSetup):
             path = os.path.join(self.folder_path_labels, filename_label)
             with open(path, 'a') as the_file:
                 for i in range(objects_per_image):
-                    class_id = random.randint(0, no_classes-1)
+                    class_id = random.randint(0, no_classes - 1)
                     x = random.uniform(0.1, 0.9)
                     y = random.uniform(0.1, 0.9)
                     w = random.uniform(0.1, 1.0)
@@ -81,6 +85,43 @@ class TestCLICrop(MockedApiWorkflowSetup):
         cli_string = "lightly-crop label_dir=/blub"
         self.parse_cli_string(cli_string)
         self.assertEqual(self.cfg['label_dir'], '/blub')
+
+    def test_read_yolo(self):
+        for f in os.listdir(self.cfg.label_dir):
+            if f.endswith('.txt'):
+                filepath = os.path.join(self.cfg.label_dir, f)
+                read_yolo_label_file(filepath, 0.1)
+
+    def test_crop_dataset_by_bounding_boxes_and_save(self):
+        dataset = LightlyDataset(self.cfg.input_dir)
+        output_dir = self.cfg.output_dir
+        no_files = len(dataset.get_filenames())
+        bounding_boxes_list_list = [[BoundingBox(0, 0, 1, 1)]] * no_files
+        class_indices_list_list = [[1]] * no_files
+        class_names = ["class_0", "class_1"]
+        with self.subTest("all_correct"):
+            crop_dataset_by_bounding_boxes_and_save(dataset,
+                                                    output_dir,
+                                                    bounding_boxes_list_list,
+                                                    class_indices_list_list,
+                                                    class_names)
+        with self.subTest("wrong length of bounding_boxes_list_list"):
+            with self.assertRaises(ValueError):
+                crop_dataset_by_bounding_boxes_and_save(dataset,
+                                                        output_dir,
+                                                        bounding_boxes_list_list[:-1],
+                                                        class_indices_list_list,
+                                                        class_names)
+        with self.subTest("wrong internal length of class_indices_list_list"):
+            with self.assertRaises(ValueError):
+                class_indices_list_list[0] *= 2
+                crop_dataset_by_bounding_boxes_and_save(dataset,
+                                                        output_dir,
+                                                        bounding_boxes_list_list[:-1],
+                                                        class_indices_list_list,
+                                                        class_names)
+
+
 
     def test_crop_with_class_names(self):
         cli_string = "lightly-crop crop_padding=0.1"
