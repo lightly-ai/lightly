3ccb0134fbd5970f75144d201bb822b62a6861d6
philippmwirth <philipp.m.wirth@gmail.com>
2021-08-25 14:41:34 +0000

    Add projection heads to init

--- lightly/models/modules/__init__.py
+++ lightly/models/modules/__init__.py
@@ -8,4 +8,12 @@ can be combined with any lightly model.
 # Copyright (c) 2021. Lightly AG and its affiliates.
 # All Rights Reserved
 
+from lightly.models.modules.heads import BarlowTwinsProjectionHead
+from lightly.models.modules.heads import BYOLProjectionHead
+from lightly.models.modules.heads import MoCoProjectionHead
+from lightly.models.modules.heads import NNCLRProjectionHead
+from lightly.models.modules.heads import NNCLRPredictionHead
+from lightly.models.modules.heads import SimCLRProjectionHead
+from lightly.models.modules.heads import SimSiamProjectionHead
+from lightly.models.modules.heads import SimSiamPredictionHead
 from lightly.models.modules.nn_memory_bank import NNMemoryBankModule
