7182d7c2dbb3e7c4b1bca8722cb869894a6b2257
Shikhar Mohan <42655650+shikharmn@users.noreply.github.com>
2021-08-30 09:23:14 +0530

    Add Jigsaw augmentation

---
+++ lightly/transforms/jigsaw.py
@@ -0,0 +1,52 @@
+""" Jigsaw Crop """
+
+# Copyright (c) 2021. Lightly AG and its affiliates.
+# All Rights Reserved
+
+import numpy as np
+from PIL import ImageOps
+
+
+class Jigsaw(object):
+    """Implementation of Jigsaw image augmentation, inspired from PyContrast library.
+    
+    Generates n_grid**2 random crops and returns a list.
+    
+    This augmentation is instrumental to PIRL.
+    
+    Attributes:
+        n_grid:
+            Side length of the meshgrid, sqrt of the number of crops.
+        img_size:
+            Size of image.
+        crop_size:
+            Size of crops.
+    """
+    def __init__(self, n_grid=3, img_size=255, crop_size=64):
+        self.n_grid = n_grid
+        self.img_size = img_size
+        self.crop_size = crop_size
+        self.grid_size = int(img_size / self.n_grid)
+        self.side = self.grid_size - self.crop_size
+
+        yy, xx = np.meshgrid(np.arange(n_grid), np.arange(n_grid))
+        self.yy = np.reshape(yy * self.grid_size, (n_grid * n_grid,))
+        self.xx = np.reshape(xx * self.grid_size, (n_grid * n_grid,))
+
+    def __call__(self, img):
+        """Performs the Jigsaw augmentation
+        Args:
+            img:
+                PIL image to perform Jigsaw augmentation on
+        Returns:
+            List of cropped PIL images
+        """
+        r_x = np.random.randint(0, self.side + 1, self.n_grid * self.n_grid)
+        r_y = np.random.randint(0, self.side + 1, self.n_grid * self.n_grid)
+        img = np.asarray(img, np.uint8)
+        crops = []
+        for i in range(self.n_grid * self.n_grid):
+            crops.append(img[self.xx[i] + r_x[i]: self.xx[i] + r_x[i] + self.crop_size,
+                         self.yy[i] + r_y[i]: self.yy[i] + r_y[i] + self.crop_size, :])
+        crops = [Image.fromarray(crop) for crop in crops]
+        return crops
