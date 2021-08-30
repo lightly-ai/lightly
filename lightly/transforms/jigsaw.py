ca2cf9b131f2c4e5e3100a94fb3ab2fe641960fe
Shikhar Mohan <shikhar_mn@iitkgp.ac.in>
2021-08-30 22:20:38 +0530

    Minor Changes to Jigsaw
    Jigsaw crop outputs torch tensor now instead of list.

--- lightly/transforms/jigsaw.py
+++ lightly/transforms/jigsaw.py
@@ -3,8 +3,10 @@
 # Copyright (c) 2021. Lightly AG and its affiliates.
 # All Rights Reserved
 
+import torch
+from torchvision import transforms
 import numpy as np
-from PIL import ImageOps
+from PIL import Image
 
 
 class Jigsaw(object):
@@ -21,13 +23,16 @@ class Jigsaw(object):
             Size of image.
         crop_size:
             Size of crops.
+        transform:
+            Transformation to apply on each crop.
     """
-    def __init__(self, n_grid=3, img_size=255, crop_size=64):
+    def __init__(self, n_grid=3, img_size=255, crop_size=64, transform=transforms.ToTensor()):
         self.n_grid = n_grid
         self.img_size = img_size
         self.crop_size = crop_size
         self.grid_size = int(img_size / self.n_grid)
         self.side = self.grid_size - self.crop_size
+        self.transform = transform
 
         yy, xx = np.meshgrid(np.arange(n_grid), np.arange(n_grid))
         self.yy = np.reshape(yy * self.grid_size, (n_grid * n_grid,))
@@ -39,7 +44,7 @@ class Jigsaw(object):
             img:
                 PIL image to perform Jigsaw augmentation on
         Returns:
-            List of cropped PIL images
+            Torch tensor with stacked crops.
         """
         r_x = np.random.randint(0, self.side + 1, self.n_grid * self.n_grid)
         r_y = np.random.randint(0, self.side + 1, self.n_grid * self.n_grid)
@@ -49,4 +54,5 @@ class Jigsaw(object):
             crops.append(img[self.xx[i] + r_x[i]: self.xx[i] + r_x[i] + self.crop_size,
                          self.yy[i] + r_y[i]: self.yy[i] + r_y[i] + self.crop_size, :])
         crops = [Image.fromarray(crop) for crop in crops]
+        crops = torch.stack([self.transform(crop) for crop in crops])
         return crops
