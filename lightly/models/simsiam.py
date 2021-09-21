50aca93d99c2cc72ec66cbb26c83643ba9e11e22
IgorSusmelj <igor_7779@hotmail.com>
2021-09-21 09:57:16 +0200

    Add deprecation warning (#500)
    * Add deprecation warning
    
    * Fix warning message
    
    * Add deprecation warning
    
    * Add hint about deprecation

--- lightly/models/simsiam.py
+++ lightly/models/simsiam.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2020. Lightly AG and its affiliates.
 # All Rights Reserved
 
+import warnings
+
 import torch
 import torch.nn as nn
 
@@ -60,6 +62,11 @@ class SimSiam(nn.Module):
             out_dim,
         )
 
+        warnings.warn(Warning(
+            'The high-level building block SimSiam will be deprecated in version 1.2.0. '
+            + 'Use low-level building blocks instead. '
+            + 'See https://docs.lightly.ai/lightly.models.html for more information'),
+            PendingDeprecationWarning)
         
     def forward(self, 
                 x0: torch.Tensor, 
