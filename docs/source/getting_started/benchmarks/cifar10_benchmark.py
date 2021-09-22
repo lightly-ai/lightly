187ff5c61ddaab43a0d093790ab7770d959846cf
IgorSusmelj <igor_7779@hotmail.com>
2021-09-22 20:54:46 +0000

    Add new low-level moco and byol models

--- docs/source/getting_started/benchmarks/cifar10_benchmark.py
+++ docs/source/getting_started/benchmarks/cifar10_benchmark.py
@@ -23,7 +23,8 @@ Code to reproduce the benchmark results:
 | SimSiam |  800   | 512        | 0.91          | 6.9 GBytes     |
 
 """
-from lightly.models.modules.heads import ProjectionHead
+from lightly.models.modules.heads import BYOLProjectionHead, ProjectionHead, MoCoProjectionHead
+from lightly.models.modules.momentum import deactivate_requires_grad, update_momentum, batch_shuffle, batch_unshuffle
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -33,6 +34,7 @@ import numpy as np
 import pytorch_lightning as pl
 import lightly
 from lightly.utils import BenchmarkModule
+import copy
 
 num_workers = 8
 memory_bank_size = 4096
@@ -139,40 +141,64 @@ def get_data_loaders(batch_size: int):
     )
 
     return dataloader_train_ssl, dataloader_train_kNN, dataloader_test
-    
+
 
 class MocoModel(BenchmarkModule):
     def __init__(self, dataloader_kNN, num_classes):
         super().__init__(dataloader_kNN, num_classes)
+
         # create a ResNet backbone and remove the classification head
         resnet = lightly.models.ResNetGenerator('resnet-18', num_splits=8)
         self.backbone = nn.Sequential(
             *list(resnet.children())[:-1],
-            nn.AdaptiveAvgPool2d(1),
+            nn.AdaptiveAvgPool2d(1)
         )
+
         # create a moco model based on ResNet
-        self.resnet_moco = \
-            lightly.models.MoCo(self.backbone, num_ftrs=512, m=0.99, batch_shuffle=True)
+        self.projection_head = MoCoProjectionHead(512, 512, 128)
+        self.backbone_momentum = copy.deepcopy(self.backbone)
+        self.projection_head_momentum = copy.deepcopy(self.projection_head)
+        deactivate_requires_grad(self.backbone_momentum.parameters())
+        deactivate_requires_grad(self.projection_head_momentum.parameters())
+
         # create our loss with the optional memory bank
         self.criterion = lightly.loss.NTXentLoss(
             temperature=0.1,
             memory_bank_size=memory_bank_size)
             
     def forward(self, x):
-        self.resnet_moco(x)
+        x = self.backbone(x)
+        return self.projection_head(x)
 
     def training_step(self, batch, batch_idx):
         (x0, x1), _, _ = batch
+
+        # update momentum
+        update_momentum(self.backbone, self.backbone_momentum, 0.99)
+        update_momentum(self.projection_head, self.projection_head_momentum, 0.99)
+
+        def step(x0_, x1_):
+            x1_, shuffle = batch_shuffle(x1_)
+            x0_ = self.backbone(x0_).flatten(start_dim=1)
+            x0_ = self.projection_head(x0_)
+
+            x1_ = self.backbone_momentum(x1_).flatten(start_dim=1)
+            x1_ = self.projection_head_momentum(x1_)
+            x1_ = batch_unshuffle(x1_, shuffle)
+            return x0_, x1_
+
         # We use a symmetric loss (model trains faster at little compute overhead)
         # https://colab.research.google.com/github/facebookresearch/moco/blob/colab-notebook/colab/moco_cifar10_demo.ipynb
-        loss_1 = self.criterion(*self.resnet_moco(x0, x1))
-        loss_2 = self.criterion(*self.resnet_moco(x1, x0))
+        loss_1 = self.criterion(*step(x0, x1))
+        loss_2 = self.criterion(*step(x1, x0))
+
         loss = 0.5 * (loss_1 + loss_2)
         self.log('train_loss_ssl', loss)
         return loss
 
     def configure_optimizers(self):
-        optim = torch.optim.SGD(self.resnet_moco.parameters(), lr=6e-2,
+        params = list(self.backbone.parameters()) + list(self.projection_head.parameters())
+        optim = torch.optim.SGD(params, lr=6e-2,
                                 momentum=0.9, weight_decay=5e-4)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, max_epochs)
         return [optim], [scheduler]
@@ -255,9 +281,67 @@ class SimSiamModel(BenchmarkModule):
         return [optim], [scheduler]
 
 
-model_names = ['MoCo_128', 'SimCLR_128', 'SimSiam_128',
-               'MoCo_512', 'SimCLR_512', 'SimSiam_512']
-models = [MocoModel, SimCLRModel, SimSiamModel]
+class BYOLModel(BenchmarkModule):
+    def __init__(self, dataloader_kNN, num_classes):
+        super().__init__(dataloader_kNN, num_classes)
+        # create a ResNet backbone and remove the classification head
+        resnet = torchvision.models.resnet18()
+        self.backbone = nn.Sequential(
+            *list(resnet.children())[:-1],
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        # create a byol model based on ResNet
+        self.projection_head = BYOLProjectionHead(512,1024,256)
+        self.prediction_head = BYOLProjectionHead(256,1024,256)
+
+        self.backbone_momentum = copy.deepcopy(self.backbone)
+        self.projection_head_momentum = copy.deepcopy(self.projection_head)
+
+        deactivate_requires_grad(self.backbone_momentum.parameters())
+        deactivate_requires_grad(self.projection_head_momentum.parameters())
+
+        self.criterion = lightly.loss.SymNegCosineSimilarityLoss()
+
+    def forward(self, x):
+        x = self.backbone(x)
+        return self.projection_head(x)
+
+    def training_step(self, batch, batch_idx):
+        (x0, x1), _, _ = batch
+
+        # update momentum
+        update_momentum(self.backbone, self.backbone_momentum, 0.99)
+        update_momentum(self.projection_head, self.projection_head_momentum, 0.99)
+
+        def step(x0_, x1_):
+            x0_ = self.backbone(x0_).flatten(start_dim=1)
+            x0_ = self.projection_head(x0_)
+            x0_ = self.prediction_head(x0_)
+
+            x1_ = self.backbone_momentum(x1_).flatten(start_dim=1)
+            x1_ = self.projection_head_momentum(x1_)
+            return x0_, x1_
+
+        p0, z1 = step(x0, x1)
+        p1, z0 = step(x1, x0)
+        
+        loss = self.criterion((z0, p0), (z1, p1))
+        self.log('train_loss_ssl', loss)
+        return loss
+
+    def configure_optimizers(self):
+        params = list(self.backbone.parameters()) \
+            + list(self.projection_head.parameters()) \
+            + list(self.prediction_head.parameters())
+        optim = torch.optim.SGD(params, lr=6e-2,
+                                momentum=0.9, weight_decay=5e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, max_epochs)
+        return [optim], [scheduler]
+
+model_names = ['MoCo_128', 'SimCLR_128', 'SimSiam_128', 'BYOL_128',
+               'MoCo_512', 'SimCLR_512', 'SimSiam_512', 'BYOL_512']
+models = [MocoModel, SimCLRModel, SimSiamModel, BYOLModel]
 bench_results = []
 gpu_memory_usage = []
 
