032d230ebd42d5ef62e93bc1e89bfcb865d03646
IgorSusmelj <igor_7779@hotmail.com>
2021-09-30 09:50:12 +0000

    Fix typo

--- lightly/models/__init__.py
+++ lightly/models/__init__.py
@@ -1,9 +1,9 @@
 """The lightly.models package provides model implementations.
 
 *Note that the high-level building blocks will be deprecated with 
-lightly version 1.2.0. Instead use low-level building blocks to build the
+lightly version 1.2.0. Instead, use low-level building blocks to build the
 models yourself. 
-Have a look at the benchmark code to see a reference implmentation:*
+Have a look at the benchmark code to see a reference implementation:*
 `lightly benchmarks <https://github.com/lightly-ai/lightly/tree/master/docs/source/getting_started/benchmarks>`_
 
 The package contains an implementation of the commonly used ResNet and
