0e896aadc96f3a5eb2de693548ddab3e4030190c
guarin <guarin@lightly.ai>
2021-11-19 10:04:25 +0000

    Add NegativeCosineSimilarity (#503)

---
+++ lightly/loss/negative_cosine_similarity.py
@@ -0,0 +1,45 @@
+""" Negative Cosine Similarity Loss Function """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+import torch
+from torch.nn.functional import cosine_similarity
+
+
+class NegativeCosineSimilarity(torch.nn.Module):
+    """Implementation of the Negative Cosine Simililarity used in the SimSiam[0] paper.
+
+    [0] SimSiam, 2020, https://arxiv.org/abs/2011.10566
+
+    Examples:
+
+        >>> # initialize loss function
+        >>> loss_fn = NegativeCosineSimilarity()
+        >>>
+        >>> # generate two random transforms of images
+        >>> t0 = transforms(images)
+        >>> t1 = transforms(images)
+        >>>
+        >>> # feed through a SimSiam model
+        >>> p0, p1, z0, z1 = model(t0, t1)
+        >>>
+        >>> # calculate loss
+        >>> loss = 0.5 * (loss_fn(p0, z0) + loss_fn(p1, z1))
+    """
+
+    def __init__(self, dim: int = 1, eps: float = 1e-8) -> None:
+        """Same parameters as in torch.nn.CosineSimilarity
+
+        Args:
+            dim (int, optional):
+                Dimension where cosine similarity is computed. Default: 1
+            eps (float, optional):
+                Small value to avoid division by zero. Default: 1e-8
+        """
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+
+    def forward(self, x0: torch.Tensor, x1: torch.Tensor) -> torch.Tensor:
+        return -cosine_similarity(x0, x1, self.dim, self.eps).mean()
