94898120dded039c000673a2c3043ceaabafe2cc
MalteEbner <malte.ebner@gmail.com>
2021-11-23 19:10:32 +0100

    addressed PR comments by Guarin

---
+++ tests/UNMOCKED_end2end_tests/delete_datasets_test_unmocked_cli.py
@@ -0,0 +1,18 @@
+import sys
+
+from lightly.api import ApiWorkflowClient
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 + 2:
+        num_datasets, token = \
+            (sys.argv[1 + i] for i in range(2))
+    else:
+        raise ValueError("ERROR in number of command line arguments, must be 2."
+                         "Example: python test_api 6 TOKEN")
+
+    api_workflow_client = ApiWorkflowClient(token=token)
+
+    for i in range(num_datasets):
+        dataset_name = f"test_unmocked_cli_{i}"
+        api_workflow_client.create_dataset(dataset_name)
+        api_workflow_client.delete_dataset_by_id(api_workflow_client.dataset_id)
--- tests/UNMOCKED_end2end_tests/run_all_unmocked_tests.sh
+++ tests/UNMOCKED_end2end_tests/run_all_unmocked_tests.sh
@@ -7,8 +7,13 @@ echo "token: ${TOKEN}"
 
 echo "############################### Download the clothing_dataset_small"
 
-git clone https://github.com/alexeygrigorev/clothing-dataset-small clothing_dataset_small
-INPUT_DIR="clothing_dataset_small/test/dress"
+DIR_DATASET=clothing_dataset_small
+if [ -d "DIR_DATASET" ]; then
+  echo "Skipping download of dataset, it already exists"
+else
+  git clone https://github.com/alexeygrigorev/clothing-dataset-small DIR_DATASET
+fi
+INPUT_DIR="${DIR_DATASET}/test/dress"
 
 
 # Run the tests
@@ -32,6 +37,9 @@ lightly-upload token=${TOKEN} input_dir=${INPUT_DIR} new_dataset_name=test_unmoc
 echo "############################### Test 6"
 lightly-upload token=${TOKEN} input_dir=${INPUT_DIR} new_dataset_name=test_unmocked_cli_6 upload=thumbnails
 
+echo "############################### Deleting all datasets again"
+NUMBER_OF_DATASETS=6
+python tests/UNMOCKED_end2end_tests/delete_datasets_test_unmocked_cli.py $NUMBER_OF_DATASETS $TOKEN
 
 echo "############################### Test active learning"
 INPUT_DIR="${PWD}/clothing_dataset_small/test"
