70f37d61cf7f4a03414ad61242b2dfe6f5016fec
MalteEbner <malte.ebner@gmail.com>
2021-11-23 19:10:01 +0100

    cleanup UNMOCKED_TESTS

--- tests/UNMOCKED_end2end_tests/call_benchmark_upload.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from tests.UNMOCKED_end2end_tests.benchmark_upload import benchmark_upload
-
-path_to_dataset = "/path/to/my/dataset"
-token = os.environ[f"TOKEN"]
-
-benchmark_upload(path_to_dataset=path_to_dataset, token=token)
