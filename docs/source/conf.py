65d1757e8b6cd70410f49cff11557cf04339e40c
guarin <guarin@lightly.ai>
2021-12-02 11:04:19 +0000

    Update docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Lightly AG'
 author = 'Philipp Wirth, Igor Susmelj'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.2.0'
 master_doc = 'index'
 
 
