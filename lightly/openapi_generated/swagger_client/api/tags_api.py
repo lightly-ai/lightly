27fe33cf32b263e8b1f7d4bc7a500973aad792e4
guarin <guarin@lightly.ai>
2021-12-10 13:47:51 +0000

    Update openapi

--- lightly/openapi_generated/swagger_client/api/tags_api.py
+++ lightly/openapi_generated/swagger_client/api/tags_api.py
@@ -350,6 +350,121 @@ class TagsApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def export_tag_to_label_box_data_rows(self, dataset_id, tag_id, **kwargs):  # noqa: E501
+        """export_tag_to_label_box_data_rows  # noqa: E501
+
+        Export samples of a tag as a json for importing into LabelBox as outlined here; https://docs.labelbox.com/docs/images-json ```openapi\\+warning The image URLs are special in that the resource can be accessed by anyone in posession of said URL for the time specified by the expiresIn query param ```   # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.export_tag_to_label_box_data_rows(dataset_id, tag_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param MongoObjectID dataset_id: ObjectId of the dataset (required)
+        :param MongoObjectID tag_id: ObjectId of the tag (required)
+        :param int expires_in: if defined, the URLs provided will only be valid for amount of seconds from time of issuence
+        :param bool preview_example: if true, will generate a preview example of how the structure will look
+        :param str access_control: which access control name to be used
+        :return: LabelBoxDataRows
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.export_tag_to_label_box_data_rows_with_http_info(dataset_id, tag_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.export_tag_to_label_box_data_rows_with_http_info(dataset_id, tag_id, **kwargs)  # noqa: E501
+            return data
+
+    def export_tag_to_label_box_data_rows_with_http_info(self, dataset_id, tag_id, **kwargs):  # noqa: E501
+        """export_tag_to_label_box_data_rows  # noqa: E501
+
+        Export samples of a tag as a json for importing into LabelBox as outlined here; https://docs.labelbox.com/docs/images-json ```openapi\\+warning The image URLs are special in that the resource can be accessed by anyone in posession of said URL for the time specified by the expiresIn query param ```   # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.export_tag_to_label_box_data_rows_with_http_info(dataset_id, tag_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param MongoObjectID dataset_id: ObjectId of the dataset (required)
+        :param MongoObjectID tag_id: ObjectId of the tag (required)
+        :param int expires_in: if defined, the URLs provided will only be valid for amount of seconds from time of issuence
+        :param bool preview_example: if true, will generate a preview example of how the structure will look
+        :param str access_control: which access control name to be used
+        :return: LabelBoxDataRows
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['dataset_id', 'tag_id', 'expires_in', 'preview_example', 'access_control']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method export_tag_to_label_box_data_rows" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'dataset_id' is set
+        if self.api_client.client_side_validation and ('dataset_id' not in params or
+                                                       params['dataset_id'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `dataset_id` when calling `export_tag_to_label_box_data_rows`")  # noqa: E501
+        # verify the required parameter 'tag_id' is set
+        if self.api_client.client_side_validation and ('tag_id' not in params or
+                                                       params['tag_id'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `tag_id` when calling `export_tag_to_label_box_data_rows`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'dataset_id' in params:
+            path_params['datasetId'] = params['dataset_id']  # noqa: E501
+        if 'tag_id' in params:
+            path_params['tagId'] = params['tag_id']  # noqa: E501
+
+        query_params = []
+        if 'expires_in' in params:
+            query_params.append(('expiresIn', params['expires_in']))  # noqa: E501
+        if 'preview_example' in params:
+            query_params.append(('previewExample', params['preview_example']))  # noqa: E501
+        if 'access_control' in params:
+            query_params.append(('accessControl', params['access_control']))  # noqa: E501
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['ApiKeyAuth', 'auth0Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/v1/datasets/{datasetId}/tags/{tagId}/export/LabelBoxDataRows', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='LabelBoxDataRows',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def export_tag_to_label_studio_tasks(self, dataset_id, tag_id, **kwargs):  # noqa: E501
         """export_tag_to_label_studio_tasks  # noqa: E501
 
@@ -363,6 +478,8 @@ class TagsApi(object):
         :param MongoObjectID dataset_id: ObjectId of the dataset (required)
         :param MongoObjectID tag_id: ObjectId of the tag (required)
         :param int expires_in: if defined, the URLs provided will only be valid for amount of seconds from time of issuence
+        :param bool preview_example: if true, will generate a preview example of how the structure will look
+        :param str access_control: which access control name to be used
         :return: LabelStudioTasks
                  If the method is called asynchronously,
                  returns the request thread.
@@ -387,12 +504,14 @@ class TagsApi(object):
         :param MongoObjectID dataset_id: ObjectId of the dataset (required)
         :param MongoObjectID tag_id: ObjectId of the tag (required)
         :param int expires_in: if defined, the URLs provided will only be valid for amount of seconds from time of issuence
+        :param bool preview_example: if true, will generate a preview example of how the structure will look
+        :param str access_control: which access control name to be used
         :return: LabelStudioTasks
                  If the method is called asynchronously,
                  returns the request thread.
         """
 
-        all_params = ['dataset_id', 'tag_id', 'expires_in']  # noqa: E501
+        all_params = ['dataset_id', 'tag_id', 'expires_in', 'preview_example', 'access_control']  # noqa: E501
         all_params.append('async_req')
         all_params.append('_return_http_data_only')
         all_params.append('_preload_content')
@@ -427,6 +546,10 @@ class TagsApi(object):
         query_params = []
         if 'expires_in' in params:
             query_params.append(('expiresIn', params['expires_in']))  # noqa: E501
+        if 'preview_example' in params:
+            query_params.append(('previewExample', params['preview_example']))  # noqa: E501
+        if 'access_control' in params:
+            query_params.append(('accessControl', params['access_control']))  # noqa: E501
 
         header_params = {}
 
