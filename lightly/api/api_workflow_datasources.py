6619c3b690dd1b11a7136f94bbf55c5ab4e1e3ea
guarin <guarin@lightly.ai>
2021-12-10 15:36:22 +0000

    Update openapi, use self.dataset_id

--- lightly/api/api_workflow_datasources.py
+++ lightly/api/api_workflow_datasources.py
@@ -1,7 +1,13 @@
 from typing import List
-from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data import DatasourceRawSamplesData
-from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data_row import DatasourceRawSamplesDataRow
-from lightly.openapi_generated.swagger_client.models.datasource_update_last_resource_request import DatasourceUpdateLastResourceRequest
+from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data import (
+    DatasourceRawSamplesData,
+)
+from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data_row import (
+    DatasourceRawSamplesDataRow,
+)
+from lightly.openapi_generated.swagger_client.models.datasource_processed_until_timestamp_request import (
+    DatasourceProcessedUntilTimestampRequest,
+)
 
 import time
 
@@ -11,12 +17,12 @@ def _convert_raw_data_to_list(data: List[DatasourceRawSamplesDataRow]):
 
 class _DatasourcesMixin:
 
-    def download_raw_samples(self, dataset_id, _from: int = 0, to: int = None):
+    def download_raw_samples(self, _from: int = 0, to: int = None):
         if to is None:
             to = int(time.time())
         
         response: DatasourceRawSamplesData = self._datasources_api.get_list_of_raw_samples_from_datasource_by_dataset_id(
-            dataset_id=dataset_id,
+            dataset_id=self.dataset_id,
             _from=_from,
             to=to,
         )
@@ -24,19 +30,16 @@ class _DatasourcesMixin:
         data = _convert_raw_data_to_list(response.data)
         while response.has_more:
             response: DatasourceRawSamplesData = self._datasources_api.get_list_of_raw_samples_from_datasource_by_dataset_id(
-                dataset_id=dataset_id,
-                cursor=cursor
+                dataset_id=self.dataset_id, cursor=cursor
             )
             cursor = response.cursor
             new_data = _convert_raw_data_to_list(response.data)
             data.extend(new_data)
         return data
 
-    def download_new_raw_samples(self, dataset_id):
+    def download_new_raw_samples(self):
         # get lastResourceAt
-        response = self.last_resource_at(
-            dataset_id=dataset_id
-        )
+        response = self.get_processed_until_timestamp()
 
         _from = int(response)
         to = int(time.time())
@@ -46,14 +49,15 @@ class _DatasourcesMixin:
         self.update_last_resource_at(dataset_id=dataset_id, timestamp=to)
         return data
 
-    def last_resource_at(self, dataset_id: str):
-        return self._datasources_api.get_last_resource_timestamp_of_datasource_by_dataset_id(
-            dataset_id=dataset_id
+    def get_processed_until_timestamp(self):
+        return self._datasources_api.get_datasource_processed_until_timestamp_by_dataset_id(
+            dataset_id=self.dataset_id
         )
 
-    def update_last_resource_at(self, dataset_id: str, timestamp: int):
-        body = DatasourceUpdateLastResourceRequest(last_resource_at=timestamp)
-        self._datasources_api.update_last_resource_timestamp_of_datasource_by_dataset_id(
-            dataset_id=dataset_id,
-            body=body
+    def update_processed_until_timestamp(self, timestamp: int):
+        body = DatasourceProcessedUntilTimestampRequest(
+            processed_until_timestamp=timestamp
+        )
+        self._datasources_api.update_datasource_processed_until_timestamp_by_dataset_id(
+            dataset_id=self.dataset_id, body=body
         )
