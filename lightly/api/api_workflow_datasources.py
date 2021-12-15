a30cc543721972429a29fd185d0fce78bf6f3443
guarin <guarin@lightly.ai>
2021-12-15 12:35:39 +0000

    Add docstrings and typehints

--- lightly/api/api_workflow_datasources.py
+++ lightly/api/api_workflow_datasources.py
@@ -1,23 +1,27 @@
-from typing import List
-from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data import (
-    DatasourceRawSamplesData,
-)
-from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data_row import (
-    DatasourceRawSamplesDataRow,
-)
-from lightly.openapi_generated.swagger_client.models.datasource_processed_until_timestamp_request import (
-    DatasourceProcessedUntilTimestampRequest,
-)
-
 import time
+from typing import List, Tuple
 
-def _convert_raw_data_to_list(data: List[DatasourceRawSamplesDataRow]):
-    return [(d.file_name, d.read_url) for d in data]
+from lightly.openapi_generated.swagger_client.models.datasource_processed_until_timestamp_request import DatasourceProcessedUntilTimestampRequest
+from lightly.openapi_generated.swagger_client.models.datasource_raw_samples_data import DatasourceRawSamplesData
 
 
 class _DatasourcesMixin:
 
-    def download_raw_samples(self, from_: int = 0, to: int = None):
+    def download_raw_samples(
+        self, from_: int = 0, to: int = None
+    ) -> List[Tuple[str, str]]:
+        """Downloads all filenames and read urls from the datasource between `from_` and `to`.
+
+        Samples which have timestamp == `from_` or timestamp == `to` will also be included.
+        
+        Args:
+            from_ (int): Unix timestamp from which on samples are downloaded.
+            to (int): Unix timestamp up to and including which samples are downloaded.
+        
+        Returns:
+            Samples (list(tuple)): Each tuple contains (filename, url) of the respective sample
+
+        """
         if to is None:
             to = int(time.time())
         response: DatasourceRawSamplesData = self._datasources_api.get_list_of_raw_samples_from_datasource_by_dataset_id(
@@ -26,17 +30,27 @@ class _DatasourcesMixin:
             to=to,
         )
         cursor = response.cursor
-        data = _convert_raw_data_to_list(response.data)
+        samples = response.data
         while response.has_more:
             response: DatasourceRawSamplesData = self._datasources_api.get_list_of_raw_samples_from_datasource_by_dataset_id(
                 dataset_id=self.dataset_id, cursor=cursor
             )
             cursor = response.cursor
-            new_data = _convert_raw_data_to_list(response.data)
-            data.extend(new_data)
-        return data
+            samples.extend(response.data)
+        samples = [(s.file_name, s.read_url) for s in samples]
+        return samples
 
-    def download_new_raw_samples(self):
+    def download_new_raw_samples(self) -> List[Tuple[str, str]]:
+        """Downloads filenames and read urls of unprocessed samples from the datasource.
+
+        All samples after the timestamp of `ApiWorkflowClient.get_processed_until_timestamp()` are 
+        fetched. After downloading the samples the timestamp is updated to the current time.
+        This function can be repeatedly called to retrieve new samples from the datasource.
+        
+        Returns:
+            Samples (list(tuple)): Each tuple contains (filename, url) of the respective sample
+
+        """
         response = self.get_processed_until_timestamp()
         from_ = int(response)
         
@@ -51,12 +65,22 @@ class _DatasourcesMixin:
         self.update_processed_until_timestamp(timestamp=to)
         return data
 
-    def get_processed_until_timestamp(self):
+    def get_processed_until_timestamp(self) -> int:
+        """Returns the timestamp until which samples have been processed.
+        
+        Returns:
+            timestamp (int): Unix timestamp of last processed sample
+        """
         return self._datasources_api.get_datasource_processed_until_timestamp_by_dataset_id(
             dataset_id=self.dataset_id
         )
 
-    def update_processed_until_timestamp(self, timestamp: int):
+    def update_processed_until_timestamp(self, timestamp: int) -> None:
+        """Sets the timestamp until which samples have been processed.
+        
+        Args:
+            timestamp (int): Unix timestamp of last processed sample
+        """
         body = DatasourceProcessedUntilTimestampRequest(
             processed_until_timestamp=timestamp
         )
