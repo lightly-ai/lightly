ca3fb4f0863a5d352b61542dcb5dd030af731eb8
guarin <guarin@lightly.ai>
2022-01-06 14:20:27 +0000

    Add types

--- lightly/loss/gather.py
+++ lightly/loss/gather.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 import torch.distributed as dist
 
@@ -11,15 +13,15 @@ class GatherLayer(torch.autograd.Function):
     """
 
     @staticmethod
-    def forward(ctx, input):
+    def forward(ctx, input: torch.Tensor) -> Tuple[torch.Tensor, ...]:
         ctx.save_for_backward(input)
-        output = [torch.zeros_like(input) for _ in range(dist.get_world_size())]
+        output = [torch.empty_like(input) for _ in range(dist.get_world_size())]
         dist.all_gather(output, input)
         return tuple(output)
 
     @staticmethod
-    def backward(ctx, *grads):
+    def backward(ctx, *grads: torch.Tensor) -> torch.Tensor:
         (input,) = ctx.saved_tensors
-        grad_out = torch.zeros_like(input)
+        grad_out = torch.empty_like(input)
         grad_out[:] = grads[dist.get_rank()]
         return grad_out
