b396ec5da98429111f7893d137997ccf98b67500
Philipp Wirth <65946090+philippmwirth@users.noreply.github.com>
2022-01-19 08:10:51 +0100

    Update reordering.py

--- lightly/utils/reordering.py
+++ lightly/utils/reordering.py
@@ -6,6 +6,32 @@ def sort_items_by_keys(
     items: List[any], 
     sorted_keys: List[any]
 ):
+    """Sorts the items in the same order as the sorted keys.
+
+    Args:
+        keys:
+            Keys by which items can be identified.
+        items:
+            Items to sort.
+        sorted_keys:
+            Keys in sorted order.
+
+    Returns:
+        The list of sorted items.
+
+    Examples:
+        >>> keys = [3, 2, 1]
+        >>> items = ['!', 'world', 'hello']
+        >>> sorted_keys = [1, 2, 3]
+        >>> sorted_items = sort_items_by_keys(
+        >>>     keys,
+        >>>     items,
+        >>>     sorted_keys,
+        >>> )
+        >>> print(sorted_items)
+        >>> > ['hello', 'world', '!']
+
+    """
     if len(keys) != len(items) or len(keys) != len(sorted_keys):
         raise ValueError(f"All inputs (keys,  items and sorted_keys) "
                          f"must have the same length, "
