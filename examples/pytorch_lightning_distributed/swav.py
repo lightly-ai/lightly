e9c10cc52d23aaec8039e899e7931114d298f8b3
guarin <43336610+guarin@users.noreply.github.com>
2022-01-20 11:37:00 +0100

    Guarin lig 539 multi gpu examples (#667)
    * Add distributed examples

---
+++ examples/pytorch_lightning_distributed/swav.py
@@ -0,0 +1,76 @@
+import torch
+from torch import nn
+import torchvision
+import pytorch_lightning as pl
+
+from lightly.data import LightlyDataset
+from lightly.data import SwaVCollateFunction
+from lightly.loss import SwaVLoss
+from lightly.models.modules import SwaVProjectionHead
+from lightly.models.modules import SwaVPrototypes
+
+
+class SwaV(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        resnet = torchvision.models.resnet18()
+        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.projection_head = SwaVProjectionHead(512, 512, 128)
+        self.prototypes = SwaVPrototypes(128, n_prototypes=512)
+
+        # enable sinkhorn_gather_distributed to gather features from all gpus
+        # while running the sinkhorn algorithm in the loss calculation
+        self.criterion = SwaVLoss(sinkhorn_gather_distributed=True)
+
+    def forward(self, x):
+        x = self.backbone(x).flatten(start_dim=1)
+        x = self.projection_head(x)
+        x = nn.functional.normalize(x, dim=1, p=2)
+        p = self.prototypes(x)
+        return p
+
+    def training_step(self, batch, batch_idx):
+        crops, _, _ = batch
+        multi_crop_features = [model(x.to(self.device)) for x in crops]
+        high_resolution = multi_crop_features[:2]
+        low_resolution = multi_crop_features[2:]
+        loss = self.criterion(high_resolution, low_resolution)
+        return loss
+
+    def configure_optimizers(self):
+        optim = torch.optim.Adam(model.parameters(), lr=0.001)
+        return optim
+
+
+model = SwaV()
+
+# we ignore object detection annotations by setting target_transform to return 0
+pascal_voc = torchvision.datasets.VOCDetection(
+    "datasets/pascal_voc", download=True, target_transform=lambda t: 0
+)
+dataset = LightlyDataset.from_torch_dataset(pascal_voc)
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+collate_fn = SwaVCollateFunction()
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=128,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+gpus = torch.cuda.device_count()
+
+# train with DDP and use Synchronized Batch Norm for a more accurate batch norm
+# calculation
+trainer = pl.Trainer(
+    max_epochs=10, 
+    gpus=gpus,
+    strategy='ddp',
+    sync_batchnorm=True,
+)
+trainer.fit(model=model, train_dataloaders=dataloader)
