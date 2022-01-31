3a650e933aa24f67685be726c64900e64d942633
guarin <guarin@lightly.ai>
2022-01-31 16:50:32 +0000

    Add dino head

--- tests/models/test_ProjectionHeads.py
+++ tests/models/test_ProjectionHeads.py
@@ -1,11 +1,10 @@
 import unittest
 
 import torch
-import torch.nn as nn
-
 
 from lightly.models.modules.heads import BarlowTwinsProjectionHead
 from lightly.models.modules.heads import BYOLProjectionHead
+from lightly.models.modules.heads import DINOProjectionHead
 from lightly.models.modules.heads import MoCoProjectionHead
 from lightly.models.modules.heads import NNCLRProjectionHead
 from lightly.models.modules.heads import NNCLRPredictionHead
@@ -37,13 +36,15 @@ class TestProjectionHeads(unittest.TestCase):
             SimCLRProjectionHead,
             SimSiamProjectionHead,
             SimSiamPredictionHead,
-            SwaVProjectionHead,         
+            SwaVProjectionHead,
+            DINOProjectionHead,       
         ]
 
 
-    def test_single_projection_head(self, device: str = 'cpu'):
+    def test_single_projection_head(self, device: str = 'cpu', seed=0):
         for head_cls in self.heads:
             for in_features, hidden_features, out_features in self.n_features:
+                torch.manual_seed(seed)
                 head = head_cls(in_features, hidden_features, out_features)
                 head = head.eval()
                 head = head.to(device)
@@ -59,11 +60,12 @@ class TestProjectionHeads(unittest.TestCase):
                         self.assertEqual(y.shape[1], out_features)
 
     @unittest.skipUnless(torch.cuda.is_available(), "skip")
-    def test_single_projection_head_cuda(self):
-        self.test_single_projection_head(device='cuda')
+    def test_single_projection_head_cuda(self, seed=0):
+        self.test_single_projection_head(device='cuda', seed=seed)
 
-    def test_swav_prototypes(self, device: str = 'cpu'):
+    def test_swav_prototypes(self, device: str = 'cpu', seed=0):
         for in_features, _, n_prototypes in self.n_features:
+            torch.manual_seed(seed)
             prototypes = SwaVPrototypes(in_features, n_prototypes)
             prototypes = prototypes.eval()
             prototypes = prototypes.to(device)
@@ -78,5 +80,38 @@ class TestProjectionHeads(unittest.TestCase):
                         self.assertEqual(y.shape[1], n_prototypes)
 
     @unittest.skipUnless(torch.cuda.is_available(), "skip")
-    def test_swav_prototypes_cuda(self):
-        self.test_swav_prototypes(device='cuda')
+    def test_swav_prototypes_cuda(self, seed=0):
+        self.test_swav_prototypes(device='cuda', seed=seed)
+    
+    def test_dino_projection_head(self, device="cpu", seed=0):
+        input_dim, hidden_dim, output_dim = self.n_features[0]
+        for bottleneck_dim in [8, 16, 32]:
+            for batch_norm in [False, True]:
+                for norm_last_layer in [False, True]:
+                    torch.manual_seed(seed)
+                    head = DINOProjectionHead(
+                        input_dim=input_dim,
+                        hidden_dim=hidden_dim,
+                        output_dim=output_dim,
+                        bottleneck_dim=bottleneck_dim,
+                        batch_norm=batch_norm,
+                        norm_last_layer=norm_last_layer,
+                    )
+                    head = head.eval()
+                    head = head.to(device)
+                    for batch_size in [1, 2]:
+                        msg = (
+                            f"bottleneck_dim={bottleneck_dim}, "
+                            f"batch_norm={batch_norm}, "
+                            f"norm_last_layer={norm_last_layer}"
+                        )
+                        with self.subTest(msg=msg):
+                            x = torch.torch.rand((batch_size, input_dim)).to(device)
+                            with torch.no_grad():
+                                y = head(x)
+                            self.assertEqual(y.shape[0], batch_size)
+                            self.assertEqual(y.shape[1], output_dim)
+
+    @unittest.skipUnless(torch.cuda.is_available(), "skip")
+    def test_dino_projection_head_cuda(self, seed=0):
+        self.test_dino_projection_head(device="cuda", seed=seed)
