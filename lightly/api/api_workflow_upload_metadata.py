4dd1e1b7ce12dcde8fe9c32d8d62712bed4d3e78
IgorSusmelj <igor_7779@hotmail.com>
2022-03-03 17:54:18 +0100

    Add improved error message (#716)
    * Add improved error message
    
    * Make formatting better :)

--- lightly/api/api_workflow_upload_metadata.py
+++ lightly/api/api_workflow_upload_metadata.py
@@ -175,8 +175,11 @@ class _UploadCustomMetadataMixin:
             custom_metadata,
         )
         if len(filename_to_metadata) != len(custom_metadata[COCO_ANNOTATION_KEYS.images]):
-            raise ValueError("There exist image names in the custom metadata "
-                             "without corresponding filenames on the server.")
+            raise ValueError(
+                f'There is a mismatch between the number of images '
+                f'in the metadata file ({len(filename_to_metadata)}) and on the '
+                f'server ({len(custom_metadata[COCO_ANNOTATION_KEYS.images])}).'
+            )
 
         # retry upload if it times out
         def upload_sample_metadata(args):
