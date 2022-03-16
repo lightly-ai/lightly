1326e19608596a766a13d5ef5b5f3d59d772fca4
MalteEbner <malte.ebner@gmail.com>
2022-03-16 17:57:55 +0100

    Update trigger_job_s3.py

--- docs/source/docker/integration/examples/trigger_job_s3.py
+++ docs/source/docker/integration/examples/trigger_job_s3.py
@@ -10,23 +10,20 @@ client = lightly.api.ApiWorkflowClient(token="TOKEN", dataset_id="DATASET_ID")
 # value according to your need.
 client.schedule_compute_worker_run(
     worker_config={
+        'enable_corruptness_check': True,
+        'remove_exact_duplicates': True,
+        'enable_training': False,
+        'pretagging': False,
+        'pretagging_debug': False,
+        'method': 'coreset',
         'stopping_condition': {
-            'n_samples': 10,
-            'enable_corruptness_check': True,
-            'remove_exact_duplicates': True,
-            'enable_training': False,
-            'pretagging': False,
-            'pretagging_debug': False,
-            'method': 'coreset',
-            'stopping_condition': {
-                'n_samples': 0.1,
-                'min_distance': -1
-            },
-            'scorer': 'object-frequency',
-            'scorer_config': {
-                'frequency_penalty': 0.25,
-                'min_score': 0.9
-            }
+            'n_samples': 0.1,
+            'min_distance': -1
+        },
+        'scorer': 'object-frequency',
+        'scorer_config': {
+            'frequency_penalty': 0.25,
+            'min_score': 0.9
         }
     },
     lightly_config={
