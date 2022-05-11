20bd31b46f6fd871d9626946c820f24f984ed485
guarin <guarin@lightly.ai>
2022-05-11 12:03:14 +0000

    Add note about runtime

--- docs/source/getting_started/benchmarks/cifar10_benchmark.py
+++ docs/source/getting_started/benchmarks/cifar10_benchmark.py
@@ -8,7 +8,7 @@ Updated: 18.02.2022 (6618fa3c36b0c9f3a9d7a21bcdb00bf4fd258ee8))
 | Model         | Batch Size | Epochs |  KNN Test Accuracy |       Time | Peak GPU Usage |
 ------------------------------------------------------------------------------------------
 | BarlowTwins   |        128 |    200 |              0.835 |  193.4 Min |      2.2 GByte |
-| DCL           |        128 |    200 |              0.841 |  372.8 Min |      1.7 GByte |
+| DCL (*)       |        128 |    200 |              0.841 |  372.8 Min |      1.7 GByte |
 | BYOL          |        128 |    200 |              0.872 |  217.0 Min |      2.3 GByte |
 | DINO          |        128 |    200 |              0.868 |  220.7 Min |      2.3 GByte |
 | Moco          |        128 |    200 |              0.838 |  229.5 Min |      2.3 GByte |
@@ -20,8 +20,8 @@ Updated: 18.02.2022 (6618fa3c36b0c9f3a9d7a21bcdb00bf4fd258ee8))
 | BarlowTwins   |        512 |    200 |              0.827 |  160.7 Min |      7.5 GByte |
 | BYOL          |        512 |    200 |              0.872 |  188.5 Min |      7.7 GByte |
 | DINO          |        512 |    200 |              0.862 |  191.1 Min |      7.5 GByte |
-| Moco (*)      |        512 |    200 |              0.850 |  196.8 Min |      7.8 GByte |
-| NNCLR (*)     |        512 |    200 |              0.836 |  164.7 Min |      7.6 GByte |
+| Moco (**)     |        512 |    200 |              0.850 |  196.8 Min |      7.8 GByte |
+| NNCLR (**)    |        512 |    200 |              0.836 |  164.7 Min |      7.6 GByte |
 | SimCLR        |        512 |    200 |              0.828 |  158.2 Min |      7.5 GByte |
 | SimSiam       |        512 |    200 |              0.814 |  159.0 Min |      7.6 GByte |
 | SwaV          |        512 |    200 |              0.833 |  158.4 Min |      7.5 GByte |
@@ -29,14 +29,15 @@ Updated: 18.02.2022 (6618fa3c36b0c9f3a9d7a21bcdb00bf4fd258ee8))
 | BarlowTwins   |        512 |    800 |              0.857 |  641.5 Min |      7.5 GByte |
 | BYOL          |        512 |    800 |              0.911 |  754.2 Min |      7.8 GByte |
 | DINO          |        512 |    800 |              0.884 |  765.5 Min |      7.6 GByte |
-| Moco (*)      |        512 |    800 |              0.900 |  787.7 Min |      7.8 GByte |
-| NNCLR (*)     |        512 |    800 |              0.896 |  659.2 Min |      7.6 GByte |
+| Moco (**)     |        512 |    800 |              0.900 |  787.7 Min |      7.8 GByte |
+| NNCLR (**)    |        512 |    800 |              0.896 |  659.2 Min |      7.6 GByte |
 | SimCLR        |        512 |    800 |              0.875 |  632.5 Min |      7.5 GByte |
 | SimSiam       |        512 |    800 |              0.906 |  636.5 Min |      7.6 GByte |
 | SwaV          |        512 |    800 |              0.881 |  634.9 Min |      7.5 GByte |
 ------------------------------------------------------------------------------------------
 
-(*): Increased size of memory bank from 4096 to 8192 to avoid too quickly 
+(*): Benchmark ran on a smaller Tesla T4 GPU which results in larger runtimes.
+(**): Increased size of memory bank from 4096 to 8192 to avoid too quickly 
 changing memory bank due to larger batch size.
 
 The benchmarks were created on a single NVIDIA RTX A6000.
