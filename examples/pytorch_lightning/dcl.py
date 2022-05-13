c013c3f2bf0df61985533f488c6ea86a7ed95e7a
guarin <guarin@lightly.ai>
2022-05-13 07:17:06 +0000

    Add examples

---
+++ examples/pytorch_lightning/dcl.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+import torchvision
+import pytorch_lightning as pl
+
+from lightly.data import LightlyDataset
+from lightly.data import SimCLRCollateFunction
+from lightly.loss import DCLLoss, DCLWLoss
+from lightly.models.modules import SimCLRProjectionHead
+
+
+class DCL(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        resnet = torchvision.models.resnet18()
+        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.projection_head = SimCLRProjectionHead(512, 2048, 2048)
+        self.criterion = DCLLoss()
+        # or use the weighted DCLW loss:
+        # self.criterion = DCLWLoss()
+
+    def forward(self, x):
+        x = self.backbone(x).flatten(start_dim=1)
+        z = self.projection_head(x)
+        return z
+
+    def training_step(self, batch, batch_index):
+        (x0, x1), _, _ = batch
+        z0 = self.forward(x0)
+        z1 = self.forward(x1)
+        loss = self.criterion(z0, z1)
+        return loss
+
+    def configure_optimizers(self):
+        optim = torch.optim.SGD(self.parameters(), lr=0.06)
+        return optim
+
+
+model = DCL()
+
+cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+dataset = LightlyDataset.from_torch_dataset(cifar10)
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+collate_fn = SimCLRCollateFunction(
+    input_size=32,
+    gaussian_blur=0.,
+)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+gpus = 1 if torch.cuda.is_available() else 0
+
+trainer = pl.Trainer(max_epochs=10, gpus=gpus)
+trainer.fit(model=model, train_dataloaders=dataloader)
