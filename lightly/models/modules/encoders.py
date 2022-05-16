f02617b88a77ed5c877b6d51776e3404e29d0340
guarin <guarin@lightly.ai>
2022-05-16 09:26:06 +0000

    Refactor, add MAEBackbone

--- lightly/models/modules/encoders.py
+++ lightly/models/modules/encoders.py
@@ -4,23 +4,18 @@ from typing import Callable, Optional
 import torch
 import torch.nn as nn
 import torchvision
+from torchvision.models import vision_transformer
 
 from lightly.models import utils
 
 # TODO: Check for correct torchvision version
 
-class MAEEncoder(torchvision.models.vision_transformer.Encoder):
-
-    def encode(self, input: torch.Tensor, idx_keep: Optional[torch.Tensor] = None):
-        input = input + self.pos_embedding
-        if idx_keep is not None:
-            input = utils.get_at_index(input, idx_keep)
-        return self.ln(self.layers(self.dropout(input)))
+class MAEEncoder(vision_transformer.Encoder):
 
     @classmethod
-    def from_vit_encoder(cls, vit_encoder):
-        # Create encoder instance with dummy values as they will be overwritten
-        # by the copied vit_encoder attributes.
+    def from_vit_encoder(cls, vit_encoder: vision_transformer.Encoder):
+        # Create a new instance with dummy values as they will be overwritten
+        # by the copied vit_encoder attributes
         encoder = cls(
             seq_length=1,
             num_layers=1,
@@ -35,6 +30,59 @@ class MAEEncoder(torchvision.models.vision_transformer.Encoder):
         encoder.layers = vit_encoder.layers
         encoder.ln = vit_encoder.ln
         return encoder
+
+    def forward(self, input: torch.Tensor, idx_keep: Optional[torch.Tensor] = None):
+        """Encode input tokens. If idx_keep is specified only the tokens with
+        the provided indices are encoded.
+        """
+        input = input + self.pos_embedding
+        if idx_keep is not None:
+            input = utils.get_at_index(input, idx_keep)
+        return self.ln(self.layers(self.dropout(input)))
+
+
+class MAEBackbone(torchvision.models.vision_transformer.VisionTransformer):
+    @classmethod
+    def from_vit(cls, vit: torchvision.models.vision_transformer.VisionTransformer):
+        # Create a new instance with dummy values as they will be overwritten
+        # by the copied vit_encoder attributes
+        encoder = cls(
+            image_size=vit.image_size,
+            patch_size=vit.patch_size,
+            num_layers=1,
+            num_heads=1,
+            hidden_dim=vit.hidden_dim,
+            mlp_dim=vit.mlp_dim,
+            dropout=vit.dropout,
+            attention_dropout=vit.attention_dropout,
+            num_classes=vit.num_classes,
+            representation_size=vit.representation_size,
+            norm_layer=vit.norm_layer,
+        )
+        encoder.conv_proj = vit.conv_proj
+        encoder.class_token = vit.class_token
+        encoder.seq_length = vit.seq_length
+        encoder.heads = vit.heads
+        encoder.encoder = MAEEncoder.from_vit_encoder(vit.encoder)
+        return encoder
+
+    def forward(self, images, idx_keep: Optional[torch.Tensor] = None):
+        """Returns encoded class tokens from images."""
+        out = self.encode(images, idx_keep)
+        class_token = out[:, 0]
+        return class_token
+
+    def encode(self, images, idx_keep: Optional[torch.Tensor] = None):
+        """Returns encoded class and patch tokens from images."""
+        out = self.images_to_patch_embeddings(images)
+        out = utils.prepend_class_token(out, self.class_token)
+        return self.encoder(out, idx_keep)
+
+    def images_to_patch_embeddings(self, images: torch.Tensor):
+        """Converts images into patch embeddings."""
+        # output has shape (batch_size, height_n_patches * width_n_patches, embed_dim)
+        x = self.conv_proj(images)
+        return x.flatten(2).transpose(1, 2) 
 
 
 class MAEDecoder(torchvision.models.vision_transformer.Encoder):
@@ -65,13 +113,21 @@ class MAEDecoder(torchvision.models.vision_transformer.Encoder):
         self.prediction_head = nn.Linear(hidden_dim, out_dim)
 
     def forward(self, input):
-        return self.decode(input)
+        """Returns predicted pixel values from encoded input tokens."""
+        out = self.embed(input)
+        out = self.decode(input)
+        return self.predict(out)
 
     def embed(self, input):
+        """Converts encoded input tokens into decoder tokens with 
+        size self.hidden_dimension.
+        """
         return self.decoder_embed(input)
 
     def decode(self, input):
+        """Decodes encoded and embedded input tokens."""
         return super().forward(input)
 
     def predict(self, input):
+        """Predics pixel values from decoded input tokens."""
         return self.prediction_head(input)
