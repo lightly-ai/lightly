d7bbf3656fe5d105c9bc67e70ccd4aa5b6645e71
guarin <guarin@lightly.ai>
2022-05-18 13:45:46 +0000

    Test fixed positional embedding

--- lightly/models/modules/encoders.py
+++ lightly/models/modules/encoders.py
@@ -10,6 +10,71 @@ from lightly.models import utils
 
 # TODO: Check for correct torchvision version
 
+
+import numpy as np
+
+import torch
+
+# --------------------------------------------------------
+# 2D sine-cosine position embedding
+# References:
+# Transformer: https://github.com/tensorflow/models/blob/master/official/nlp/transformer/model_utils.py
+# MoCo v3: https://github.com/facebookresearch/moco-v3
+# --------------------------------------------------------
+def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
+    """
+    grid_size: int of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    """
+    grid_h = np.arange(grid_size, dtype=np.float32)
+    grid_w = np.arange(grid_size, dtype=np.float32)
+    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([2, 1, grid_size, grid_size])
+    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+    if cls_token:
+        pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
+    return pos_embed
+
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1) # (H*W, D)
+    return emb
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out) # (M, D/2)
+    emb_cos = np.cos(out) # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+def get_pos_embedding(seq_length, hidden_dim):
+    grid_size = int((seq_length - 1)**.5)
+    pos_embed = get_2d_sincos_pos_embed(hidden_dim, grid_size, cls_token=True).float()
+    return nn.Parameter(torch.from_numpy(pos_embed).float().unsqueeze(0), requires_grad=False)
+
 class MAEEncoder(vision_transformer.Encoder):
 
     @classmethod
@@ -25,7 +90,9 @@ class MAEEncoder(vision_transformer.Encoder):
             dropout=0,
             attention_dropout=0,
         )
-        encoder.pos_embedding = vit_encoder.pos_embedding
+        # encoder.pos_embedding = vit_encoder.pos_embedding
+        _, seq_length, hidden_dim = vit_encoder.pos_embedding.shape
+        encoder.pos_embedding = get_pos_embedding(seq_length, hidden_dim)
         encoder.dropout = vit_encoder.dropout
         encoder.layers = vit_encoder.layers
         encoder.ln = vit_encoder.ln
@@ -109,6 +176,7 @@ class MAEDecoder(torchvision.models.vision_transformer.Encoder):
             attention_dropout=attention_dropout,
             norm_layer=norm_layer,
         )
+        self.pos_embedding = get_pos_embedding(seq_length, hidden_dim)
         self.decoder_embed = nn.Linear(embed_input_dim, hidden_dim, bias=True)
         self.prediction_head = nn.Linear(hidden_dim, out_dim)
 
