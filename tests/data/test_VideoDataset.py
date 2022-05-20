bugfix + unittest offset calculation (#810)

tests/data/test_VideoDataset.py
@@ -5,6 +5,7 @@ from fractions import Fraction
 import unittest
 import os
 import shutil
+from typing import List
 from unittest import mock
 
 import numpy as np
@@ -40,6 +41,23 @@ class TestVideoDataset(unittest.TestCase):
         if not os.path.exists(path_to_folder):
             os.makedirs(path_to_folder)
 
+    def create_dataset_specified_frames_per_video(self, frames_per_video: List[int], w=32, h=32, c=3):
+        self.input_dir = tempfile.mkdtemp()
+        self.ensure_dir(self.input_dir)
+        self.frames_over_videos = [
+            (np.random.randn(frames, w, h, c) * 255).astype(np.uint8)
+            for frames in frames_per_video
+        ]
+        self.extensions = ('.avi')
+
+        for frames in self.frames_over_videos:
+            path = os.path.join(self.input_dir, f'output-{len(frames):03}.avi')
+            print(path)
+            out = cv2.VideoWriter(path, 0, 1, (w, h))
+            for frame in frames:
+                out.write(frame)
+            out.release()
+
     def create_dataset(self, n_videos=5, n_frames_per_video=10, w=32, h=32, c=3):
 
         self.n_videos = n_videos
@@ -59,31 +77,38 @@ class TestVideoDataset(unittest.TestCase):
             out.release()
 
     def test_video_similar_timestamps_for_different_backends(self):
-        n_videos = 5
-        n_frames_per_video = 10
-        self.create_dataset(n_videos=n_videos, n_frames_per_video=n_frames_per_video)
+        frames_per_video = list(range(1, 10))
+        self.create_dataset_specified_frames_per_video(frames_per_video)
 
         timestamps = []
         offsets = []
         backends = []
+        instances = []
 
         # iterate through different backends
         for backend in VIDEO_BACKENDS:
             torchvision.set_video_backend(backend)
 
-            _, video_timestamps, video_offsets, _ = \
+            video_instances, video_timestamps, video_offsets, _ = \
                 _make_dataset(self.input_dir, extensions=self.extensions)
             timestamps.append(video_timestamps)
             offsets.append(video_offsets)
             backends.append(backend)
+            instances.append(video_instances)
         
         # make sure backends don't match (sanity check)
         self.assertNotEqual(backends[0], backends[1])
 
         # we expect the same timestamps and offsets
         self.assertEqual(timestamps[0], timestamps[1])
-        expected_offsets = list(n_frames_per_video * i for i in range(n_videos))
-        self.assertEqual(offsets[0], offsets[1], expected_offsets)
+        self.assertEqual(offsets[0], offsets[1])
+
+        expected_frame_counts = [int(filename[-7:-4]) for filename in instances[0]]
+        # calculate expected offsets with old (slow) implementation
+        expected_offsets = [0] + expected_frame_counts[:-1]
+        for i in range(1, len(expected_offsets)):
+            expected_offsets[i] = expected_offsets[i - 1] + expected_offsets[i]
+        self.assertEqual(expected_offsets, offsets[0])
 
         shutil.rmtree(self.input_dir)
 