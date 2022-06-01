459f902136b430c40db28a1e3fa16b27e361d7df
philippmwirth <philipp.m.wirth@gmail.com>
2022-06-01 05:40:33 +0000

    Improve docstring

--- lightly/utils/debug.py
+++ lightly/utils/debug.py
@@ -4,6 +4,14 @@ import torch
 def std_of_l2_normalized(z: torch.Tensor):
     """Calculates the mean of the standard deviation of z along each dimension.
 
+    This measure was used by [0] to determine the level of collapse of the
+    learned representations. If the returned number is 0., the outputs z have
+    collapsed to a constant vector. "If the output z has a zero-mean isotropic
+    Gaussian distribution" [0], the returned number should be close to 1/sqrt(d)
+    where d is the dimensionality of the output.
+
+    [0]: https://arxiv.org/abs/2011.10566
+
     Args:
         z:
             A torch tensor of shape batch_size x dimension.
