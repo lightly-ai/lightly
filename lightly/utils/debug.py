12082cca1977f1a6d42eec7654b33d76108d3164
philippmwirth <philipp.m.wirth@gmail.com>
2022-06-01 13:13:29 +0000

    First working commit

--- lightly/utils/debug.py
+++ lightly/utils/debug.py
@@ -1,4 +1,12 @@
+from typing import List, Union
+from PIL import Image
+import matplotlib.pyplot as plt
+
 import torch
+import torchvision
+
+from lightly.data.collate import BaseCollateFunction, MultiViewCollateFunction, SimCLRCollateFunction, DINOCollateFunction, SwaVCollateFunction
+
 
 @torch.no_grad()
 def std_of_l2_normalized(z: torch.Tensor):
@@ -31,3 +39,90 @@ def std_of_l2_normalized(z: torch.Tensor):
 
     z_norm = torch.nn.functional.normalize(z, dim=1)
     return torch.std(z_norm, dim=0).mean()
+
+
+
+def apply_transform_without_normalize(
+    image: Image.Image,
+    transform #.TODO: typehint: transforms.Transform,
+):
+    """TODO"""
+    # TODO: change this
+    if isinstance(transform, torchvision.transforms.Compose):
+        for transform_ in transform.transforms:
+            if isinstance(transform_, torchvision.transforms.Compose):
+                image = apply_transform_without_normalize(image, transform_)
+                continue
+            if isinstance(transform_, torchvision.transforms.Normalize):
+                continue
+            if isinstance(transform_, torchvision.transforms.ToTensor):
+                continue
+            print(transform_)
+            image = transform_(image)
+    else:
+        image = transform_(image)
+    return image
+
+
+
+def generate_grid_of_augmented_images(
+    input_images: List[Image.Image],
+    collate_function: Union[BaseCollateFunction, MultiViewCollateFunction],
+):
+    """TODO"""
+
+    grid = []
+    if isinstance(collate_function, BaseCollateFunction):
+        for _ in range(2):
+            grid.append([
+                apply_transform_without_normalize(image, collate_function.transform)
+                for image in input_images
+            ])
+    elif isinstance(collate_function, MultiViewCollateFunction):
+        for transform in collate_function.transforms:
+            grid.append([
+                apply_transform_without_normalize(image, transform)
+                for image in input_images
+            ])
+    else:
+        raise ValueError('TODO')
+    return grid
+
+
+def plot_augmented_images(
+    input_images: List[Image.Image],
+    collate_function: Union[BaseCollateFunction, MultiViewCollateFunction],
+):
+    """TODO"""
+
+    grid = generate_grid_of_augmented_images(input_images, collate_function)
+    nrows = len(grid) + 1 # extra row for the original images
+    ncols = len(input_images)
+
+    fig, axs = plt.subplots(nrows, ncols)
+
+    grid.insert(0, input_images)
+    for i in range(nrows):
+        for ax, img in zip(axs[i], grid[i]):
+            ax.imshow(img)
+            ax.set_axis_off()
+
+    fig.tight_layout()
+    return fig
+
+
+if __name__ == '__main__':
+
+    import numpy
+    from PIL import Image
+
+    input_images = []
+    for i in range(5):
+        imarray = numpy.random.rand(100,100,3) * 255
+        im = Image.fromarray(imarray.astype('uint8')).convert('RGB')
+        input_images.append(im)
+
+    collate_function = DINOCollateFunction()
+
+    fig = plot_augmented_images(input_images, collate_function)
+    fig.savefig('hello.png')
