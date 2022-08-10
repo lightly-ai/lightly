d7331b23f9cbf2a0984daf786baa51a9192e0b2d
MalteEbner <malte.ebner@gmail.com>
2022-08-10 15:12:54 +0200

    allow passing a DockerWorkerSelectionConfig as selection_config

--- lightly/api/api_workflow_compute_worker.py
+++ lightly/api/api_workflow_compute_worker.py
@@ -1,31 +1,46 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
-from lightly.openapi_generated.swagger_client.models.create_docker_worker_registry_entry_request import CreateDockerWorkerRegistryEntryRequest
-from lightly.openapi_generated.swagger_client.models.docker_run_data import DockerRunData
-from lightly.openapi_generated.swagger_client.models.docker_run_scheduled_create_request import DockerRunScheduledCreateRequest
-from lightly.openapi_generated.swagger_client.models.docker_run_scheduled_data import DockerRunScheduledData
-from lightly.openapi_generated.swagger_client.models.docker_run_scheduled_priority import DockerRunScheduledPriority
-from lightly.openapi_generated.swagger_client.models.docker_worker_type import DockerWorkerType
-from lightly.openapi_generated.swagger_client.models.docker_worker_config import DockerWorkerConfig
-from lightly.openapi_generated.swagger_client.models.docker_worker_config_create_request import DockerWorkerConfigCreateRequest
+from lightly.openapi_generated.swagger_client import DockerWorkerSelectionConfig
+from lightly.openapi_generated.swagger_client.models.create_docker_worker_registry_entry_request import (
+    CreateDockerWorkerRegistryEntryRequest,
+)
+from lightly.openapi_generated.swagger_client.models.docker_run_data import (
+    DockerRunData,
+)
+from lightly.openapi_generated.swagger_client.models.docker_run_scheduled_create_request import (
+    DockerRunScheduledCreateRequest,
+)
+from lightly.openapi_generated.swagger_client.models.docker_run_scheduled_data import (
+    DockerRunScheduledData,
+)
+from lightly.openapi_generated.swagger_client.models.docker_run_scheduled_priority import (
+    DockerRunScheduledPriority,
+)
+from lightly.openapi_generated.swagger_client.models.docker_worker_type import (
+    DockerWorkerType,
+)
+from lightly.openapi_generated.swagger_client.models.docker_worker_config import (
+    DockerWorkerConfig,
+)
+from lightly.openapi_generated.swagger_client.models.docker_worker_config_create_request import (
+    DockerWorkerConfigCreateRequest,
+)
 
 
 class _ComputeWorkerMixin:
-
-    def register_compute_worker(self, name: str = 'Default') -> str:
+    def register_compute_worker(self, name: str = "Default") -> str:
         """Registers a new compute worker.
-        
+
         Args:
             name:
                 The name of the compute worker.
-        
+
         Returns:
             The id of the newly registered compute worker.
 
         """
         request = CreateDockerWorkerRegistryEntryRequest(
-            name=name, 
-            worker_type=DockerWorkerType.FULL
+            name=name, worker_type=DockerWorkerType.FULL
         )
         response = self._compute_worker_api.register_docker_worker(request)
         return response.id
@@ -37,7 +52,7 @@ class _ComputeWorkerMixin:
 
     def delete_compute_worker(self, worker_id: str):
         """Removes a compute worker.
-        
+
         Args:
             worker_id:
                 The id of the worker to remove.
@@ -46,31 +61,33 @@ class _ComputeWorkerMixin:
         self._compute_worker_api.delete_docker_worker_registry_entry_by_id(worker_id)
 
     def create_compute_worker_config(
-        self, 
-        worker_config: Optional[Dict[str, Any]] = None, 
+        self,
+        worker_config: Optional[Dict[str, Any]] = None,
         lightly_config: Optional[Dict[str, Any]] = None,
-        selection_config: Optional[Dict[str, Any]] = None,
+        selection_config: Optional[
+            Union[Dict[str, Any], DockerWorkerSelectionConfig]
+        ] = None,
     ) -> str:
         """Creates a new configuration for a compute worker run.
-        
+
         Args:
             worker_config:
-                Compute worker configuration. All possible values are listed in 
+                Compute worker configuration. All possible values are listed in
                 our docs: https://docs.lightly.ai/docker/configuration/configuration.html#list-of-parameters
             lightly_config:
                 Lightly configuration. All possible values are listed in our
                 docs: https://docs.lightly.ai/lightly.cli.html#default-settings
             selection_config:
                 Selection configuration. See the docs for more information:
-                TODO: add link
+                https://docs.lightly.ai/docker/getting_started/selection.html
 
         Returns:
             The id of the created config.
 
         """
         config = DockerWorkerConfig(
-            worker_type=DockerWorkerType.FULL, 
-            docker=worker_config, 
+            worker_type=DockerWorkerType.FULL,
+            docker=worker_config,
             lightly=lightly_config,
             selection=selection_config,
         )
@@ -82,21 +99,23 @@ class _ComputeWorkerMixin:
         self,
         worker_config: Optional[Dict[str, Any]] = None,
         lightly_config: Optional[Dict[str, Any]] = None,
-        selection_config: Optional[Dict[str, Any]] = None,
+        selection_config: Optional[
+            Union[Dict[str, Any], DockerWorkerSelectionConfig]
+        ] = None,
         priority: str = DockerRunScheduledPriority.MID,
     ) -> str:
         """Schedules a run with the given configurations.
-        
+
         Args:
             worker_config:
-                Compute worker configuration. All possible values are listed in 
+                Compute worker configuration. All possible values are listed in
                 our docs: https://docs.lightly.ai/docker/configuration/configuration.html#list-of-parameters
             lightly_config:
                 Lightly configuration. All possible values are listed in our
                 docs: https://docs.lightly.ai/lightly.cli.html#default-settings
             selection_config:
                 Selection configuration. See the docs for more information:
-                TODO: add link
+                https://docs.lightly.ai/docker/getting_started/selection.html
 
         Returns:
             The id of the scheduled run.
@@ -108,8 +127,7 @@ class _ComputeWorkerMixin:
             selection_config=selection_config,
         )
         request = DockerRunScheduledCreateRequest(
-            config_id=config_id, 
-            priority=priority
+            config_id=config_id, priority=priority
         )
         response = self._compute_worker_api.create_docker_run_scheduled_by_dataset_id(
             body=request,
