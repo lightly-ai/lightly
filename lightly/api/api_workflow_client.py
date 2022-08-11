ddec64dcef6bd38508ed7c1eefdfbe1aed0bcd77
Jeremy A. Prescott <jeremy@lightly.ai>
2022-08-11 16:40:37 +0200

    write tests

--- lightly/api/api_workflow_client.py
+++ lightly/api/api_workflow_client.py
@@ -243,7 +243,7 @@ class ApiWorkflowClient(_UploadEmbeddingsMixin,
             response = sess.put(signed_write_url, data=file, headers=headers)
         else:
             response = sess.put(signed_write_url, data=file)
-        print('response', response.content)
+        print('response', response.text)
         response.raise_for_status()
         return response
 
