d2a138da1b51c10b1fd8f14ca8a98ee0129da66a
Jeremy A. Prescott <jeremy@lightly.ai>
2022-08-11 09:03:51 +0200

    write tests

--- lightly/api/utils.py
+++ lightly/api/utils.py
@@ -151,8 +151,8 @@ def get_signed_url_destination(signed_url: str = '') -> DatasourceType:
     Returns:
         DatasourceType
     """
-    
-    assert isinstance(signed_url, 'str')
+
+    assert isinstance(signed_url, str)
 
     if 'storage.googleapis.com/' in signed_url:
         return DatasourceType.GCS
