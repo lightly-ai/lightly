3e09fef67fd181760827f7a2ffef3ba993ca3153
IgorSusmelj <igor_7779@hotmail.com>
2022-09-06 08:54:19 +0000

    Add tests for collaboration

---
+++ tests/api_workflow/test_api_workflow_collaboration.py
@@ -0,0 +1,17 @@
+from tests.api_workflow.mocked_api_workflow_client import MockedApiWorkflowSetup, MockedApiWorkflowClient
+
+
+class TestApiWorkflowDatasets(MockedApiWorkflowSetup):
+
+    def setUp(self) -> None:
+        self.api_workflow_client = MockedApiWorkflowClient(token="token_xyz")
+
+    def test_share_empty_dataset(self):
+        self.api_workflow_client.share_dataset(dataset_id="some-dataset-id", user_emails=[])
+
+    def test_share_dataset(self):
+        self.api_workflow_client.share_dataset(dataset_id="some-dataset-id", user_emails=["someone@something.com"])
+
+    def test_get_shared_users(self):
+        user_emails = self.api_workflow_client.get_shared_users(dataset_id="some-dataset-id")
+        assert user_emails == ["user1@gmail.com", "user2@something.com"]
