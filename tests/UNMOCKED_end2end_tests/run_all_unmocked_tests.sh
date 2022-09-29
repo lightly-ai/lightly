26ed1f4c0ed0604437a01742840d99a239683eca
Shaun Daley <shaundaley39@gmail.com>
2022-09-29 13:54:13 +0200

    drop second lightly-magic from unmocked tests

--- tests/UNMOCKED_end2end_tests/run_all_unmocked_tests.sh
+++ tests/UNMOCKED_end2end_tests/run_all_unmocked_tests.sh
@@ -38,7 +38,8 @@ lightly-upload token=$TOKEN input_dir=$INPUT_DIR new_dataset_name=test_unmocked_
 
 echo "############################### Test 4"
 lightly-magic token=$TOKEN input_dir=$INPUT_DIR trainer.max_epochs=0 new_dataset_name=test_unmocked_cli_4_${DATE_TIME}
-lightly-magic token=$TOKEN input_dir=$INPUT_DIR trainer.max_epochs=0 new_dataset_name=test_unmocked_cli_4_${DATE_TIME}
+# This would fail, since a dataset with this name already exists
+# lightly-magic token=$TOKEN input_dir=$INPUT_DIR trainer.max_epochs=0 new_dataset_name=test_unmocked_cli_4_${DATE_TIME}
 ((NUMBER_OF_DATASETS=NUMBER_OF_DATASETS+1))
 
 echo "############################### Test 5"
