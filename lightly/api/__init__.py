4c8d1eee4076d0c6190c4ef57a41562a34d1756f
MalteEbner <malte.ebner@gmail.com>
2022-11-02 18:19:39 +0100

    allow GET requests to pass arrays in the query

--- lightly/api/__init__.py
+++ lightly/api/__init__.py
@@ -5,3 +5,10 @@
 
 from lightly.api.api_workflow_client import ApiWorkflowClient
 from lightly.api.api_workflow_compute_worker import ArtifactNotExist
+
+# Mock the urlencode to use doseq, so that arrays in the query are properly encoded
+from six.moves.urllib.parse import urlencode
+def doseq_urlencode(fields):
+    return urlencode(fields, doseq=True)
+import urllib3.request
+urllib3.request.urlencode = doseq_urlencode
