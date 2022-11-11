1928cbcae429305dc724076b6225a97e3e0150f8
Atharva Phatak <athp456@gmail.com>
2022-11-10 19:15:58 -0500

    text-fixes

--- tests/loss/test_InfoNCELoss.py
+++ tests/loss/test_InfoNCELoss.py
@@ -4,7 +4,7 @@ import unittest
 import torchvision
 
 class InfoNCETest(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         resnet = torchvision.models.resnet50(pretrained = False).children()
         self.model = torch.nn.Sequential(*list(resnet.children)[:-1])
         self.dim = 360
@@ -18,7 +18,7 @@ class InfoNCETest(unittest.TestCase):
         z2 = self.model(viewTwo)
         loss = self.loss(z1, z2)
     
-    def test_infonce_backward(self, seed):
+    def test_infonce_backward(self, seed=42):
         torch.manual_seed(seed=seed)
         out0 = torch.randn(1,3,256,256)
         out1 = torch.rand(1,3,256,256)
