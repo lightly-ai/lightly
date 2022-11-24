20c30cb6419a8af49fccfba5feb85b958e08a411
Ra1Nik <niccolo@lightly.ai>
2022-11-24 17:40:43 +0100

    add example file

---
+++ examples/pytorch/vicreg.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+import torchvision
+
+from lightly.data import LightlyDataset
+from lightly.data import ImageCollateFunction
+from lightly.models.modules import BarlowTwinsProjectionHead
+from lightly.loss import VICRegLoss
+
+
+class VICReg(nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+        self.backbone = backbone
+        self.projection_head = BarlowTwinsProjectionHead(512, 2048, 2048)
+
+    def forward(self, x):
+        x = self.backbone(x).flatten(start_dim=1)
+        z = self.projection_head(x)
+        return z
+
+
+resnet = torchvision.models.resnet18()
+backbone = nn.Sequential(*list(resnet.children())[:-1])
+model = VICReg(backbone)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+dataset = LightlyDataset.from_torch_dataset(cifar10)
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+collate_fn = ImageCollateFunction(input_size=32)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+criterion = VICRegLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.06)
+
+print("Starting Training")
+for epoch in range(10):
+    total_loss = 0
+    for (x0, x1), _, _ in dataloader:
+        x0 = x0.to(device)
+        x1 = x1.to(device)
+        z0 = model(x0)
+        z1 = model(x1)
+        loss = criterion(z0, z1)
+        total_loss += loss.detach()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    avg_loss = total_loss / len(dataloader)
+    print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
