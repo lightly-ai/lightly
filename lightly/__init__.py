31a803d812d837217aece6ccbd810ff673237a23
Philipp Wirth <65946090+philippmwirth@users.noreply.github.com>
2022-12-19 18:34:55 +0100

    Philipp fix version check (#1011)
    The version check was broken because of circular imports that were swallowed by a try-catch.
    I resolved the circular import by moving the relevant function to lightly.api.utils.

--- lightly/__init__.py
+++ lightly/__init__.py
@@ -123,12 +123,13 @@ else:
     from lightly import transforms
     from lightly import utils
     
-    from lightly.api.version_checking import do_version_check
-
     if os.getenv('LIGHTLY_DID_VERSION_CHECK', 'False') == 'False':
         os.environ['LIGHTLY_DID_VERSION_CHECK'] = 'True'
-
-        try:
-            do_version_check(current_version=__version__)
-        except Exception as e:
-            pass
+        from multiprocessing import current_process
+        if current_process().name == 'MainProcess':
+            from lightly.api.version_checking import is_latest_version, LightlyAPITimeoutException
+            from lightly.openapi_generated.swagger_client.rest import ApiException
+            try:
+                is_latest_version(current_version=__version__)
+            except (ValueError, ApiException, LightlyAPITimeoutException):
+                pass
