f08ec1d4fd403dc5d990bd8874fee6b55940f685
Ubuntu <ubuntu@ip-172-31-31-27.eu-central-1.compute.internal>
2022-12-22 15:43:25 +0000

    Update naming in test

--- tests/models/test_ModelUtils.py
+++ tests/models/test_ModelUtils.py
@@ -12,7 +12,7 @@ from lightly.models.utils import deactivate_requires_grad
 from lightly.models.utils import update_momentum
 from lightly.models.utils import normalize_weight
 from lightly.models.utils import _no_grad_trunc_normal
-from lightly.models.utils import cosine_decay_schedule
+from lightly.models.utils import cosine_schedule
 
 
 def has_grad(model: nn.Module):
@@ -223,12 +223,12 @@ class TestModelUtils(unittest.TestCase):
     def test_random_token_mask(self):
         self._test_random_token_mask_parameters(device='cpu')
 
-    def test_cosine_decay_schedule(self):
-        momentum_0 = cosine_decay_schedule(1, 10, 0.99, 1)
+    def test_cosine_schedule(self):
+        momentum_0 = cosine_schedule(1, 10, 0.99, 1)
         momentum_hand_computed_0 = 0.99030154
-        momentum_1 = cosine_decay_schedule(95, 100, 0.7, 2)
+        momentum_1 = cosine_schedule(95, 100, 0.7, 2)
         momentum_hand_computed_1 = 1.99477063
-        momentum_2 = cosine_decay_schedule(1, 1, 0.996, 1)
+        momentum_2 = cosine_schedule(1, 1, 0.996, 1)
         momentum_hand_computed_2 = 1
 
         self.assertAlmostEqual(momentum_0, momentum_hand_computed_0, 6)
