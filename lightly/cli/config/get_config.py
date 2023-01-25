11a06b971c06ac5c7b5385ab18b77d00b2e698f7
MalteEbner <malte.ebner@gmail.com>
2023-01-25 15:34:27 +0100

    formatting

--- lightly/cli/config/get_config.py
+++ lightly/cli/config/get_config.py
@@ -4,6 +4,6 @@ from omegaconf import OmegaConf, DictConfig
 
 
 def get_lightly_config() -> DictConfig:
-    config_path = Path(__file__).with_name('config.yaml')
+    config_path = Path(__file__).with_name("config.yaml")
     conf = OmegaConf.load(config_path)
     return conf
