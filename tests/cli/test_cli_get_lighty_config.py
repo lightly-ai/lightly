c55305aaccd4be517a1396a2020a85f078ea1e7b
MalteEbner <malte.ebner@gmail.com>
2023-01-26 09:36:29 +0100

    Update tests/cli/test_cli_get_lighty_config.py
    Co-authored-by: Philipp Wirth <65946090+philippmwirth@users.noreply.github.com>

--- tests/cli/test_cli_get_lighty_config.py
+++ tests/cli/test_cli_get_lighty_config.py
@@ -4,7 +4,7 @@ from lightly.cli.config.get_config import get_lightly_config
 def test_get_lightly_config() -> None:
     conf = get_lightly_config()
     # Assert some default values
-    assert conf.append == False
+    assert not conf.append
     assert conf.checkpoint == ""
     assert conf.loader.batch_size == 16
     assert conf.trainer.weights_summary is None
