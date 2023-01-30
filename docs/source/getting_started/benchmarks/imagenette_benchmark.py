ca9a969143dc96870d62823fa02a15d779279bba
Ra1Nik <niccolo@lightly.ai>
2023-01-30 12:03:15 +0100

    Add benchmark code

--- docs/source/getting_started/benchmarks/imagenette_benchmark.py
+++ docs/source/getting_started/benchmarks/imagenette_benchmark.py
@@ -193,8 +193,8 @@ def get_data_loaders(batch_size: int, model):
         batch_size: Desired batch size for all dataloaders
     """
     col_fn = collate_fn
-    if model == SwaVModel:
-        col_fn = swav_collate_fn
+    if model == SwaVModel or model == SwaVQueueModel:
+        col_fn = swav_collate_fn        
     elif model == DINOModel:
         col_fn = dino_collate_fn
     elif model == MAEModel:
@@ -1158,25 +1158,102 @@ class TiCoModel(BenchmarkModule):
         cosine_scheduler = scheduler.CosineWarmupScheduler(optim, self.warmup_epochs, max_epochs)
         return [optim], [cosine_scheduler]
 
+class SwaVQueueModel(BenchmarkModule):
+    def __init__(self, dataloader_kNN, num_classes):
+        super().__init__(dataloader_kNN, num_classes)
+        # create a ResNet backbone and remove the classification head
+        resnet = torchvision.models.resnet18()
+        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.projection_head = heads.SwaVProjectionHead(512, 512, 128)
+        self.prototypes = heads.SwaVPrototypes(128, 512, 10)
+        self.start_queue_at_epoch = 15
+        self.queues = nn.ModuleList([lightly.loss.memory_bank.MemoryBankModule(size=3840) for _ in range(2)])
+        self.criterion = lightly.loss.SwaVLoss()
+
+    def forward(self, high_resolution, low_resolution):
+         self.prototypes.normalize()
+
+         high_resolution_features = [self._subforward(x) for x in high_resolution]
+         low_resolution_features = [self._subforward(x) for x in low_resolution]
+
+         high_resolution_prototypes = [self.prototypes(x, self.global_step) for x in high_resolution_features]
+         low_resolution_prototypes = [self.prototypes(x, self.global_step) for x in low_resolution_features]
+         queue_prototypes = self._get_queue_prototypes(high_resolution_features, self.current_epoch)
+
+         return high_resolution_prototypes, low_resolution_prototypes, queue_prototypes
+    
+    def training_step(self, batch, batch_idx):
+        batch_swav, _, _ = batch
+        high_resolution, low_resolution = batch_swav[:2], batch_swav[2:]
+        high_resolution, low_resolution, queue = self.forward(high_resolution, low_resolution)
+        loss = self.criterion(high_resolution, low_resolution, queue)
+        return loss
+    
+    def _subforward(self, input):
+         features = self.backbone(input).flatten(start_dim=1)
+         features = self.projection_head(features)
+         features = nn.functional.normalize(features, dim=1, p=2)
+         return features
+    
+    @torch.no_grad()
+    def _get_queue_prototypes(self, high_resolution_features):
+
+        if len(high_resolution_features) != len(self.queues):
+            raise ValueError(
+                f"The number of queues ({len(self.queues)}) should be equal to the number of high "
+                f"resolution inputs ({len(high_resolution_features)}). Set `n_queues` accordingly."
+            )
+
+        # Get the queue features
+        queue_features = []
+        for i in range(len(self.queues)):
+            _, features = self.queues[i](high_resolution_features[i], update=True)
+            # Queue features are in (num_ftrs X queue_length) shape, while the high res
+            # features are in (batch_size X num_ftrs). Swap the axes for interoperability.
+            features = torch.permute(features, (1, 0))
+            queue_features.append(features)
+
+        # If loss calculation with queue prototypes starts at a later epoch,
+        # just queue the features and return None instead of queue prototypes.
+        if (
+            self.start_queue_at_epoch > 0
+            and self.current_epoch < self.start_queue_at_epoch
+        ):
+            return None
+
+        # Assign prototypes
+        queue_prototypes = [self.prototypes(x, self.global_step) for x in queue_features]
+        return queue_prototypes
+
+    def configure_optimizers(self):
+        optim = torch.optim.Adam(
+            self.parameters(),
+            lr=1e-3 * lr_factor,
+            weight_decay=1e-6,
+        )
+        cosine_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, max_epochs)
+        return [optim], [cosine_scheduler]
+
 
 models = [
-    BarlowTwinsModel,
-    BYOLModel,
-    DCL,
-    DCLW,
-    DINOModel,
+    #BarlowTwinsModel,
+    #BYOLModel,
+    #DCL,
+    #DCLW,
+    #DINOModel,
     # MAEModel, # disabled by default because MAE uses larger images with size 224
     # MSNModel, # disabled by default because MSN uses larger images with size 224
-    MocoModel,
-    NNCLRModel,
-    SimCLRModel,
+    #MocoModel,
+    #NNCLRModel,
+    #SimCLRModel,
     # SimMIMModel, # disabled by default because SimMIM uses larger images with size 224
-    SimSiamModel,
-    SwaVModel,
-    SMoGModel,
-    TiCoModel,
-    VICRegModel,
-    VICRegLModel,
+    #SimSiamModel,
+    #SwaVModel,
+    SwaVQueueModel,
+    #SMoGModel,
+    #TiCoModel,
+    #VICRegModel,
+    #VICRegLModel,
 ]
 bench_results = dict()
 
