fad7fea70f748be1395da0168a318f7c0c4f5391
Ubuntu <ubuntu@ip-172-31-31-27.eu-central-1.compute.internal>
2023-02-03 10:16:38 +0000

    Revert num_epochs

--- docs/source/getting_started/benchmarks/imagenette_benchmark.py
+++ docs/source/getting_started/benchmarks/imagenette_benchmark.py
@@ -80,7 +80,7 @@ num_workers = 12
 memory_bank_size = 4096
 
 # set max_epochs to 800 for long run (takes around 10h on a single V100)
-max_epochs = 800
+max_epochs = 200
 knn_k = 200
 knn_t = 0.1
 classes = 10
