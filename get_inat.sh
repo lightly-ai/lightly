5c154ab08728e8cd2c84816e3bdac0f042aade7e
damaggu <markusm2342@gmail.com>
2023-02-07 21:04:05 -0800

    get inat mini dataset

---
+++ get_inat.sh
@@ -0,0 +1,9 @@
+wget https://ml-inat-competition-datasets.s3.amazonaws.com/2021/train_mini.tar.gz
+wget https://ml-inat-competition-datasets.s3.amazonaws.com/2021/train_mini.json.tar.gz
+wget https://ml-inat-competition-datasets.s3.amazonaws.com/2021/val.tar.gz
+wget https://ml-inat-competition-datasets.s3.amazonaws.com/2021/val.json.tar.gz
+#
+tar -xvf train_mini.tar.gz
+tar -xvf train_mini.json.tar.gz
+tar -xvf val.tar.gz
+tar -xvf val.json.tar.gz
