fa6637c3b32123b6d7c19e7431641441e7e4ff20
damaggu <markusm2342@gmail.com>
2023-02-08 16:31:10 -0800

    update get inat

--- get_inat.sh
+++ get_inat.sh
@@ -7,3 +7,10 @@ tar -xvf train_mini.tar.gz
 tar -xvf train_mini.json.tar.gz
 tar -xvf val.tar.gz
 tar -xvf val.json.tar.gz
+
+mkdir datasets
+mkdir datasets/inat
+mv train_mini datasets/inat
+mv train_mini.json datasets/inat
+mv val datasets/inat
+mv val.json datasets/inat
