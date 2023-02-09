a1c68caafb8de79b5a734e132c4a2cf0145d5eab
damaggu <markusm2342@gmail.com>
2023-02-08 16:35:51 -0800

    enable distributed

--- imagenette_benchmark_contrastMAE.py
+++ imagenette_benchmark_contrastMAE.py
@@ -102,18 +102,18 @@ project_name = dataset_name + '_benchmark'
 log_model = False
 
 #  Set to True to enable Distributed Data Parallel training.
-distributed = False
+distributed = True
 
 # Set to True to enable Synchronized Batch Norm (requires distributed=True).
 # If enabled the batch norm is calculated over all gpus, otherwise the batch
 # norm is only calculated from samples on the same gpu.
-sync_batchnorm = False
+sync_batchnorm = True
 
 # Set to True to gather features from all gpus before calculating
 # the loss (requires distributed=True).
 #  If enabled then the loss on every gpu is calculated with features from all
 # gpus, otherwise only features from the same gpu are used.
-gather_distributed = False
+gather_distributed = True
 
 # benchmark
 n_runs = 1  # optional, increase to create multiple runs and report mean + std
