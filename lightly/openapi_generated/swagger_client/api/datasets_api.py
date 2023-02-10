b99663cfe68cea90d228637b3b362467ce51e597
MalteEbner <malte.ebner@gmail.com>
2023-02-10 16:22:59 +0100

    generate new swagger client

--- lightly/openapi_generated/swagger_client/api/datasets_api.py
+++ lightly/openapi_generated/swagger_client/api/datasets_api.py
@@ -623,6 +623,109 @@ class DatasetsApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def get_datasets_enriched_query_by_name(self, dataset_name, **kwargs):  # noqa: E501
+        """get_datasets_enriched_query_by_name  # noqa: E501
+
+        Query for datasets  enriched with additional information by their name prefix unless exact flag is set  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_datasets_enriched_query_by_name(dataset_name, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param DatasetNameQuery dataset_name: (required)
+        :param bool shared: if set, only returns the datasets which have been shared with the user
+        :param bool exact: if set, only returns the datasets which match the name exactly (not just by prefix)
+        :return: list[DatasetDataEnriched]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_datasets_enriched_query_by_name_with_http_info(dataset_name, **kwargs)  # noqa: E501
+        else:
+            (data) = self.get_datasets_enriched_query_by_name_with_http_info(dataset_name, **kwargs)  # noqa: E501
+            return data
+
+    def get_datasets_enriched_query_by_name_with_http_info(self, dataset_name, **kwargs):  # noqa: E501
+        """get_datasets_enriched_query_by_name  # noqa: E501
+
+        Query for datasets  enriched with additional information by their name prefix unless exact flag is set  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_datasets_enriched_query_by_name_with_http_info(dataset_name, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param DatasetNameQuery dataset_name: (required)
+        :param bool shared: if set, only returns the datasets which have been shared with the user
+        :param bool exact: if set, only returns the datasets which match the name exactly (not just by prefix)
+        :return: list[DatasetDataEnriched]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['dataset_name', 'shared', 'exact']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_datasets_enriched_query_by_name" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'dataset_name' is set
+        if self.api_client.client_side_validation and ('dataset_name' not in params or
+                                                       params['dataset_name'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `dataset_name` when calling `get_datasets_enriched_query_by_name`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'dataset_name' in params:
+            path_params['datasetName'] = params['dataset_name']  # noqa: E501
+
+        query_params = []
+        if 'shared' in params:
+            query_params.append(('shared', params['shared']))  # noqa: E501
+        if 'exact' in params:
+            query_params.append(('exact', params['exact']))  # noqa: E501
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['ApiKeyAuth', 'auth0Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/v1/datasets/enriched/query/name/{datasetName}', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='list[DatasetDataEnriched]',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_datasets_query_by_name(self, dataset_name, **kwargs):  # noqa: E501
         """get_datasets_query_by_name  # noqa: E501
 
@@ -633,7 +736,7 @@ class DatasetsApi(object):
         >>> result = thread.get()
 
         :param async_req bool
-        :param DatasetName dataset_name: (required)
+        :param DatasetNameQuery dataset_name: (required)
         :param bool shared: if set, only returns the datasets which have been shared with the user
         :param bool exact: if set, only returns the datasets which match the name exactly (not just by prefix)
         :return: list[DatasetData]
@@ -657,7 +760,7 @@ class DatasetsApi(object):
         >>> result = thread.get()
 
         :param async_req bool
-        :param DatasetName dataset_name: (required)
+        :param DatasetNameQuery dataset_name: (required)
         :param bool shared: if set, only returns the datasets which have been shared with the user
         :param bool exact: if set, only returns the datasets which match the name exactly (not just by prefix)
         :return: list[DatasetData]
--- lightly/openapi_generated/swagger_client/api/docker_api.py
+++ lightly/openapi_generated/swagger_client/api/docker_api.py
@@ -651,6 +651,105 @@ class DockerApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def create_docker_worker_config_v2(self, body, **kwargs):  # noqa: E501
+        """create_docker_worker_config_v2  # noqa: E501
+
+        Creates a docker worker v2 configuration.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.create_docker_worker_config_v2(body, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param DockerWorkerConfigV2CreateRequest body: (required)
+        :return: CreateEntityResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.create_docker_worker_config_v2_with_http_info(body, **kwargs)  # noqa: E501
+        else:
+            (data) = self.create_docker_worker_config_v2_with_http_info(body, **kwargs)  # noqa: E501
+            return data
+
+    def create_docker_worker_config_v2_with_http_info(self, body, **kwargs):  # noqa: E501
+        """create_docker_worker_config_v2  # noqa: E501
+
+        Creates a docker worker v2 configuration.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.create_docker_worker_config_v2_with_http_info(body, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param DockerWorkerConfigV2CreateRequest body: (required)
+        :return: CreateEntityResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['body']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method create_docker_worker_config_v2" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'body' is set
+        if self.api_client.client_side_validation and ('body' not in params or
+                                                       params['body'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `body` when calling `create_docker_worker_config_v2`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if 'body' in params:
+            body_params = params['body']
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['ApiKeyAuth', 'auth0Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/v1/docker/worker/config/v2', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='CreateEntityResponse',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def delete_docker_worker_registry_entry_by_id(self, worker_id, **kwargs):  # noqa: E501
         """delete_docker_worker_registry_entry_by_id  # noqa: E501
 
