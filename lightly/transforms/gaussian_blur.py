8a762ebff97f76c63ecd4a27890d549a6afa1eac
Ra1Nik <niccolo@lightly.ai>
2023-02-16 09:26:43 +0100

    Add deprecation warning for GaussianBlur and fix collate functions. (#1055)
    Add a deprecation warning for Lightly GaussianBlur transform when using the kernel_size variable. 
    - Fix collate functions accordingly
    - Adapt config.yaml
    - Adapt unit test

--- lightly/transforms/gaussian_blur.py
+++ lightly/transforms/gaussian_blur.py
@@ -5,31 +5,46 @@
 
 import numpy as np
 from PIL import ImageFilter
-from typing import Tuple
+from typing import Tuple, Union
+from warnings import warn
 
 
 class GaussianBlur(object):
     """Implementation of random Gaussian blur.
 
-    Utilizes the built-in ImageFilter method from PIL to apply a Gaussian 
+    Utilizes the built-in ImageFilter method from PIL to apply a Gaussian
     blur to the input image with a certain probability. The blur is further
     randomized by sampling uniformly the values of the standard deviation of
     the Gaussian kernel.
 
     Attributes:
         kernel_size:
-            Mean kernel size for the Gaussian blur.
+            Will be deprecated in favor of `sigmas` argument. If set, the old behavior applies and `sigmas` is ignored.
+            Used to calculate sigma of gaussian blur with kernel_size * input_size.
         prob:
             Probability with which the blur is applied.
         scale:
-            Old unused parameters kept for compatibility
+            Will be deprecated in favor of `sigmas` argument. If set, the old behavior applies and `sigmas` is ignored.
+            Used to scale the `kernel_size` of a factor of `kernel_scale`
         sigmas:
-            Tuple of min and max value from which the std of the gaussian kernel is sampled
+            Tuple of min and max value from which the std of the gaussian kernel is sampled.
+            Is ignored if `kernel_size` is set.
 
     """
 
-    def __init__(self, kernel_size: float, prob: float = 0.5,
-                 scale: float = 0.2, sigmas: Tuple[float, float] = (.2, 2)):
+    def __init__(
+        self,
+        kernel_size: Union[float, None] = None,
+        prob: float = 0.5,
+        scale: Union[float, None] = None,
+        sigmas: Tuple[float, float] = (0.2, 2),
+    ):
+        if scale != None or kernel_size != None:
+            warn(
+                "The 'kernel_size' and 'scale' arguments of the GaussianBlur augmentation will be deprecated.  "
+                "Please use the 'sigmas' parameter instead.",
+                PendingDeprecationWarning,
+            )
         self.prob = prob
         self.sigmas = sigmas
 
@@ -39,7 +54,7 @@ class GaussianBlur(object):
         Args:
             sample:
                 PIL image to which blur will be applied.
-        
+
         Returns:
             Blurred image or original image.
         """
@@ -47,6 +62,9 @@ class GaussianBlur(object):
         if prob < self.prob:
             # choose randomized std for Gaussian filtering
             sigma = np.random.uniform(self.sigmas[0], self.sigmas[1])
+            # PIL GaussianBlur https://github.com/python-pillow/Pillow/blob/76478c6865c78af10bf48868345db2af92f86166/src/PIL/ImageFilter.py#L154 label the
+            # sigma parameter of the gaussian filter as radius. Before, the radius of the patch was passed as the argument.
+            # The issue was addressed here https://github.com/lightly-ai/lightly/issues/1051 and solved by AurelienGauffre.
             return sample.filter(ImageFilter.GaussianBlur(radius=sigma))
         # return original image
         return sample
