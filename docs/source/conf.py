8950f0e51b46efe8ec91def2116e64f508c41aca
MalteEbner <malte.ebner@gmail.com>
2023-03-23 10:32:08 +0100

    remove docs for active learning (#1118)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,3 @@ html_context = {
     "copyright_year": copyright_year,
     "website_url": website_url,
 }
-
-redirects = {
-    "docker/advanced/active_learning": "../../docker/getting_started/selection.html"
-}
