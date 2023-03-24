dc6c36aa987bfbe744cd632c067f2febb98676f3
MalteEbner <malte.ebner@gmail.com>
2023-03-24 11:43:04 +0100

    formatting

--- lightly/active_learning/__init__.py
+++ lightly/active_learning/__init__.py
@@ -1,13 +1,15 @@
 import warnings
 
+
 def raise_active_learning_deprecation_warning():
     print("asdf")
     warnings.warn(
-        "Using active learning via the lightly package is deprecated and will be removed soon "
+        "Using active learning via the lightly package is deprecated and will be removed soon. "
         "Please use the Lightly Solution instead. "
-        "See https://docs.lightly.ai for more information including tutorials on doing active learning.",
+        "See https://docs.lightly.ai for more information and tutorials on doing active learning.",
         DeprecationWarning,
         stacklevel=2,
     )
+
 
 raise_active_learning_deprecation_warning()
