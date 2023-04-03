0f3796d0dc7ab162683d5a7df828e7a5fd39b558
philippmwirth <philippmwirth@gmail.com>
2023-04-03 15:39:39 +0000

    Update swagger client code

--- lightly/openapi_generated/swagger_client/api/teams_api.py
+++ lightly/openapi_generated/swagger_client/api/teams_api.py
@@ -127,3 +127,193 @@ class TeamsApi(object):
             _preload_content=params.get('_preload_content', True),
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
+
+    def get_team_by_id(self, team_id, **kwargs):  # noqa: E501
+        """get_team_by_id  # noqa: E501
+
+        Get basic team information by ID.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_team_by_id(team_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param MongoObjectID team_id: id of the team (required)
+        :return: TeamData
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_team_by_id_with_http_info(team_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.get_team_by_id_with_http_info(team_id, **kwargs)  # noqa: E501
+            return data
+
+    def get_team_by_id_with_http_info(self, team_id, **kwargs):  # noqa: E501
+        """get_team_by_id  # noqa: E501
+
+        Get basic team information by ID.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_team_by_id_with_http_info(team_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param MongoObjectID team_id: id of the team (required)
+        :return: TeamData
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['team_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_team_by_id" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'team_id' is set
+        if self.api_client.client_side_validation and ('team_id' not in params or
+                                                       params['team_id'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `team_id` when calling `get_team_by_id`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'team_id' in params:
+            path_params['teamId'] = params['team_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['ApiKeyAuth', 'auth0Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/v1/teams/{teamId}', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='TeamData',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def get_team_members_by_id(self, team_id, **kwargs):  # noqa: E501
+        """get_team_members_by_id  # noqa: E501
+
+        Get the members of a team. One needs to be part of the team to do so.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_team_members_by_id(team_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param MongoObjectID team_id: id of the team (required)
+        :return: list[ProfileBasicData]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_team_members_by_id_with_http_info(team_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.get_team_members_by_id_with_http_info(team_id, **kwargs)  # noqa: E501
+            return data
+
+    def get_team_members_by_id_with_http_info(self, team_id, **kwargs):  # noqa: E501
+        """get_team_members_by_id  # noqa: E501
+
+        Get the members of a team. One needs to be part of the team to do so.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_team_members_by_id_with_http_info(team_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param MongoObjectID team_id: id of the team (required)
+        :return: list[ProfileBasicData]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['team_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_team_members_by_id" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'team_id' is set
+        if self.api_client.client_side_validation and ('team_id' not in params or
+                                                       params['team_id'] is None):  # noqa: E501
+            raise ValueError("Missing the required parameter `team_id` when calling `get_team_members_by_id`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'team_id' in params:
+            path_params['teamId'] = params['team_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['ApiKeyAuth', 'auth0Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/v1/teams/{teamId}/members', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='list[ProfileBasicData]',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
