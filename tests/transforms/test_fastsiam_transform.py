c26466c69bba9f893d8e2eef002f8929d6a71013
philippmwirth <philippmwirth@gmail.com>
2023-04-05 14:35:41 +0000

    Fix unit tests

--- tests/transforms/test_fastsiam_transform.py
+++ tests/transforms/test_fastsiam_transform.py
@@ -4,10 +4,10 @@ from lightly.transforms.fast_siam_transform import FastSiamTransform
 
 
 def test_multi_view_on_pil_image():
-    multi_view_transform = FastSiamTransform(K=2, input_size=32)
+    multi_view_transform = FastSiamTransform(num_views=3, input_size=32)
     sample = Image.new("RGB", (100, 100))
     output = multi_view_transform(sample)
-    assert len(output) == 3  # K + 1
+    assert len(output) == 3
     assert output[0].shape == (3, 32, 32)
     assert output[1].shape == (3, 32, 32)
     assert output[2].shape == (3, 32, 32)
