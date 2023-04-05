277bb2fdd76f1e2faeecd821a0ccdc830ae65b08
Huan-Cheng Chang <huan@lightly.ai>
2023-04-05 12:53:29 +0000

    minor

--- lightly/cli/config/get_config.py
+++ lightly/cli/config/get_config.py
@@ -6,7 +6,7 @@ from omegaconf import DictConfig, OmegaConf
 def get_lightly_config() -> DictConfig:
     config_path = Path(__file__).with_name("config.yaml")
     conf = OmegaConf.load(config_path)
-    # FIXME: remove this when hydra is completely dropped
+    # TODO(Huan, 05.04.2023): remove this when hydra is completely dropped
     if conf.get("hydra"):
         # This config entry is only for hydra; not referenced in any logic
         del conf["hydra"]
