f072f8be2e3ab698b16be4d60ff0720c6fc082d4
huan-lightly-0 <128385781+huan-lightly-0@users.noreply.github.com>
2023-04-05 15:14:29 +0200

    lig-2904: Remove hydra from lightly config (#1131)

--- lightly/cli/config/get_config.py
+++ lightly/cli/config/get_config.py
@@ -6,4 +6,8 @@ from omegaconf import DictConfig, OmegaConf
 def get_lightly_config() -> DictConfig:
     config_path = Path(__file__).with_name("config.yaml")
     conf = OmegaConf.load(config_path)
+    # TODO(Huan, 05.04.2023): remove this when hydra is completely dropped
+    if conf.get("hydra"):
+        # This config entry is only for hydra; not referenced in any logic
+        del conf["hydra"]
     return conf
