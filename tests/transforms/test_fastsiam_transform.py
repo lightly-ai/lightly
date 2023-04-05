5135b6377e1087664f23e220cf0906379709955d
philippmwirth <philippmwirth@gmail.com>
2023-04-05 06:43:30 +0000

    Add test for FastSiam transform

---
+++ tests/transforms/test_fastsiam_transform.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+from lightly.transforms.fast_siam_transform import FastSiamTransform
+
+
+def test_multi_view_on_pil_image():
+    multi_view_transform = FastSiamTransform(K=2, input_size=32)
+    sample = Image.new("RGB", (100, 100))
+    output = multi_view_transform(sample)
+    assert len(output) == 3  # K + 1
+    assert output[0].shape == (3, 32, 32)
+    assert output[1].shape == (3, 32, 32)
+    assert output[2].shape == (3, 32, 32)
