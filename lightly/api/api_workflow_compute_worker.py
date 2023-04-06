2b042304751534f103d678c06328eaf57c11f244
huan-lightly-0 <128385781+huan-lightly-0@users.noreply.github.com>
2023-04-06 18:05:44 +0200

    lig-3137: Add get_compute_workers to API client (#1133)

--- lightly/api/api_workflow_compute_worker.py
+++ lightly/api/api_workflow_compute_worker.py
@@ -20,6 +20,7 @@ from lightly.openapi_generated.swagger_client import (
     DockerWorkerConfigV3CreateRequest,
     DockerWorkerConfigV3Docker,
     DockerWorkerConfigV3Lightly,
+    DockerWorkerRegistryEntryData,
     DockerWorkerType,
     SelectionConfig,
     SelectionConfigEntry,
@@ -109,6 +110,13 @@ class _ComputeWorkerMixin:
         """Returns the ids of all registered compute workers."""
         entries = self._compute_worker_api.get_docker_worker_registry_entries()
         return [entry.id for entry in entries]
+
+    def get_compute_workers(self) -> List[DockerWorkerRegistryEntryData]:
+        """Returns the ids of all registered compute workers."""
+        entries: list[
+            DockerWorkerRegistryEntryData
+        ] = self._compute_worker_api.get_docker_worker_registry_entries()
+        return entries
 
     def delete_compute_worker(self, worker_id: str):
         """Removes a compute worker.
