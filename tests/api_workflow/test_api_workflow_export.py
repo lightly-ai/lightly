c503ebe832125ceeff875bc7ae7607bb6090306b
MalteEbner <malte.ebner@gmail.com>
2023-04-11 17:44:36 +0200

    add datasourceUrl to export endpoint

--- tests/api_workflow/test_api_workflow_export.py
+++ tests/api_workflow/test_api_workflow_export.py
@@ -1,5 +1,7 @@
-from lightly.api import api_workflow_download_dataset
-from lightly.openapi_generated.swagger_client import DatasetEmbeddingData
+from unittest.mock import MagicMock
+
+from lightly.api import api_workflow_download_dataset, ApiWorkflowClient
+from lightly.openapi_generated.swagger_client import DatasetEmbeddingData, FileNameFormat, TagsApi
 from tests.api_workflow.mocked_api_workflow_client import MockedApiWorkflowSetup
 
 
@@ -62,18 +64,34 @@ class TestApiWorkflowExport(MockedApiWorkflowSetup):
         self.assertTrue(all(isinstance(task, dict) for task in tasks))
 
     def test_export_tag_to_basic_filenames_and_read_urls(self):
-        filenames_and_read_urls = (
-            self.api_workflow_client.export_filenames_and_read_urls_by_tag_name(
-                "initial-tag"
-            )
-        )
-        self.assertIsNotNone(filenames_and_read_urls)
-        self.assertTrue(
-            all(
-                isinstance(filenames_and_read_url, dict)
-                for filenames_and_read_url in filenames_and_read_urls
-            )
-        )
+
+        def mocked_export_tag_to_basic_filenames(dataset_id: str, tag_id: str, file_name_format: str):
+            return {
+                FileNameFormat.NAME: "\n".join(["sample1.jpg", "sample2.jpg"]),
+                FileNameFormat.REDIRECTED_READ_URL: "\n".join(["READ_URL_1", "READ_URL_2"]),
+                FileNameFormat.DATASOURCE_FULL: "\n".join(["s3://my_datasource/sample1.jpg", "s3://my_datasource/sample2.jpg"]),
+            }[file_name_format]
+
+        mocked_client = MagicMock(spec=ApiWorkflowClient)
+        mocked_client.dataset_id = "some_dataset_id"
+        mocked_client._tags_api = MagicMock(spec_set=TagsApi)
+        mocked_client._tags_api.export_tag_to_basic_filenames.side_effect = mocked_export_tag_to_basic_filenames
+
+        data = ApiWorkflowClient.export_filenames_and_read_urls_by_tag_id(self=mocked_client, tag_id="tag_id")
+
+        assert data == [
+                {
+                    "fileName": "sample1.jpg",
+                    "readUrl": "READ_URL_1",
+                    "datasourceUrl": "s3://my_datasource/sample1.jpg",
+                },
+                {
+                    "fileName": "sample2.jpg",
+                    "readUrl": "READ_URL_2",
+                    "datasourceUrl": "s3://my_datasource/sample2.jpg",
+                },
+            ]
+
 
     def test_export_filenames_by_tag_name(self):
         filenames = self.api_workflow_client.export_filenames_by_tag_name("initial-tag")
