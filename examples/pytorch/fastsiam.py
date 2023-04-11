5378a09612423b2f66a22d9176f771b33f5f5c15
Philipp Wirth <65946090+philippmwirth@users.noreply.github.com>
2023-04-11 13:53:42 +0200

    Add FastSiam examples (#1137)
    Follow-up to #1130 adding examples for FastSiam using PyTorch and PyTorch-Lightning.

---
+++ examples/pytorch/fastsiam.py
@@ -0,0 +1,76 @@
+# Note: The model and training settings do not follow the reference settings
+# from the paper. The settings are chosen such that the example can easily be
+# run on a small dataset with a single GPU.
+
+import torch
+import torchvision
+from torch import nn
+
+from lightly.data import LightlyDataset
+from lightly.data.multi_view_collate import MultiViewCollate
+from lightly.loss import NegativeCosineSimilarity
+from lightly.models.modules import SimSiamPredictionHead, SimSiamProjectionHead
+from lightly.transforms import FastSiamTransform
+
+
+class FastSiam(nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+        self.backbone = backbone
+        self.projection_head = SimSiamProjectionHead(512, 512, 128)
+        self.prediction_head = SimSiamPredictionHead(128, 64, 128)
+
+    def forward(self, x):
+        f = self.backbone(x).flatten(start_dim=1)
+        z = self.projection_head(f)
+        p = self.prediction_head(z)
+        z = z.detach()
+        return z, p
+
+
+resnet = torchvision.models.resnet18()
+backbone = nn.Sequential(*list(resnet.children())[:-1])
+model = FastSiam(backbone)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+transform = FastSiamTransform(input_size=32)
+dataset = LightlyDataset.from_torch_dataset(cifar10, transform=transform)
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+collate_fn = MultiViewCollate()
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+criterion = NegativeCosineSimilarity()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.06)
+
+print("Starting Training")
+for epoch in range(10):
+    total_loss = 0
+    for views, _, _ in dataloader:
+        features = [model(view.to(device)) for view in views]
+        zs = torch.stack([z for z, _ in features])
+        ps = torch.stack([p for _, p in features])
+
+        loss = 0.0
+        for i in range(len(views)):
+            mask = torch.arange(len(views), device=device) != i
+            loss += criterion(ps[i], torch.mean(zs[mask], dim=0)) / len(views)
+
+        total_loss += loss.detach()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    avg_loss = total_loss / len(dataloader)
+    print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
