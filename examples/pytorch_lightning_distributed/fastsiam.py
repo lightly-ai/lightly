0d14d03b1b6c61d8b77e9f65f0e270da77b13717
philippmwirth <philippmwirth@gmail.com>
2023-04-11 09:47:11 +0000

    Add pytorch_lightning FastSiam examples

---
+++ examples/pytorch_lightning_distributed/fastsiam.py
@@ -0,0 +1,81 @@
+# Note: The model and training settings do not follow the reference settings
+# from the paper. The settings are chosen such that the example can easily be
+# run on a small dataset with a single GPU.
+
+import pytorch_lightning as pl
+import torch
+import torchvision
+from torch import nn
+
+from lightly.data import LightlyDataset
+from lightly.data.multi_view_collate import MultiViewCollate
+from lightly.loss import NegativeCosineSimilarity
+from lightly.models.modules import SimSiamPredictionHead, SimSiamProjectionHead
+from lightly.transforms import FastSiamTransform
+
+
+class FastSiam(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        resnet = torchvision.models.resnet18()
+        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.projection_head = SimSiamProjectionHead(512, 512, 128)
+        self.prediction_head = SimSiamPredictionHead(128, 64, 128)
+        self.criterion = NegativeCosineSimilarity()
+
+    def forward(self, x):
+        f = self.backbone(x).flatten(start_dim=1)
+        z = self.projection_head(f)
+        p = self.prediction_head(z)
+        z = z.detach()
+        return z, p
+
+    def training_step(self, batch, batch_idx):
+        views, _, _ = batch
+        features = [self.forward(view) for view in views]
+        zs = torch.stack([z for z, _ in features])
+        ps = torch.stack([p for _, p in features])
+
+        loss = 0.0
+        for i in range(len(views)):
+            mask = torch.arange(len(views), device=self.device) != i
+            loss += self.criterion(ps[i], torch.mean(zs[mask], dim=0)) / len(views)
+
+        self.log("train_loss_ssl", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optim = torch.optim.SGD(self.parameters(), lr=0.06)
+        return optim
+
+
+model = FastSiam()
+
+cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+transform = FastSiamTransform(input_size=32)
+dataset = LightlyDataset.from_torch_dataset(cifar10, transform=transform)
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+collate_fn = MultiViewCollate()
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+# Train with DDP and use Synchronized Batch Norm for a more accurate batch norm
+# calculation. Distributed sampling is also enabled with replace_sampler_ddp=True.
+trainer = pl.Trainer(
+    max_epochs=10,
+    devices="auto",
+    accelerator="gpu",
+    strategy="ddp",
+    sync_batchnorm=True,
+    use_distributed_sampler=True,  # or replace_sampler_ddp=True for PyTorch Lightning <2.0
+)
+trainer.fit(model=model, train_dataloaders=dataloader)
