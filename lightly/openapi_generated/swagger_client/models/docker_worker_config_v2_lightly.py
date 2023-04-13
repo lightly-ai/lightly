b094632e6c9f79906bd8179a4320ba6c0c4b84d0
philippmwirth <philippmwirth@gmail.com>
2023-04-13 06:38:50 +0000

    Generate api code: rr_degrees

--- lightly/openapi_generated/swagger_client/models/docker_worker_config_v2_lightly.py
+++ lightly/openapi_generated/swagger_client/models/docker_worker_config_v2_lightly.py
@@ -38,7 +38,7 @@ class DockerWorkerConfigV2Lightly(object):
         'trainer': 'DockerWorkerConfigV2LightlyTrainer',
         'criterion': 'DockerWorkerConfigV3LightlyCriterion',
         'optimizer': 'DockerWorkerConfigV3LightlyOptimizer',
-        'collate': 'DockerWorkerConfigV3LightlyCollate'
+        'collate': 'DockerWorkerConfigV2LightlyCollate'
     }
 
     attribute_map = {
@@ -188,7 +188,7 @@ class DockerWorkerConfigV2Lightly(object):
 
 
         :return: The collate of this DockerWorkerConfigV2Lightly.  # noqa: E501
-        :rtype: DockerWorkerConfigV3LightlyCollate
+        :rtype: DockerWorkerConfigV2LightlyCollate
         """
         return self._collate
 
@@ -198,7 +198,7 @@ class DockerWorkerConfigV2Lightly(object):
 
 
         :param collate: The collate of this DockerWorkerConfigV2Lightly.  # noqa: E501
-        :type: DockerWorkerConfigV3LightlyCollate
+        :type: DockerWorkerConfigV2LightlyCollate
         """
 
         self._collate = collate
