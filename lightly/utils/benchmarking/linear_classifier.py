c6c72fafcc704dca29e5bd00ddc7bffda8f4fbe9
guarin <guarin@lightly.ai>
2023-04-25 08:22:00 +0000

    Add docstring for LinearClassifier

--- lightly/utils/benchmarking/linear_classifier.py
+++ lightly/utils/benchmarking/linear_classifier.py
@@ -20,6 +20,64 @@ class LinearClassifier(LightningModule):
         topk: Tuple[int, ...] = (1, 5),
         freeze_model: bool = False,
     ) -> None:
+        """Linear classifier for benchmarking.
+
+        Settings based on SimCLR [0].
+
+        - [0]: https://arxiv.org/abs/2002.05709
+
+        Args:
+            model:
+                Model used for feature extraction. Must define a forward(images) method
+                that returns a feature tensor.
+            batch_size:
+                Batch size per device.
+            feature_dim:
+                Dimension of features returned by forward method of model.
+            num_classes:
+                Number of classes in the dataset.
+            topk:
+                Tuple of integers defining the top-k accuracy metrics to compute.
+            freeze_model:
+                If True, the model is frozen and only the classification head is
+                trained. This corresponds to the linear eval setting. Set to False for
+                finetuning.
+
+        Examples:
+
+            >>> from pytorch_lightning import Trainer
+            >>> from torch import nn
+            >>> import torchvision
+            >>> from lightly.models import LinearClassifier
+            >>> from lightly.modles.modules import SimCLRProjectionHead
+            >>>
+            >>> class SimCLR(nn.Module):
+            >>>     def __init__(self):
+            >>>         super().__init__()
+            >>>         self.backbone = torchvision.models.resnet18()
+            >>>         self.backbone.fc = nn.Identity() # Ignore classification layer
+            >>>         self.projection_head = SimCLRProjectionHead(512, 512, 128)
+            >>>
+            >>>     def forward(self, x):
+            >>>         features = self.backbone(x).flatten(dim=1)
+            >>>         return features
+            >>>
+            >>> # Initialize a model.
+            >>> model = SimCLR()
+            >>>
+            >>> # Wrap it with a LinearClassifier.
+            >>> linear_classifier = LinearClassifier(
+            ...     model,
+            ...     batch_size=256,
+            ...     num_classes=10,
+            ...     freeze_model=True, # linear evaluation, set to False for finetune
+            ... )
+            >>>
+            >>> # Train the linear classifier.
+            >>> trainer = Trainer(max_epochs=90)
+            >>> trainer.fit(linear_classifier, train_dataloader, val_dataloader)
+
+        """
         super().__init__()
         self.save_hyperparameters(ignore="model")
 
@@ -33,13 +91,13 @@ class LinearClassifier(LightningModule):
         self.classification_head = Linear(feature_dim, num_classes)
         self.criterion = CrossEntropyLoss()
 
-    def forward(self, x: Tensor) -> Tensor:
-        features = self.model.forward(x).flatten(start_dim=1)
+    def forward(self, images: Tensor) -> Tensor:
+        features = self.model.forward(images).flatten(start_dim=1)
         return self.classification_head(features)
 
     def shared_step(self, batch, batch_idx) -> Tuple[Tensor, Dict[int, Tensor]]:
-        features, targets = batch[0], batch[1]
-        predictions = self.forward(features)
+        images, targets = batch[0], batch[1]
+        predictions = self.forward(images)
         loss = self.criterion(predictions, targets)
         _, predicted_labels = predictions.topk(max(self.topk))
         topk = mean_topk_accuracy(predicted_labels, targets, k=self.topk)
