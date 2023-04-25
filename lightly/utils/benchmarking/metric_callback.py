950fec3961937753df0c0b69211eca9f6043ae63
guarin <guarin@lightly.ai>
2023-04-25 09:59:03 +0000

    Add metric callback docstring

--- lightly/utils/benchmarking/metric_callback.py
+++ lightly/utils/benchmarking/metric_callback.py
@@ -7,6 +7,27 @@ from torch import Tensor
 
 
 class MetricCallback(Callback):
+    """Callback that collects log metrics from the LightningModule and stores them after
+    every epoch.
+
+    Example::
+
+        >>> from lightly.utils.benchmarking import MetricCallback
+        >>> from pytorch_lightning import LightningModule, Trainer
+        >>>
+        >>> class Model(LightningModule):
+        >>>     def training_step(self, batch, batch_idx):
+        >>>         ...
+        >>>         self.log("train_acc", acc)
+        >>>         ...
+        >>>
+        >>> metric_callback = MetricCallback()
+        >>> trainer = Trainer(callbacks=[metric_callback], max_epochs=10)
+        >>> trainer.fit(Model(), train_dataloder, val_dataloader)
+        >>>
+        >>> max_train_acc = max(metric_callback.get("train_acc"))
+    """
+
     def __init__(self):
         super().__init__()
         self.metrics = defaultdict(list)
@@ -24,6 +45,7 @@ class MetricCallback(Callback):
 
     def _append_metrics(self, trainer: Trainer) -> None:
         for name, value in trainer.callback_metrics.items():
+            # Only store scalar values.
             if isinstance(value, float) or (
                 isinstance(value, Tensor) and value.numel() == 1
             ):
