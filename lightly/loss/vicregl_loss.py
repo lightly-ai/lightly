addfefc2e44ad859202fe5fd425e473bc633f143
guarin <43336610+guarin@users.noreply.github.com>
2023-05-23 17:05:03 +0200

    Format VICReg Loss (#1241)

--- lightly/loss/vicregl_loss.py
+++ lightly/loss/vicregl_loss.py
@@ -1,8 +1,8 @@
 from typing import Optional, Sequence, Tuple
 
 import torch
-from torch import Tensor
 import torch.distributed as dist
+from torch import Tensor
 
 from lightly.loss.vicreg_loss import (
     VICRegLoss,
