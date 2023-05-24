5a4d9d043347ce8809c6b9fa0f0312235c2ff6ef
Shaun Daley <shaundaley39@gmail.com>
2023-05-24 14:51:47 +0200

    remove lightly-upload and lightly-magic upload options from CLI

--- lightly/cli/lightly_cli.py
+++ lightly/cli/lightly_cli.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""**Lightly Magic:** Train, embed, and upload in one command.
+"""**Lightly Magic:** Train and embed in one command.
 
 This module contains the entrypoint for the **lightly-magic**
 command-line interface.
@@ -14,7 +14,6 @@ from omegaconf import DictConfig
 from lightly.cli._helpers import fix_hydra_arguments
 from lightly.cli.embed_cli import _embed_cli
 from lightly.cli.train_cli import _train_cli
-from lightly.cli.upload_cli import _upload_cli
 from lightly.utils.hipify import print_as_warning
 
 
@@ -63,10 +62,6 @@ def _lightly_cli(cfg, is_cli_call=True):
     embeddings = _embed_cli(cfg, is_cli_call)
     cfg["embeddings"] = embeddings
 
-    if cfg["token"] and (cfg["dataset_id"] or cfg["new_dataset_name"]):
-        print("#" * 10 + " Starting to upload your dataset to the Lightly platform.")
-        _upload_cli(cfg)
-
     print("#" * 10 + " Finished")
 
 
@@ -83,21 +78,6 @@ def lightly_cli(cfg):
     Command-Line Args:
         input_dir:
             Path to the input directory where images are stored.
-        token:
-            User access token to the Lightly platform. If dataset_id
-            and token are specified, the images and embeddings are
-            uploaded to the platform.
-            (Required for upload)
-        dataset_id:
-            Identifier of the dataset on the Lightly platform. If
-            dataset_id and token are specified, the images and
-            embeddings are uploaded to the platform.
-            (Required for upload)
-        custom_metadata:
-            Path to a .json file containing custom metadata. The file must be in
-            the COCO annotations (although annotations can be empty) format and
-            contain an additional field `metadata` storing a list of metadata entries.
-            The metadata entries are matched with the images via `image_id`.
 
     Examples:
         >>> # train model and embed images with default settings
@@ -105,12 +85,6 @@ def lightly_cli(cfg):
         >>>
         >>> # train model for 10 epochs and embed images
         >>> lightly-magic input_dir=data/ trainer.max_epochs=10
-        >>>
-        >>> # train model, embed images, and upload to the Lightly platform
-        >>> lightly-magic input_dir=data/ token='123' dataset_id='XYZ'
-        >>>
-        >>> # upload images, embeddings, and custom metadata
-        >>> lightly-magic input_dir=data/ token='123' dataset_id='XYZ' custom_metadata=custom_metadata.json
 
 
     """
