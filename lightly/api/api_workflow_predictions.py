bb99ee51de0423b5fc722c69f090dd88e0172b9e
MalteEbner <malte.ebner@gmail.com>
2023-07-13 11:06:42 +0200

    remove create_or_update_predictions (#1313)

--- lightly/api/api_workflow_predictions.py
+++ lightly/api/api_workflow_predictions.py
@@ -1,7 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor
-from typing import Mapping, Optional, Sequence, Tuple
-
-import tqdm
+from typing import Sequence
 
 from lightly.openapi_generated.swagger_client.models import (
     PredictionSingleton,
@@ -54,92 +51,6 @@ class _PredictionsMixin:
             dataset_id=self.dataset_id,
             prediction_uuid_timestamp=prediction_version_id,
         )
-
-    def create_or_update_predictions(
-        self,
-        sample_id_to_prediction_singletons: Mapping[str, Sequence[PredictionSingleton]],
-        prediction_version_id: int = -1,
-        progress_bar: Optional[tqdm.tqdm] = None,
-        max_workers: int = 8,
-    ) -> None:
-        """Creates or updates the predictions for specific samples.
-
-        Args:
-            sample_id_to_prediction_singletons
-                A mapping from the sample_id of the sample to its corresponding prediction singletons.
-                The singletons can be from different tasks and different types.
-
-            prediction_version_id:
-                A numerical ID (e.g., timestamp) to distinguish different predictions of different model versions.
-                Use the same id if you don't require versioning or if you wish to overwrite the previous schema.
-                This ID must match the ID of a prediction task schema.
-
-            progress_bar:
-                Tqdm progress bar to show how many prediction files have already been uploaded.
-
-            max_workers:
-                Maximum number of workers uploading predictions in parallel.
-
-        Example:
-          >>> import time
-          >>> from tqdm import tqdm
-          >>> from typing import List
-          >>> from lightly.api import ApiWorkflowClient
-          >>> from lightly.api.utils import paginate_endpoint
-          >>> from lightly.openapi_generated.swagger_client.models import (
-          >>>     PredictionTaskSchema,
-          >>>     TaskType,
-          >>>     PredictionTaskSchemaCategory,
-          >>>     SampleDataModes,
-          >>> )
-          >>> from lightly.api.prediction_singletons import PredictionSingletonClassificationRepr
-          >>>
-          >>> client = ApiWorkflowClient(
-          >>>     token="MY_LIGHTLY_TOKEN", dataset_id="MY_DATASET_ID"
-          >>> )
-          >>>
-          >>> samples: List[SampleDataModes] = list(
-          >>>     paginate_endpoint(
-          >>>         client._samples_api.get_samples_partial_by_dataset_id,
-          >>>         page_size=25000,
-          >>>         dataset_id=client.dataset_id,
-          >>>         mode=SamplePartialMode.FILENAMES,
-          >>>     )
-          >>> )
-          >>> sample_id_to_prediction_singletons_dummy = {
-          >>>     sample.id: [PredictionSingletonClassificationRepr(taskName="my-task", categoryId=i%4, score=0.9, probabilities=[0.1, 0.2, 0.3, 0.4])]
-          >>>     for i, sample in enumerate(samples)
-          >>> }
-          >>> client.create_or_update_predictions(
-          >>>     sample_id_to_prediction_singletons=sample_id_to_prediction_singletons_dummy,
-          >>>     progress_bar=tqdm(desc="Uploading predictions", total=len(samples), unit=" predictions")
-          >>> )
-
-
-        """
-
-        # handle the case where len(sample_id_to_prediction_singletons) < max_workers
-        max_workers = min(len(sample_id_to_prediction_singletons), max_workers)
-        max_workers = max(max_workers, 1)
-
-        def upload_prediction(
-            sample_id_prediction_singletons_tuple: Tuple[
-                str, Sequence[PredictionSingleton]
-            ]
-        ) -> None:
-            (sample_id, prediction_singletons) = sample_id_prediction_singletons_tuple
-            self.create_or_update_prediction(
-                sample_id=sample_id,
-                prediction_singletons=prediction_singletons,
-                prediction_version_id=prediction_version_id,
-            )
-
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            for _ in executor.map(
-                upload_prediction, sample_id_to_prediction_singletons.items()
-            ):
-                if progress_bar is not None:
-                    progress_bar.update(1)
 
     def create_or_update_prediction(
         self,
