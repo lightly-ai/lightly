c358c3b153eb439901b842a2fde4029ade484a01
Natyren <bredis.georgy@yandex.ru>
2023-07-15 12:11:56 +0300

    fix typo

--- lightly/models/modules/ijepa.py
+++ lightly/models/modules/ijepa.py
@@ -69,12 +69,15 @@ class IJEPAPredictor(vision_transformer.Encoder):
         self.predictor_embed = nn.Linear(mlp_dim, predictor_embed_dim, bias=True)
         self.mask_token = nn.Parameter(torch.zeros(1, 1, predictor_embed_dim))
         self.predictor_proj = nn.Linear(predictor_embed_dim, mlp_dim, bias=True)
-        self.predictor_pos_embed = nn.Parameter(torch.zeros(1, num_patches, predictor_embed_dim),
-                                                requires_grad=False)
-        predictor_pos_embed = utils._get_2d_sincos_pos_embed(self.predictor_pos_embed.shape[-1],
-                                                      int(num_patches**.5),
-                                                      cls_token=False)
-        self.predictor_pos_embed.data.copy_(torch.from_numpy(predictor_pos_embed).float().unsqueeze(0))
+        self.predictor_pos_embed = nn.Parameter(
+             torch.zeros(1, num_patches, predictor_embed_dim), requires_grad=False
+        )
+        predictor_pos_embed = utils._get_2d_sincos_pos_embed(
+             self.predictor_pos_embed.shape[-1], int(num_patches**0.5), cls_token=False
+        )
+        self.predictor_pos_embed.data.copy_(
+             torch.from_numpy(predictor_pos_embed).float().unsqueeze(0)
+        )
 
     @classmethod
     def from_vit_encoder(cls, vit_encoder, num_patches):
