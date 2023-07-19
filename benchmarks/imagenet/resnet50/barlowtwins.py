3a3c6b61f31550251416ee65cd425cf3f223b259
Igor Susmelj <igor_7779@hotmail.com>
2023-07-19 21:44:36 +0000

    Update model

--- benchmarks/imagenet/resnet50/barlowtwins.py
+++ benchmarks/imagenet/resnet50/barlowtwins.py
@@ -10,7 +10,7 @@ from torchvision.models import resnet50
 from lightly.loss import BarlowTwinsLoss
 from lightly.models.modules import BarlowTwinsProjectionHead
 from lightly.models.utils import get_weight_decay_parameters, update_momentum
-from lightly.transforms import SimCLRTransform
+from lightly.transforms import BYOLTransform
 from lightly.utils.benchmarking import OnlineLinearClassifier
 from lightly.utils.lars import LARS
 from lightly.utils.scheduler import CosineWarmupScheduler, cosine_schedule
@@ -25,64 +25,31 @@ class BarlowTwins(LightningModule):
         resnet = resnet50()
         resnet.fc = Identity()  # Ignore classification head
         self.backbone = resnet
-        self.projection_head = BYOLProjectionHead()
-        self.student_backbone = copy.deepcopy(self.backbone)
-        self.student_projection_head = BYOLProjectionHead()
-        self.student_prediction_head = BYOLPredictionHead()
-        self.criterion = NegativeCosineSimilarity()
+        self.projection_head = BarlowTwinsProjectionHead()
+        self.criterion = BarlowTwinsLoss(lambda_param=5e-3, gather_distributed=True)
 
         self.online_classifier = OnlineLinearClassifier(num_classes=num_classes)
 
     def forward(self, x: Tensor) -> Tensor:
         return self.backbone(x)
 
-    @torch.no_grad()
-    def forward_teacher(self, x: Tensor) -> Tuple[Tensor, Tensor]:
-        features = self(x).flatten(start_dim=1)
-        projections = self.projection_head(features)
-        return features, projections
-
-    def forward_student(self, x: Tensor) -> Tensor:
-        features = self.student_backbone(x).flatten(start_dim=1)
-        projections = self.student_projection_head(features)
-        predictions = self.student_prediction_head(projections)
-        return predictions
-
     def training_step(
         self, batch: Tuple[List[Tensor], Tensor, List[str]], batch_idx: int
     ) -> Tensor:
-        # Momentum update teacher.
-        # Settings follow original code for 100 epochs which are slightly different
-        # from the paper, see:
-        # https://github.com/deepmind/deepmind-research/blob/f5de0ede8430809180254ee957abf36ed62579ef/byol/configs/byol.py#L21-L23
-        momentum = cosine_schedule(
-            step=self.trainer.global_step,
-            max_steps=self.trainer.estimated_stepping_batches,
-            start_value=0.99,
-            end_value=1.0,
-        )
-        update_momentum(self.student_backbone, self.backbone, m=momentum)
-        update_momentum(self.student_projection_head, self.projection_head, m=momentum)
-
         # Forward pass and loss calculation.
         views, targets = batch[0], batch[1]
-        teacher_features_0, teacher_projections_0 = self.forward_teacher(views[0])
-        _, teacher_projections_1 = self.forward_teacher(views[1])
-        student_predictions_0 = self.forward_student(views[0])
-        student_predictions_1 = self.forward_student(views[1])
-        # NOTE: Factor 2 because: L2(norm(x), norm(y)) = 2 - 2 * cossim(x, y)
-        loss_0 = 2 * self.criterion(teacher_projections_0, student_predictions_1)
-        loss_1 = 2 * self.criterion(teacher_projections_1, student_predictions_0)
-        # NOTE: No mean because original code only takes mean over batch dimension, not
-        # views.
-        loss = loss_0 + loss_1
+        features = self.forward(torch.cat(views)).flatten(start_dim=1)
+        z = self.projection_head(features)
+        z0, z1 = z.chunk(len(views))
+        loss = self.criterion(z0, z1)
+
         self.log(
             "train_loss", loss, prog_bar=True, sync_dist=True, batch_size=len(targets)
         )
 
         # Online linear evaluation.
         cls_loss, cls_log = self.online_classifier.training_step(
-            (teacher_features_0.detach(), targets), batch_idx
+            (features.detach(), targets.repeat(len(views))), batch_idx
         )
         self.log_dict(cls_log, sync_dist=True, batch_size=len(targets))
         return loss + cls_loss
@@ -99,22 +66,21 @@ class BarlowTwins(LightningModule):
         return cls_loss
 
     def configure_optimizers(self):
+        lr_factor = self.batch_size_per_device * self.trainer.world_size / 256
+
         # Don't use weight decay for batch norm, bias parameters, and classification
         # head to improve performance.
         params, params_no_weight_decay = get_weight_decay_parameters(
-            [
-                self.student_backbone,
-                self.student_projection_head,
-                self.student_prediction_head,
-            ]
+            [self.backbone, self.projection_head]
         )
         optimizer = LARS(
             [
-                {"name": "byol", "params": params},
+                {"name": "barlowtwins", "params": params},
                 {
-                    "name": "byol_no_weight_decay",
+                    "name": "barlowtwins_no_weight_decay",
                     "params": params_no_weight_decay,
                     "weight_decay": 0.0,
+                    "lr": 0.0048 * lr_factor,
                 },
                 {
                     "name": "online_classifier",
@@ -122,13 +88,11 @@ class BarlowTwins(LightningModule):
                     "weight_decay": 0.0,
                 },
             ],
-            # Settings follow original code for 100 epochs which are slightly different
-            # from the paper, see:
-            # https://github.com/deepmind/deepmind-research/blob/f5de0ede8430809180254ee957abf36ed62579ef/byol/configs/byol.py#L21-L23
-            lr=0.45 * self.batch_size_per_device * self.trainer.world_size / 256,
+            lr=0.2 * lr_factor,
             momentum=0.9,
-            weight_decay=1e-6,
+            weight_decay=1.5e-6,
         )
+
         scheduler = {
             "scheduler": CosineWarmupScheduler(
                 optimizer=optimizer,
@@ -144,5 +108,5 @@ class BarlowTwins(LightningModule):
         return [optimizer], [scheduler]
 
 
-# BYOL uses same transform as SimCLR.
-transform = SimCLRTransform()
+# BarlowTwins uses same transform as BYOL.
+transform = BYOLTransform()
