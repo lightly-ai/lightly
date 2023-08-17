5a5a7c7dca897b7270ff69a90be3e1c588141326
Igor Susmelj <igor_7779@hotmail.com>
2023-08-17 21:59:22 +0000

    Make sure we average if we reduce across GPUs

--- lightly/loss/barlow_twins_loss.py
+++ lightly/loss/barlow_twins_loss.py
@@ -64,6 +64,7 @@ class BarlowTwinsLoss(torch.nn.Module):
         if self.gather_distributed and dist.is_initialized():
             world_size = dist.get_world_size()
             if world_size > 1:
+                c = c / world_size
                 dist.all_reduce(c)
 
         invariance_loss = torch.diagonal(c).add_(-1).pow_(2).sum()
