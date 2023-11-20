abbb9cd0639a3661581b87291688133a556264a2
philippmwirth <philippmwirth@gmail.com>
2023-11-20 12:53:57 +0000

    Fix

--- lightly/models/modules/nn_memory_bank.py
+++ lightly/models/modules/nn_memory_bank.py
@@ -21,8 +21,7 @@ class NNMemoryBankModule(MemoryBankModule):
 
     Attributes:
         size:
-            Number of keys the memory bank can store. If set to 0,
-            memory bank is not used.
+            Number of keys the memory bank can store.
 
     Examples:
         >>> model = NNCLR(backbone)
@@ -40,6 +39,8 @@ class NNMemoryBankModule(MemoryBankModule):
     """
 
     def __init__(self, size: int = 2**16):
+        if size <= 0:
+            raise ValueError(f"Memory bank size must be positive, got {size}.")
         super(NNMemoryBankModule, self).__init__(size)
 
     def forward(
@@ -57,6 +58,7 @@ class NNMemoryBankModule(MemoryBankModule):
         """
 
         output, bank = super(NNMemoryBankModule, self).forward(output, update=update)
+        assert bank is not None
         bank = bank.to(output.device).t()
 
         output_normed = torch.nn.functional.normalize(output, dim=1)
