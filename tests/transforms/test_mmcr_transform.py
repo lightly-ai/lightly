c5515126d01003ba1fcf4ad5f9607f9c8b0d861c
John Sutor <johnsutor3@gmail.com>
2023-12-12 08:16:38 -0500

    Remove test using PyTorch Tensor

--- tests/transforms/test_mmcr_transform.py
+++ tests/transforms/test_mmcr_transform.py
@@ -1,5 +1,4 @@
 import pytest
-import torch
 from PIL import Image
 
 from lightly.transforms.mmcr_transform import MMCRTransform
@@ -13,13 +12,6 @@ def test_raise_value_error() -> None:
 def test_num_views() -> None:
     multi_view_transform = MMCRTransform(k=3)
     assert len(multi_view_transform.transforms) == 3
-
-
-def test_multi_view_on_tensor() -> None:
-    multi_view_transform = MMCRTransform(k=3)
-    sample = torch.rand(3, 100, 100)
-    output = multi_view_transform(sample)
-    assert len(output) == 3
 
 
 def test_multi_view_on_pil_image() -> None:
