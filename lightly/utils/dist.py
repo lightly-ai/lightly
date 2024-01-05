1f4c5a899d414ad5627f735cd18705af3779dd38
guarin <guarin@lightly.ai>
2024-01-05 13:55:54 +0000

    Fix typings in dist.py

--- lightly/utils/dist.py
+++ lightly/utils/dist.py
@@ -1,10 +1,13 @@
-from typing import Optional, Tuple
+from typing import Any, Callable, Literal, Optional, Tuple, TypeVar, Union
+from torch.autograd import Function
+from torch.autograd.function import FunctionCtx
 
 import torch
 import torch.distributed as dist
+from torch import Tensor
 
 
-class GatherLayer(torch.autograd.Function):
+class GatherLayer(Function):
     """Gather tensors from all processes, supporting backward propagation.
 
     This code was taken and adapted from here:
@@ -13,15 +16,16 @@ class GatherLayer(torch.autograd.Function):
     """
 
     @staticmethod
-    def forward(ctx, input: torch.Tensor) -> Tuple[torch.Tensor, ...]:
+    def forward(ctx: FunctionCtx, input: Tensor) -> Tuple[Tensor, ...]:
         ctx.save_for_backward(input)
         output = [torch.empty_like(input) for _ in range(dist.get_world_size())]
         dist.all_gather(output, input)
         return tuple(output)
 
     @staticmethod
-    def backward(ctx, *grads: torch.Tensor) -> torch.Tensor:
-        (input,) = ctx.saved_tensors
+    def backward(ctx: FunctionCtx, *grads: Tensor) -> Tensor:
+        # Type ignore because FunctionCtx is not fully typed.
+        (input,) = ctx.saved_tensors  # type: ignore
         grad_out = torch.empty_like(input)
         grad_out[:] = grads[dist.get_rank()]
         return grad_out
@@ -37,12 +41,13 @@ def world_size() -> int:
     return dist.get_world_size() if dist.is_initialized() else 1
 
 
-def gather(input: torch.Tensor) -> Tuple[torch.Tensor]:
+def gather(input: Tensor) -> Tuple[Tensor]:
     """Gathers this tensor from all processes. Supports backprop."""
-    return GatherLayer.apply(input)
+    # Type ignore is required because Function.apply is untyped.
+    return GatherLayer.apply(input)  # type: ignore
 
 
-def eye_rank(n: int, device: Optional[torch.device] = None) -> torch.Tensor:
+def eye_rank(n: int, device: Optional[torch.device] = None) -> Tensor:
     """Returns an (n, n * world_size) zero matrix with the diagonal for the rank
     of this process set to 1.
 
@@ -70,7 +75,12 @@ def eye_rank(n: int, device: Optional[torch.device] = None) -> torch.Tensor:
     return diag_mask
 
 
-def rank_zero_only(fn):
+_T = TypeVar("_T")
+
+
+# TODO(Guarin, 01/2024): Refine typings for callable with ParamSpec once we drop support
+# for Python <=3.9.
+def rank_zero_only(fn: Callable[..., _T]) -> Callable[..., Union[_T, None]]:
     """Decorator that only runs the function on the process with rank 0.
 
     Example:
@@ -82,14 +92,22 @@ def rank_zero_only(fn):
 
     """
 
-    def wrapped(*args, **kwargs):
+    def wrapped(*args: Any, **kwargs: Any) -> Union[_T, None]:
         if rank() == 0:
             return fn(*args, **kwargs)
+        return None
 
     return wrapped
 
 
+# Type ignore required because 'file' has Any type.
 @rank_zero_only
-def print_rank_zero(*args, **kwargs) -> None:
+def print_rank_zero(  # type: ignore[misc]
+    *values: object,
+    sep: Union[str, None] = " ",
+    end: Union[str, None] = "\n",
+    file: Any = None,
+    flush: Literal[False] = False
+) -> None:
     """Equivalent to print, but only runs on the process with rank 0."""
-    print(*args, **kwargs)
+    print(*values, sep=sep, end=end, file=file, flush=flush)
