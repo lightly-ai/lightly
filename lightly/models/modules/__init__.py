60640d8e205eebae903a8b16c84f3e6f6a8818e4
guarin <43336610+guarin@users.noreply.github.com>
2024-02-01 09:27:30 +0100

    Add timm version check (#1487)
    * Only import AIM model parts if timm >= 0.9.9

--- lightly/models/modules/__init__.py
+++ lightly/models/modules/__init__.py
@@ -8,7 +8,7 @@ can be combined with any lightly model.
 # Copyright (c) 2021. Lightly AG and its affiliates.
 # All Rights Reserved
 
-from lightly import _timm_available, _torchvision_vit_available
+
 from lightly.models.modules.heads import (
     BarlowTwinsProjectionHead,
     BYOLPredictionHead,
@@ -27,16 +27,17 @@ from lightly.models.modules.heads import (
     SwaVPrototypes,
 )
 from lightly.models.modules.nn_memory_bank import NNMemoryBankModule
+from lightly.utils import dependency as _dependency
 
-if _torchvision_vit_available:
+if _dependency.torchvision_vit_available():
     # Requires torchvision >=0.12
     from lightly.models.modules.masked_autoencoder import (
         MAEBackbone,
         MAEDecoder,
         MAEEncoder,
     )
-if _timm_available:
-    # Requires timm.
+if _dependency.timm_vit_available():
+    # Requires timm >= 0.9.9
     from lightly.models.modules.heads_timm import AIMPredictionHead
     from lightly.models.modules.masked_causal_vision_transformer import (
         MaskedCausalVisionTransformer,
