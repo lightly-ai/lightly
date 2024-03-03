b38935c2ba723d19491a8a0a7d911d9d9351af08
Ersi <ersi@lightly.ai>
2024-03-03 13:07:58 +0000

    added docstring

--- lightly/models/modules/masked_vision_transformer.py
+++ lightly/models/modules/masked_vision_transformer.py
@@ -5,6 +5,11 @@ from torch import Tensor
 
 
 class MaskedVisionTransformer(ABC):
+    """
+    Abstract base class for Masked Vision Transformer models.
+
+    Defines the interface for a Masked Vision Transformer. This class includes abstract methods that must be implemented by concrete subclasses to define the forward pass, tokenization of images, and various operations needed for the transformer.
+    """
     @abstractmethod
     def forward(
         self,
