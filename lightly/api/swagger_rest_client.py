e16c1f0e2095d97b3f7138c8a599b90ec2a79401
MalteEbner <malte.ebner@gmail.com>
2024-04-11 13:13:27 +0200

    add PrettyPrintApiException for more nicely formatted errors (#1520)

--- lightly/api/swagger_rest_client.py
+++ lightly/api/swagger_rest_client.py
@@ -1,7 +1,43 @@
+import json
+from json import JSONDecodeError
 from typing import Any, Dict, Optional, Tuple, Union
 
 from lightly.openapi_generated.swagger_client.api_client import Configuration
+from lightly.openapi_generated.swagger_client.exceptions import ApiException
 from lightly.openapi_generated.swagger_client.rest import RESTClientObject
+
+
+class PrettyPrintApiException(ApiException):
+    def __init__(self, current_exception: ApiException):
+        super().__init__(current_exception.status, current_exception.reason)
+        self.body = current_exception.body
+        self.headers = current_exception.headers
+
+    def __str__(self) -> str:
+        error_message = "\n"
+        error_message += "#" * 100
+        error_message += "\n"
+        error_message += f"Error Code: {self.status}"
+        error_message += "\n"
+        error_message += f"Error Reason: {self.reason}"
+        error_message += "\n"
+        error_message += "\n"
+        try:
+            error_body_dict = json.loads(self.body)
+        except JSONDecodeError:
+            pass
+        else:
+            if "error" in error_body_dict:
+                error_message += f"Error Message: {error_body_dict['error']}"
+
+        error_message += "\n"
+
+        error_message += "#" * 100
+
+        # make the error message red
+        error_message = f"\033[91m{error_message}\033[0m"
+
+        return error_message
 
 
 class PatchRESTClientObjectMixin:
@@ -61,16 +97,19 @@ class PatchRESTClientObjectMixin:
             _request_timeout = self.timeout
 
         # Call RESTClientObject.request
-        return super().request(
-            method=method,
-            url=url,
-            query_params=query_params,
-            headers=headers,
-            body=body,
-            post_params=post_params,
-            _preload_content=_preload_content,
-            _request_timeout=_request_timeout,
-        )
+        try:
+            return super().request(
+                method=method,
+                url=url,
+                query_params=query_params,
+                headers=headers,
+                body=body,
+                post_params=post_params,
+                _preload_content=_preload_content,
+                _request_timeout=_request_timeout,
+            )
+        except ApiException as e:
+            raise PrettyPrintApiException(e) from None
 
     def __getstate__(self) -> Dict[str, Any]:
         """__getstate__ method for pickling."""
