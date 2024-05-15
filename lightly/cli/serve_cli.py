3484416224093c449ac90673425cb907a4770e64
guarin <43336610+guarin@users.noreply.github.com>
2024-05-15 13:11:16 +0200

    Validate input and lightly mount (#1537)
    * Validate input and lightly mount

--- lightly/cli/serve_cli.py
+++ lightly/cli/serve_cli.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import hydra
 
 from lightly.api import serve
+from lightly.api.serve import validate_input_mount, validate_lightly_mount
 from lightly.cli._helpers import fix_hydra_arguments
 from lightly.utils.hipify import bcolors
 
@@ -28,15 +29,26 @@ def lightly_serve(cfg):
 
     """
     if not cfg.input_mount:
-        print("Please provide a valid input mount. Use --help for more information.")
+        print(
+            "Please provide a valid 'input_mount' argument. Use --help for more "
+            "information."
+        )
         sys.exit(1)
 
     if not cfg.lightly_mount:
-        print("Please provide a valid Lightly mount. Use --help for more information.")
+        print(
+            "Please provide a valid 'lightly_mount' argument. Use --help for more "
+            "information."
+        )
         sys.exit(1)
 
+    input_mount = Path(cfg.input_mount)
+    validate_input_mount(input_mount=input_mount)
+    lightly_mount = Path(cfg.lightly_mount)
+    validate_lightly_mount(lightly_mount=lightly_mount)
+
     httpd = serve.get_server(
-        paths=[Path(cfg.input_mount), Path(cfg.lightly_mount)],
+        paths=[input_mount, lightly_mount],
         host=cfg.host,
         port=cfg.port,
     )
