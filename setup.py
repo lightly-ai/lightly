c40f61d2a8979ca7a6b2b1798ae7fbc9b6071f58
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-06-24 18:53:14 +0530

    update comments in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 
-# for packaging tools not supporting, e.g., PEP 517, PEP 660
+# For compatibility with legacy builds or versions of tools
+# that don’t support certain packaging standards (e.g. PEP 517
+# or PEP 660). The configuration is kept in pyproject.toml
 setup()
