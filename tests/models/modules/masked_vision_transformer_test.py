0271d9891adf3e5c58412e1521ed259df33bf678
guarin <guarin@lightly.ai>
2024-07-24 09:50:46 +0000

    Format

--- tests/models/modules/masked_vision_transformer_test.py
+++ tests/models/modules/masked_vision_transformer_test.py
@@ -25,18 +25,20 @@ class MaskedVisionTransformerTest(ABC):
         mask_token: Optional[Parameter] = None,
         antialias: bool = True,
         weight_initialization: str = "",
-    ) -> MaskedVisionTransformer: ...
+    ) -> MaskedVisionTransformer:
+        ...
 
     @abstractmethod
-    def test__init__mask_token(self, mask_token: Optional[Parameter]) -> None: ...
+    def test__init__mask_token(self, mask_token: Optional[Parameter]) -> None:
+        ...
 
     @abstractmethod
-    def test__init__weight_initialization(self, mocker: MockerFixture) -> None: ...
+    def test__init__weight_initialization(self, mocker: MockerFixture) -> None:
+        ...
 
     @abstractmethod
-    def test__init__weight_initialization__skip(
-        self, mocker: MockerFixture
-    ) -> None: ...
+    def test__init__weight_initialization__skip(self, mocker: MockerFixture) -> None:
+        ...
 
     def test__init__weight_initialization__invalid(self) -> None:
         with pytest.raises(ValueError):
