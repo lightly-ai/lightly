b0b541be829b3b5d10ef1955d84d6a6ad2a911bf
fadkeabhi <31249309+fadkeabhi@users.noreply.github.com>
2024-10-08 22:23:48 +0530

    Move public classes and functions to top in lightly/data

--- lightly/data/_video.py
+++ lightly/data/_video.py
@@ -304,137 +304,6 @@ class VideoLoader(threading.local):
         return image
 
 
-class _TimestampFpsFromVideosDataset(Dataset):
-    def __init__(self, video_instances: List[str], pts_unit: str):
-        self.video_instances = video_instances
-        self.pts_unit = pts_unit
-
-    def __len__(self):
-        return len(self.video_instances)
-
-    def __getitem__(self, index):
-        instance = self.video_instances[index]
-        ts, fps = io.read_video_timestamps(instance, pts_unit=self.pts_unit)
-        return ts, fps
-
-
-def _make_dataset(
-    directory,
-    extensions=None,
-    is_valid_file=None,
-    pts_unit="sec",
-    tqdm_args=None,
-    num_workers: int = 0,
-):
-    """Returns a list of all video files, timestamps, and offsets.
-
-    Args:
-        directory:
-            Root directory path (should not contain subdirectories).
-        extensions:
-            Tuple of valid extensions.
-        is_valid_file:
-            Used to find valid files.
-        pts_unit:
-            Unit of the timestamps.
-        tqdm_args:
-            arguments to pass to tqdm
-        num_workers:
-            number of workers to use for multithreading
-
-    Returns:
-        A list of video files, timestamps, frame offsets, and fps.
-
-    """
-
-    if tqdm_args is None:
-        tqdm_args = {}
-    if extensions is None:
-        if is_valid_file is None:
-            ValueError("Both extensions and is_valid_file cannot be None")
-        else:
-            _is_valid_file = is_valid_file
-    else:
-
-        def is_valid_file_extension(filepath):
-            return filepath.lower().endswith(extensions)
-
-        if is_valid_file is None:
-            _is_valid_file = is_valid_file_extension
-        else:
-
-            def _is_valid_file(filepath):
-                return is_valid_file_extension(filepath) and is_valid_file(filepath)
-
-    # find all video instances (no subdirectories)
-    video_instances = []
-
-    def on_error(error):
-        raise error
-
-    for root, _, files in os.walk(directory, onerror=on_error):
-        for fname in files:
-            # skip invalid files
-            if not _is_valid_file(os.path.join(root, fname)):
-                continue
-
-            # keep track of valid files
-            path = os.path.join(root, fname)
-            video_instances.append(path)
-
-    # define loader to get the timestamps
-    num_workers = min(num_workers, len(video_instances))
-    if len(video_instances) == 1:
-        num_workers = 0
-    loader = DataLoader(
-        _TimestampFpsFromVideosDataset(video_instances, pts_unit=pts_unit),
-        num_workers=num_workers,
-        batch_size=None,
-        shuffle=False,
-    )
-
-    # actually load the data
-    tqdm_args = dict(tqdm_args)
-    tqdm_args.setdefault("unit", " video")
-    tqdm_args.setdefault("desc", "Counting frames in videos")
-    timestamps_fpss = list(tqdm(loader, **tqdm_args))
-    timestamps, fpss = zip(*timestamps_fpss)
-
-    # get frame offsets
-    frame_counts = [len(ts) for ts in timestamps]
-    offsets = [0] + list(np.cumsum(frame_counts[:-1]))
-
-    return video_instances, timestamps, offsets, fpss
-
-
-def _find_non_increasing_timestamps(timestamps: List[Fraction]) -> List[bool]:
-    """Finds all non-increasing timestamps.
-
-    Arguments:
-        timestamps:
-            Video frame timestamps.
-
-    Returns:
-        A boolean for each input timestamp which is True if the timestamp is
-        non-increasing and False otherwise.
-
-    """
-    if len(timestamps) == 0:
-        return []
-    is_non_increasing = np.zeros(
-        shape=len(timestamps),
-        dtype=bool,
-    )
-    max_timestamp = timestamps[0] - 1
-    for i, timestamp in enumerate(timestamps):
-        if timestamp > max_timestamp:
-            max_timestamp = timestamp
-        else:
-            is_non_increasing[i] = True
-
-    return list(is_non_increasing)
-
-
 class VideoDataset(datasets.VisionDataset):
     """Implementation of a video dataset.
 
@@ -732,3 +601,134 @@ class VideoDataset(datasets.VisionDataset):
                 self._video_index = video_index
 
             return self._video_loader
+
+
+class _TimestampFpsFromVideosDataset(Dataset):
+    def __init__(self, video_instances: List[str], pts_unit: str):
+        self.video_instances = video_instances
+        self.pts_unit = pts_unit
+
+    def __len__(self):
+        return len(self.video_instances)
+
+    def __getitem__(self, index):
+        instance = self.video_instances[index]
+        ts, fps = io.read_video_timestamps(instance, pts_unit=self.pts_unit)
+        return ts, fps
+
+
+def _make_dataset(
+    directory,
+    extensions=None,
+    is_valid_file=None,
+    pts_unit="sec",
+    tqdm_args=None,
+    num_workers: int = 0,
+):
+    """Returns a list of all video files, timestamps, and offsets.
+
+    Args:
+        directory:
+            Root directory path (should not contain subdirectories).
+        extensions:
+            Tuple of valid extensions.
+        is_valid_file:
+            Used to find valid files.
+        pts_unit:
+            Unit of the timestamps.
+        tqdm_args:
+            arguments to pass to tqdm
+        num_workers:
+            number of workers to use for multithreading
+
+    Returns:
+        A list of video files, timestamps, frame offsets, and fps.
+
+    """
+
+    if tqdm_args is None:
+        tqdm_args = {}
+    if extensions is None:
+        if is_valid_file is None:
+            ValueError("Both extensions and is_valid_file cannot be None")
+        else:
+            _is_valid_file = is_valid_file
+    else:
+
+        def is_valid_file_extension(filepath):
+            return filepath.lower().endswith(extensions)
+
+        if is_valid_file is None:
+            _is_valid_file = is_valid_file_extension
+        else:
+
+            def _is_valid_file(filepath):
+                return is_valid_file_extension(filepath) and is_valid_file(filepath)
+
+    # find all video instances (no subdirectories)
+    video_instances = []
+
+    def on_error(error):
+        raise error
+
+    for root, _, files in os.walk(directory, onerror=on_error):
+        for fname in files:
+            # skip invalid files
+            if not _is_valid_file(os.path.join(root, fname)):
+                continue
+
+            # keep track of valid files
+            path = os.path.join(root, fname)
+            video_instances.append(path)
+
+    # define loader to get the timestamps
+    num_workers = min(num_workers, len(video_instances))
+    if len(video_instances) == 1:
+        num_workers = 0
+    loader = DataLoader(
+        _TimestampFpsFromVideosDataset(video_instances, pts_unit=pts_unit),
+        num_workers=num_workers,
+        batch_size=None,
+        shuffle=False,
+    )
+
+    # actually load the data
+    tqdm_args = dict(tqdm_args)
+    tqdm_args.setdefault("unit", " video")
+    tqdm_args.setdefault("desc", "Counting frames in videos")
+    timestamps_fpss = list(tqdm(loader, **tqdm_args))
+    timestamps, fpss = zip(*timestamps_fpss)
+
+    # get frame offsets
+    frame_counts = [len(ts) for ts in timestamps]
+    offsets = [0] + list(np.cumsum(frame_counts[:-1]))
+
+    return video_instances, timestamps, offsets, fpss
+
+
+def _find_non_increasing_timestamps(timestamps: List[Fraction]) -> List[bool]:
+    """Finds all non-increasing timestamps.
+
+    Arguments:
+        timestamps:
+            Video frame timestamps.
+
+    Returns:
+        A boolean for each input timestamp which is True if the timestamp is
+        non-increasing and False otherwise.
+
+    """
+    if len(timestamps) == 0:
+        return []
+    is_non_increasing = np.zeros(
+        shape=len(timestamps),
+        dtype=bool,
+    )
+    max_timestamp = timestamps[0] - 1
+    for i, timestamp in enumerate(timestamps):
+        if timestamp > max_timestamp:
+            max_timestamp = timestamp
+        else:
+            is_non_increasing[i] = True
+
+    return list(is_non_increasing)
