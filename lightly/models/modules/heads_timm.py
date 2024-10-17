94023b02c5fa9348381eed889ae40b98487963b6
Ayush Shri <ayushshrivastava585@gmail.com>
2024-10-18 01:19:35 +0530

    cleanup_doc

--- lightly/models/modules/heads_timm.py
+++ lightly/models/modules/heads_timm.py
@@ -9,6 +9,15 @@ class AIMPredictionHeadBlock(Module):
     """Prediction head block for AIM [0].
 
     - [0]: AIM, 2024, https://arxiv.org/abs/2401.08541
+
+    Args:
+        input_dim: Dimensionality of the input features.
+        output_dim: Dimensionality of the output features.
+        mlp_ratio: Ratio used to determine the hidden layer size in the MLP.
+        proj_drop: Dropout rate for the projection layer.
+        act_layer: Activation layer to use.
+        norm_layer: Normalization layer to use.
+        mlp_layer: MLP layer to use.
     """
 
     def __init__(
@@ -21,6 +30,8 @@ class AIMPredictionHeadBlock(Module):
         norm_layer: Type[Module] = LayerNorm,
         mlp_layer: Type[Module] = Mlp,
     ) -> None:
+        """Initializes the AIMPredictionHeadBlock module with the specified parameters."""
+
         super().__init__()
         self.norm = norm_layer(input_dim)  # type: ignore[call-arg]
         self.mlp = mlp_layer(  # type: ignore[call-arg]
@@ -33,6 +44,14 @@ class AIMPredictionHeadBlock(Module):
         )
 
     def forward(self, x: Tensor) -> Tensor:
+        """Forward pass of the AIMPredictionHeadBlock.
+
+        Args:
+            x: Input tensor.
+
+        Returns:
+            Output tensor after applying the MLP and normalization.
+        """
         x = x + self.mlp(self.norm(x))
         return x
 
@@ -41,6 +60,18 @@ class AIMPredictionHead(Module):
     """Prediction head for AIM [0].
 
     - [0]: AIM, 2024, https://arxiv.org/abs/2401.08541
+    
+    Args:
+        input_dim: Dimensionality of the input features.
+        output_dim: Dimensionality of the output features.
+        hidden_dim: Dimensionality of the hidden layer.
+        num_blocks: Number of blocks in the prediction head.
+        mlp_ratio: Ratio used to determine the hidden layer size in the MLP.
+        proj_drop: Dropout rate for the projection layer.
+        act_layer: Activation layer to use.
+        norm_layer: Normalization layer to use.
+        mlp_layer: MLP layer to use.
+        block_fn: Block function to use for the prediction head.
     """
 
     def __init__(
@@ -56,6 +87,8 @@ class AIMPredictionHead(Module):
         mlp_layer: Type[Module] = Mlp,
         block_fn: Type[Module] = AIMPredictionHeadBlock,
     ) -> None:
+        """Initializes the AIMPredictionHead module with the specified parameters."""
+
         super().__init__()
         self.blocks = Sequential(
             # Linear layer to project the input dimension to the hidden dimension.
@@ -79,5 +112,13 @@ class AIMPredictionHead(Module):
         )
 
     def forward(self, x: Tensor) -> Tensor:
+        """Forward pass of the AIMPredictionHead.
+
+        Args:
+            x: Input tensor.
+
+        Returns:
+            Output tensor after processing through the prediction head blocks.
+        """
         x = self.blocks(x)
         return x
