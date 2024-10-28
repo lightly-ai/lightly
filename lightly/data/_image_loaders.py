663c0eafb0f0e4d037a2ab43961098f3ad525fa6
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2024-10-28 16:21:54 +0530

    Cleanup: _image.py and _utils.py file in data subpackage (#1707)
    * Add docstrings
    * Add types

--- lightly/data/_image_loaders.py
+++ lightly/data/_image_loaders.py
@@ -1,6 +1,7 @@
-"""torchvision image loaders
-(see https://pytorch.org/docs/stable/_modules/torchvision/datasets/folder.html)
+"""Module for handling image loading in torchvision-compatible format.
 
+This module provides image loading functionality similar to torchvision's implementation
+(see https://pytorch.org/vision/main/generated/torchvision.datasets.ImageFolder.html)
 """
 
 # Copyright (c) 2020. Lightly AG and its affiliates.
@@ -9,7 +10,15 @@
 from PIL import Image
 
 
-def pil_loader(path):
+def pil_loader(path: str) -> Image.Image:
+    """Loads an image using PIL.
+
+    Args:
+        path: Path to the image file.
+
+    Returns:
+        A PIL Image in RGB format.
+    """
     # open path as file to avoid ResourceWarning
     # (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, "rb") as f:
@@ -17,7 +26,17 @@ def pil_loader(path):
         return img.convert("RGB")
 
 
-def accimage_loader(path):
+def accimage_loader(path: str) -> Image.Image:
+    """Loads an image using the accimage library for faster loading.
+
+    Falls back to PIL loader if accimage fails to load the image.
+
+    Args:
+        path: Path to the image file.
+
+    Returns:
+        An image loaded either by accimage or PIL in case of failure.
+    """
     try:
         import accimage
 
@@ -27,7 +46,17 @@ def accimage_loader(path):
         return pil_loader(path)
 
 
-def default_loader(path):
+def default_loader(path: str) -> Image.Image:
+    """Loads an image using the default backend specified in torchvision.
+
+    Uses accimage if available and configured as the backend, otherwise falls back to PIL.
+
+    Args:
+        path: Path to the image file.
+
+    Returns:
+        An image loaded by either accimage or PIL depending on the backend.
+    """
     from torchvision import get_image_backend
 
     if get_image_backend() == "accimage":
