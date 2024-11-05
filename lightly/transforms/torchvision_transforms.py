aaeb94ff14ad5efe44125fa0262d7d75c6ab9b5e
Lionel <lionel@lightly.ai>
2024-11-05 17:40:50 +0100

    remove not needed types

--- lightly/transforms/torchvision_transforms.py
+++ lightly/transforms/torchvision_transforms.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
-from typing import Callable, Generic, Protocol, TypeVar, Union
+from typing import Union
 
 import torch
 from PIL.Image import Image
