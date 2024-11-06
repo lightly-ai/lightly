af9aa7f11697816eda82ddf702e7d9898b82378c
Lionel <lionel@lightly.ai>
2024-11-06 18:04:18 +0100

    SwaV: make v1 independent

--- lightly/transforms/swav_transform.py
+++ lightly/transforms/swav_transform.py
@@ -1,12 +1,12 @@
 from typing import Dict, List, Optional, Tuple, Union
 
-import torchvision.transforms as T
 from PIL.Image import Image
 from torch import Tensor
 
 from lightly.transforms.gaussian_blur import GaussianBlur
 from lightly.transforms.multi_crop_transform import MultiCropTranform
 from lightly.transforms.rotation import random_rotation_transform
+from lightly.transforms.torchvision_v2_compatibility import torchvision_transforms as T
 from lightly.transforms.utils import IMAGENET_NORMALIZE
 
 
