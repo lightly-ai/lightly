63db1acad1f5cc09f96d9d19de22493e434ff231
Lionel <lionel@lightly.ai>
2024-11-06 14:29:51 +0100

    remove assert messages; import class directly

--- tests/transforms/test_torchvision_v2compatibility.py
+++ tests/transforms/test_torchvision_v2compatibility.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from PIL import Image
+from torch import Tensor
 
 from lightly.transforms import ToTensor
 
@@ -9,7 +10,7 @@ def test_ToTensor() -> None:
     img_np = np.random.randint(0, 255, (20, 30, 3), dtype=np.uint8)
     img_pil = Image.fromarray(img_np)
     img_tens = ToTensor()(img_pil)
-    assert isinstance(img_tens, torch.Tensor), "Expected torch tensor"
-    assert img_tens.shape == (3, 20, 30), "Wrong shape"
-    assert img_tens.dtype == torch.float32, "Wrong dtype"
-    assert img_tens.max() <= 1.0 and img_tens.min() >= 0.0, "Wrong range"
+    assert isinstance(img_tens, Tensor)
+    assert img_tens.shape == (3, 20, 30)
+    assert img_tens.dtype == torch.float32
+    assert img_tens.max() <= 1.0 and img_tens.min() >= 0.0
