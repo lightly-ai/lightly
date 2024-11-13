c4902f72dc9bdbd89ac1aff0f898b68f8c7a5bd5
Vladislav Tumko <56307628+vectorvp@users.noreply.github.com>
2024-11-13 19:36:16 +0400

    tests: update AmplitudeRescaleTransform naming

--- tests/transforms/test_amplitude_rescale_transform.py
+++ tests/transforms/test_amplitude_rescale_transform.py
@@ -1,9 +1,7 @@
-import numpy as np
 import torch
 
 from lightly.transforms import (
-    AmplitudeRescaleTranform,
-    IRFFT2DTransform,
+    AmplitudeRescaleTransform,
     RFFT2DTransform,
 )
 
@@ -16,10 +14,10 @@ def test() -> None:
     rfftTransform = RFFT2DTransform()
     rfft = rfftTransform(image)
 
-    ampRescaleTf_1 = AmplitudeRescaleTranform()
+    ampRescaleTf_1 = AmplitudeRescaleTransform()
     rescaled_rfft_1 = ampRescaleTf_1(rfft)
 
-    ampRescaleTf_2 = AmplitudeRescaleTranform(range=(1.0, 2.0))
+    ampRescaleTf_2 = AmplitudeRescaleTransform(range=(1.0, 2.0))
     rescaled_rfft_2 = ampRescaleTf_2(rfft)
 
     assert rescaled_rfft_1.shape == rfft.shape
