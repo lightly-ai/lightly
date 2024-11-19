a9579c467b20effc767dbbb7e021064b157d04ba
Lionel Peer <lionel@lightly.ai>
2024-11-19 16:06:38 +0100

    Add Transform for DetCon + MultiViewTransformV2 for torchvision.transforms.v2 (#1737)
    * allow access to v2 transforms availability from anywhere
    
    * import unnecessary after exception
    
    Co-authored-by: guarin <43336610+guarin@users.noreply.github.com>
    
    * reformat
    
    * add implementation of AddGridTransform
    
    * make AddGridTransform importable
    
    * add tests for AddGridTransform
    
    * reformat
    
    * enhance docstring
    
    * fix typing issues
    
    * fix import when transforms.v2 not available
    
    * add additional type ignore for 3.7 compatibility
    
    * reformat
    
    * add transform to docs
    
    * change header
    
    * add explanation on data structures
    
    * use kw args
    
    * add assertion for mask dimension to be geq 2
    
    * remove unnecessary fixtures
    
    * make argument order consistent
    
    * add DetCon SingleView and MultiView transforms
    
    * add MultiViewTransform BaseClass for v2 transforms
    
    * add tests for DetCon transform
    
    * export all newly added transforms
    
    * add DetCon single view and DetCon multi view transforms
    
    * add torchvision transforms v2 compatible MultiViewTransforms
    
    * make newly added transforms public
    
    * remove unnecessary fixtures
    
    * add tests for DetCon transform
    
    * remove wrongfully added files
    
    * add DetCon transform and MultiView transforms for v2 to docs
    
    * fix docs references
    
    * fix import issues for minimal dependencies
    
    * fixing code format
    
    * add test for multiviewtransformv2
    
    * fix testing of multiview
    
    * use singular AddGridTransforms
    
    * consistent naming to DetConS
    
    * adjust docstring reference numbering
    
    * name refactoring
    
    ---------
    
    Co-authored-by: guarin <43336610+guarin@users.noreply.github.com>

---
+++ tests/transforms/test_detcon_transform.py
@@ -0,0 +1,61 @@
+import pytest
+import torch
+
+from lightly.utils.dependency import torchvision_transforms_v2_available
+
+if not torchvision_transforms_v2_available():
+    pytest.skip("torchvision.transforms.v2 not available", allow_module_level=True)
+from torchvision.tv_tensors import Image, Mask
+
+from lightly.transforms.detcon_transform import DetConSTransform, DetConSViewTransform
+
+
+# ignore typing due to Any type used in torchvison.transforms.v2.Transform
+@pytest.fixture()
+def img() -> Image:  # type: ignore[misc]
+    return Image(torch.randn(3, 256, 256))
+
+
+# ignore typing due to Any type used in torchvison.transforms.v2.Transform
+@pytest.fixture()
+def mask() -> Mask:  # type: ignore[misc]
+    return Mask(torch.randint(0, 4, (1, 256, 256)), dtype=torch.int64)
+
+
+class TestDetConSimCLRViewTransform:
+    def test_given_masks(self, img: Image, mask: Mask) -> None:
+        tr = DetConSViewTransform(input_size=(224, 224))
+
+        img_tr, mask_tr = tr(img, mask)
+        assert img_tr.shape == (3, 224, 224)
+        assert mask_tr.shape == (1, 224, 224)
+
+
+class TestDetConTransform:
+    def test_given_masks(self, img: Image, mask: Mask) -> None:
+        # deactivate anything that could change the mask
+        tr = DetConSTransform(input_size=(256, 256), min_scale=1.0, rr_prob=0.0)
+
+        (img_tr1, mask_tr1), (img_tr2, mask_tr2) = tr(img, mask)
+
+        assert img_tr1.shape == (3, 256, 256)
+        assert mask_tr1.shape == (1, 256, 256)
+        assert img_tr2.shape == (3, 256, 256)
+        assert mask_tr2.shape == (1, 256, 256)
+
+        assert (mask_tr1.unique() == torch.tensor([0, 1, 2, 3])).all()
+
+    def test_generate_grid_mask(self, img: Image, mask: Mask) -> None:
+        # deactivate anything that could change the mask
+        tr = DetConSTransform(
+            grid_size=(4, 4), input_size=(256, 256), min_scale=1.0, rr_prob=0.0
+        )
+
+        (img_tr1, mask_tr1), (img_tr2, mask_tr2) = tr(img, mask)
+
+        assert img_tr1.shape == (3, 256, 256)
+        assert mask_tr1.shape == (1, 256, 256)
+        assert img_tr2.shape == (3, 256, 256)
+        assert mask_tr2.shape == (1, 256, 256)
+
+        assert (mask_tr1.unique() == torch.arange(4 * 4)).all()
