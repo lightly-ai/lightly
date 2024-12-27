1d2c7e604527975d50ae61faedbd69e203e9df5a
Philipp Wirth <philipp.m.wirth@gmail.com>
2024-12-27 16:10:18 +0100

    Re Malte

--- tests/loss/test_negative_cosine_similarity.py
+++ tests/loss/test_negative_cosine_similarity.py
@@ -5,25 +5,25 @@ from lightly.loss import NegativeCosineSimilarity
 
 
 class TestNegativeCosineSimilarity:
-    def test_forward_pass(self) -> None:
+    @pytest.mark.parametrize("bsz", range(1, 20))
+    def test_forward_pass(self, bsz: int) -> None:
         loss = NegativeCosineSimilarity()
-        for bsz in range(1, 20):
-            x0 = torch.randn((bsz, 32))
-            x1 = torch.randn((bsz, 32))
+        x0 = torch.randn((bsz, 32))
+        x1 = torch.randn((bsz, 32))
 
-            # symmetry
-            l1 = loss(x0, x1)
-            l2 = loss(x1, x0)
-            assert l1 == pytest.approx(l2, abs=1e-5)
+        # symmetry
+        l1 = loss(x0, x1)
+        l2 = loss(x1, x0)
+        assert l1 == pytest.approx(l2, abs=1e-5)
 
     @pytest.mark.skipif(not torch.cuda.is_available(), reason="No cuda")
-    def test_forward_pass_cuda(self) -> None:
+    @pytest.mark.parametrize("bsz", range(1, 20))
+    def test_forward_pass_cuda(self, bsz: int) -> None:
         loss = NegativeCosineSimilarity()
-        for bsz in range(1, 20):
-            x0 = torch.randn((bsz, 32)).cuda()
-            x1 = torch.randn((bsz, 32)).cuda()
+        x0 = torch.randn((bsz, 32)).cuda()
+        x1 = torch.randn((bsz, 32)).cuda()
 
-            # symmetry
-            l1 = loss(x0, x1)
-            l2 = loss(x1, x0)
-            assert l1 == pytest.approx(l2, abs=1e-5)
+        # symmetry
+        l1 = loss(x0, x1)
+        l2 = loss(x1, x0)
+        assert l1 == pytest.approx(l2, abs=1e-5)
