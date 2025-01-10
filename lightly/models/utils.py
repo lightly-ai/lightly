d35cbd1b4e2b19a2158edbf2b802a6163e98275a
Lionel <lionel@lightly.ai>
2025-01-10 10:48:03 +0100

    another formatting error

--- lightly/models/utils.py
+++ lightly/models/utils.py
@@ -40,8 +40,8 @@ def pool_masked(
             integer indices.
         num_cls: The number of classes in the possible masks.
 
-    Returns: 
-        A tensor of shape :math:`(B, C, N)` or :math:`(C, N)` where :math:`N` 
+    Returns:
+        A tensor of shape :math:`(B, C, N)` or :math:`(C, N)` where :math:`N`
         corresponds to `num_cls`.
     """
     if source.dim() == 3:
