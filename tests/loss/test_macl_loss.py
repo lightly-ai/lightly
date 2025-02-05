158d83ce1216b155f7e7f5a2bf17dd8a038d1d9c
yutong <yutong@lightly.ai>
2025-02-05 17:58:42 +0100

    add test_macl_loss

---
+++ tests/loss/test_macl_loss.py
@@ -0,0 +1,39 @@
+import pytest
+import torch
+
+from lightly.loss import MACLLoss
+
+class TestNTXentLoss:
+    def test_forward_pass(self) -> None:
+        loss = MACLLoss()
+        for bsz in range(2, 20): # batch_size 1 is not allowed
+            batch_1 = torch.randn((bsz, 32))
+            batch_2 = torch.randn((bsz, 32))
+
+            # symmetry
+            l1 = loss(batch_1, batch_2)
+            l2 = loss(batch_2, batch_1)
+            assert (l1 - l2).pow(2).item() == pytest.approx(0.0)
+
+    def test_forward_pass_1d(self) -> None:
+        loss = MACLLoss()
+        for bsz in range(2, 20): # TODO: numerical issues for bsz=2
+            batch_1 = torch.randn((bsz, 1))
+            batch_2 = torch.randn((bsz, 1))
+
+            # symmetry
+            l1 = loss(batch_1, batch_2)
+            l2 = loss(batch_2, batch_1)
+            assert (l1 - l2).pow(2).item() == pytest.approx(0.0)
+
+    @pytest.mark.skipif(not torch.cuda.is_available(), reason="No cuda")
+    def test_forward_pass_cuda(self) -> None:
+        loss = MACLLoss()
+        for bsz in range(2, 20):
+            batch_1 = torch.randn((bsz, 32)).cuda()
+            batch_2 = torch.randn((bsz, 32)).cuda()
+
+            # symmetry
+            l1 = loss(batch_1, batch_2)
+            l2 = loss(batch_2, batch_1)
+            assert (l1 - l2).pow(2).item() == pytest.approx(0.0)
