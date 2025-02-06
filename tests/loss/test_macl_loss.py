d991baeffb5315965cabd144cc0eb3e7e830194d
yutong <yutong@lightly.ai>
2025-02-06 14:13:20 +0100

    test macl loss with batch_size 2

--- tests/loss/test_macl_loss.py
+++ tests/loss/test_macl_loss.py
@@ -1,12 +1,99 @@
+import numpy as np
 import pytest
 import torch
+import torch.nn.functional as F
+from torch import Tensor
 
 from lightly.loss import MACLLoss
 
+
+def _cal_macl_loss_original(
+    pos: Tensor, neg: Tensor, tau_0: float, alpha: float, A0: float
+) -> Tensor:
+    """The original implementation of the MACL loss.
+
+    See: https://github.com/chenhaoxing/MACL/blob/main/macl.py
+    Note that the original implementation is not numerically stable, so the error bar here is 1e-6.
+
+    Args:
+        pos (Tensor):
+            The positive cosine similarities.
+        neg (Tensor):
+            The negative cosine similarities.
+        tau_0 (float):
+            The initial temperature. Must be greater than 0.
+        alpha (float):
+            The alpha parameter. Must be in the range [0, 1].
+        A0 (float):
+            The initial value of A. Must be in the range [0, 1].
+
+    Returns:
+        The loss value.
+    """
+
+    A = torch.mean(pos.detach())
+    tau = tau_0 * (1.0 + alpha * (A - A0))
+
+    logits = torch.cat([pos, neg], dim=1) / tau
+    P = torch.softmax(logits, dim=1)[:, 0]
+
+    # reweighting and loss computation
+    V = 1.0 / (1.0 - P)
+    loss: Tensor = -V.detach() * torch.log(P)
+
+    return loss.mean()
+
+
 class TestMACLLoss:
+    @pytest.mark.parametrize("t_0", [0.1, 0.5, 1.0])
+    @pytest.mark.parametrize("alpha", [0, 0.5, 1])
+    @pytest.mark.parametrize("A_0", [0, 0.5, 1])
+    @pytest.mark.parametrize("emb_dim", [2, 4, 8])
+    def test_with_batch_size_2(
+        self, t_0: float, alpha: float, A_0: float, emb_dim: int
+    ) -> None:
+        # Generate two random embeddings with batch size 2
+        z0 = torch.FloatTensor(np.random.rand(2, emb_dim))
+        z1 = torch.FloatTensor(np.random.rand(2, emb_dim))
+
+        # Calculate the loss in both directions
+        loss_function = MACLLoss(t_0=t_0, alpha=alpha, A_0=A_0)
+        l1 = float(loss_function(z0, z1))
+        l2 = float(loss_function(z1, z0))
+
+        # Calculate the positive and negative cosine similarities
+        z0_norm = F.normalize(z0, dim=-1, p=2)
+        z1_norm = F.normalize(z1, dim=-1, p=2)
+
+        pos = torch.tensor(
+            [
+                z0_norm[0] @ z1_norm[0],
+                z0_norm[1] @ z1_norm[1],
+                z0_norm[0] @ z1_norm[0],
+                z0_norm[1] @ z1_norm[1],
+            ]
+        ).unsqueeze(1)
+        neg = torch.stack(
+            [
+                torch.tensor([z0_norm[0] @ z0_norm[1], z0_norm[0] @ z1_norm[1]]),
+                torch.tensor([z0_norm[1] @ z0_norm[0], z0_norm[1] @ z1_norm[0]]),
+                torch.tensor([z1_norm[0] @ z0_norm[1], z1_norm[0] @ z1_norm[1]]),
+                torch.tensor([z1_norm[1] @ z0_norm[0], z1_norm[1] @ z1_norm[0]]),
+            ],
+            dim=0,
+        )
+
+        # Calculate the loss using the original implementation
+        l1_original = _cal_macl_loss_original(pos, neg, t_0, alpha, A_0)
+        l2_original = _cal_macl_loss_original(pos, neg, t_0, alpha, A_0)
+
+        assert l1 == pytest.approx(l2, abs=1e-6)
+        assert l1 == pytest.approx(l1_original, abs=1e-6)
+        assert l2 == pytest.approx(l2_original, abs=1e-6)
+
     def test_forward_pass(self) -> None:
         loss = MACLLoss()
-        for bsz in range(2, 20): # batch_size 1 is not allowed
+        for bsz in range(2, 20):  # batch_size 1 is not allowed
             batch_1 = torch.randn((bsz, 32))
             batch_2 = torch.randn((bsz, 32))
 
@@ -17,7 +104,7 @@ class TestMACLLoss:
 
     def test_forward_pass_1d(self) -> None:
         loss = MACLLoss()
-        for bsz in range(2, 20): # # batch_size 1 is not allowed
+        for bsz in range(2, 20):  # # batch_size 1 is not allowed
             batch_1 = torch.randn((bsz, 1))
             batch_2 = torch.randn((bsz, 1))
 
@@ -29,7 +116,7 @@ class TestMACLLoss:
     @pytest.mark.skipif(not torch.cuda.is_available(), reason="No cuda")
     def test_forward_pass_cuda(self) -> None:
         loss = MACLLoss()
-        for bsz in range(2, 20): # batch_size 1 is not allowed
+        for bsz in range(2, 20):  # batch_size 1 is not allowed
             batch_1 = torch.randn((bsz, 32)).cuda()
             batch_2 = torch.randn((bsz, 32)).cuda()
 
