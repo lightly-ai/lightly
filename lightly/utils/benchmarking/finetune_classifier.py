653bb5dd52c9db5025d59501fa1ed24d8f6eba2f
yutong <yutong@lightly.ai>
2025-02-11 13:29:49 +0100

    fix typing checks

--- lightly/utils/benchmarking/finetune_classifier.py
+++ lightly/utils/benchmarking/finetune_classifier.py
@@ -1,11 +1,17 @@
-from torch.optim import SGD
+from typing import Any, Dict, List, Tuple, Union
+
+from torch.optim import SGD, Optimizer
 
 from lightly.utils.benchmarking import LinearClassifier
 from lightly.utils.scheduler import CosineWarmupScheduler
 
 
 class FinetuneClassifier(LinearClassifier):
-    def configure_optimizers(self):
+    # Type ignore is needed because return type of LightningModule.configure_optimizers
+    # is complicated and typing changes between versions.
+    def configure_optimizers(  # type: ignore[override]
+        self,
+    ) -> Tuple[List[Optimizer], List[Dict[str, Union[Any, str]]]]:
         parameters = list(self.classification_head.parameters())
         parameters += self.model.parameters()
         optimizer = SGD(
@@ -18,7 +24,7 @@ class FinetuneClassifier(LinearClassifier):
             "scheduler": CosineWarmupScheduler(
                 optimizer=optimizer,
                 warmup_epochs=0,
-                max_epochs=self.trainer.estimated_stepping_batches,
+                max_epochs=int(self.trainer.estimated_stepping_batches),
             ),
             "interval": "step",
         }
