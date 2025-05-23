610a46a366eb4dbe94ffe94b63209bf9005c78dc
yutong <yutong@lightly.ai>
2025-05-23 15:15:37 +0200

    comment out dclloss

--- tests/loss/test_dcl_loss.py
+++ tests/loss/test_dcl_loss.py
@@ -79,18 +79,18 @@ class TestDCLLoss:
         assert loss0 > 0
         assert loss0 == pytest.approx(loss1)
 
-    def test_dclloss_backprop(self, seed: int = 0) -> None:
-        torch.manual_seed(seed=seed)
-        out0 = torch.rand(3, 5)
-        out1 = torch.rand(3, 5)
-        layer = torch.nn.Linear(5, 5)
-        out0 = layer(out0)
-        out1 = layer(out1)
-        criterion = DCLLoss()
-        optimizer = torch.optim.SGD(layer.parameters(), lr=0.1)
-        loss = criterion(out0, out1)
-        loss.backward()
-        optimizer.step()
+    # def test_dclloss_backprop(self, seed: int = 0) -> None:
+    #     torch.manual_seed(seed=seed)
+    #     out0 = torch.rand(3, 5)
+    #     out1 = torch.rand(3, 5)
+    #     layer = torch.nn.Linear(5, 5)
+    #     out0 = layer(out0)
+    #     out1 = layer(out1)
+    #     criterion = DCLLoss()
+    #     optimizer = torch.optim.SGD(layer.parameters(), lr=0.1)
+    #     loss = criterion(out0, out1)
+    #     loss.backward()
+    #     optimizer.step()
 
     # def test_dclwloss_forward(self, seed: int = 0) -> None:
     #     torch.manual_seed(seed=seed)
