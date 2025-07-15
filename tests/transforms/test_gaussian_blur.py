71a255358a5ddc2250f5ee83cc325cb6b1d46e22
yutong <yutong@lightly.ai>
2025-07-15 09:33:20 +0200

    add tensor input support

--- tests/transforms/test_gaussian_blur.py
+++ tests/transforms/test_gaussian_blur.py
@@ -1,5 +1,6 @@
 import unittest
 
+import torch
 from PIL import Image
 
 from lightly.transforms.gaussian_blur import GaussianBlur
@@ -12,6 +13,13 @@ class TestGaussianBlur(unittest.TestCase):
                 gaussian_blur = GaussianBlur()
                 sample = Image.new("RGB", (w, h))
                 gaussian_blur(sample)
+
+    def test_on_tensor(self) -> None:
+        for w in range(1, 100):
+            for h in range(1, 100):
+                gaussian_blur = GaussianBlur()
+                sample_tensor = torch.randn(3, h, w)
+                gaussian_blur(sample_tensor)
 
     def test_raise_kernel_size_deprecation(self) -> None:
         gaussian_blur = GaussianBlur(kernel_size=2)
