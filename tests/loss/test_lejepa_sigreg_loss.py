1dc465ef3a26bb3aecba67a539b24e295dc7474f
gabrielfruet <gabrielfruet538@gmail.com>
2026-02-12 08:56:32 -0300

    test: more tests for sigreg

--- tests/loss/test_lejepa_sigreg_loss.py
+++ tests/loss/test_lejepa_sigreg_loss.py
@@ -5,6 +5,29 @@ from lightly.loss.lejepa_loss import SIGReg
 
 
 class TestSIGReg:
+    @pytest.mark.parametrize("channel_size", [8, 16, 32, 64])
+    def test_works_with_any_channel_size(self, channel_size: int) -> None:
+        torch.manual_seed(0)
+        loss_fn = SIGReg()
+        proj = torch.randn(10, 1024, channel_size)
+        loss = loss_fn(proj)
+        assert loss.item() >= 0.0
+
+    @pytest.mark.parametrize("batch_size", [1, 4, 10, 32])
+    def test_works_with_any_batch_size(self, batch_size: int) -> None:
+        torch.manual_seed(0)
+        loss_fn = SIGReg()
+        proj = torch.randn(batch_size, 1024, 16)
+        loss = loss_fn(proj)
+        assert loss.item() >= 0.0
+
+    def test_dont_work_with_1d_input(self) -> None:
+        torch.manual_seed(0)
+        loss_fn = SIGReg()
+        proj = torch.randn(1024)
+        with pytest.raises(IndexError):
+            loss_fn(proj)
+
     def test_non_gaussian_positive(self) -> None:
         torch.manual_seed(0)
         loss_fn = SIGReg()
