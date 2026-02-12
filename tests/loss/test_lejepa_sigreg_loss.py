5b60366e589b3e85f1ec8f5b9d2d4bc83688fee4
gabrielfruet <gabrielfruet538@gmail.com>
2026-02-12 08:59:57 -0300

    test: more tests

--- tests/loss/test_lejepa_sigreg_loss.py
+++ tests/loss/test_lejepa_sigreg_loss.py
@@ -5,6 +5,26 @@ from lightly.loss.lejepa_loss import SIGReg
 
 
 class TestSIGReg:
+    def test_backward_pass(self) -> None:
+        torch.manual_seed(0)
+        loss_fn = SIGReg()
+        proj = torch.randn(10, 1024, 16, requires_grad=True)
+        loss = loss_fn(proj)
+        loss.backward()
+        assert proj.grad is not None
+        assert proj.grad.shape == proj.shape
+
+    def test_deterministic_output(self) -> None:
+        torch.manual_seed(0)
+        loss_fn = SIGReg()
+        proj = torch.randn(10, 1024, 16)
+        loss1 = loss_fn(proj)
+
+        torch.manual_seed(0)
+        proj = torch.randn(10, 1024, 16)
+        loss2 = loss_fn(proj)
+        assert torch.isclose(loss1, loss2)
+
     @pytest.mark.parametrize("channel_size", [8, 16, 32, 64])
     def test_works_with_any_channel_size(self, channel_size: int) -> None:
         torch.manual_seed(0)
