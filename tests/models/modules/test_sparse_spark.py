6188aa45c12080b47aa41a4d9f5c870b6321f04e
gabrielfruet <gabrielfruet538@gmail.com>
2026-02-13 08:09:31 -0300

    refactor: using fixture instead of context manager

--- tests/models/modules/test_sparse_spark.py
+++ tests/models/modules/test_sparse_spark.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from typing import Generator
 
 import pytest
@@ -7,116 +6,108 @@ import torch
 import lightly.models.modules.sparse_spark as sparse_spark
 
 
-@contextmanager
-def _cleanup_curr_active() -> Generator[None, None, None]:
-    try:
-        yield
-    finally:
-        sparse_spark._cur_active = None
+@pytest.fixture(autouse=True)
+def _cleanup_cur_active() -> Generator[None, None, None]:
+    sparse_spark._cur_active = None
+    yield
+    sparse_spark._cur_active = None
 
 
 def test__get_active_ex_or_ii_expands_mask() -> None:
-    with _cleanup_curr_active():
-        H, W = 32, 32
-        sparse_spark._cur_active = torch.tensor(
+    H, W = 32, 32
+    sparse_spark._cur_active = torch.tensor(
+        [
             [
                 [
-                    [
-                        [1, 0],
-                        [0, 1],
-                    ]
+                    [1, 0],
+                    [0, 1],
                 ]
-            ],
-            dtype=torch.bool,
-        )
+            ]
+        ],
+        dtype=torch.bool,
+    )
 
-        active = sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=True)
-        assert not isinstance(active, tuple)
+    active = sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=True)
+    assert not isinstance(active, tuple)
 
-        assert active.shape == (1, 1, H, W)
-        assert active[:, :, :16, :16].all()
-        assert active[:, :, :16, 16:].logical_not().all()
-        assert active[:, :, 16:, :16].logical_not().all()
-        assert active[:, :, 16:, 16:].all()
+    assert active.shape == (1, 1, H, W)
+    assert active[:, :, :16, :16].all()
+    assert active[:, :, :16, 16:].logical_not().all()
+    assert active[:, :, 16:, :16].logical_not().all()
+    assert active[:, :, 16:, 16:].all()
 
 
 def test__get_active_ex_or_ii_dont_shrink_mask() -> None:
-    with _cleanup_curr_active():
-        H, W = 4, 4
-        sparse_spark._cur_active = torch.ones(1, 1, 32, 32)
-        with pytest.raises(AssertionError):
-            sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
+    H, W = 4, 4
+    sparse_spark._cur_active = torch.ones(1, 1, 32, 32)
+    with pytest.raises(AssertionError):
+        sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
 
 
 def test__get_active_ex_or_ii_raise_on_non_active_mask() -> None:
-    with _cleanup_curr_active():
-        H, W = 32, 32
-        sparse_spark._cur_active = None
-        with pytest.raises(
-            AssertionError,
-        ):
-            sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
+    H, W = 32, 32
+    sparse_spark._cur_active = None
+    with pytest.raises(
+        AssertionError,
+    ):
+        sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
 
 
 def test__get_active_ex_or_ii_returning_ex_false_correct_values() -> None:
-    with _cleanup_curr_active():
-        H, W = 32, 32
-        sparse_spark._cur_active = torch.tensor(
+    H, W = 32, 32
+    sparse_spark._cur_active = torch.tensor(
+        [
             [
                 [
-                    [
-                        [1, 0],
-                        [0, 1],
-                    ]
+                    [1, 0],
+                    [0, 1],
                 ]
-            ],
-            dtype=torch.bool,
-        )
+            ]
+        ],
+        dtype=torch.bool,
+    )
 
-        active_b, active_h, active_w = sparse_spark._get_active_ex_or_ii(
-            H=H, W=W, returning_active_ex=False
-        )
-        active_ex = sparse_spark._get_active_ex_or_ii(
-            H=H, W=W, returning_active_ex=True
-        )
-        assert not isinstance(active_ex, tuple)
+    active_b, active_h, active_w = sparse_spark._get_active_ex_or_ii(
+        H=H, W=W, returning_active_ex=False
+    )
+    active_ex = sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=True)
+    assert not isinstance(active_ex, tuple)
 
-        active_ex_scattered = torch.zeros_like(active_ex)
-        active_ex_scattered[active_b, :, active_h, active_w] = 1
+    active_ex_scattered = torch.zeros_like(active_ex)
+    active_ex_scattered[active_b, :, active_h, active_w] = 1
 
-        assert torch.equal(active_ex, active_ex_scattered)
+    assert torch.equal(active_ex, active_ex_scattered)
 
 
 def test_sp_conv_forward() -> None:
-    with _cleanup_curr_active():
-        H, W = 32, 32
-        sparse_spark._cur_active = torch.tensor(
+    H, W = 32, 32
+    sparse_spark._cur_active = torch.tensor(
+        [
             [
                 [
-                    [
-                        [1, 0],
-                        [0, 1],
-                    ]
+                    [1, 0],
+                    [0, 1],
                 ]
-            ],
-            dtype=torch.bool,
-        )
+            ]
+        ],
+        dtype=torch.bool,
+    )
 
-        conv = sparse_spark.SparseConv2d(
-            in_channels=1,
-            out_channels=1,
-            kernel_size=3,
-            padding=1,
-        )
-        conv.weight.data.fill_(1)
-        assert conv.bias is not None
-        conv.bias.data.fill_(0)
+    conv = sparse_spark.SparseConv2d(
+        in_channels=1,
+        out_channels=1,
+        kernel_size=3,
+        padding=1,
+    )
+    conv.weight.data.fill_(1)
+    assert conv.bias is not None
+    conv.bias.data.fill_(0)
 
-        x = torch.ones(1, 1, H, W)
-        out = conv(x)
+    x = torch.ones(1, 1, H, W)
+    out = conv(x)
 
-        assert out.shape == (1, 1, H, W)
-        assert out[:, :, :16, :16].all()
-        assert out[:, :, :16, 16:].logical_not().all()
-        assert out[:, :, 16:, :16].logical_not().all()
-        assert out[:, :, 16:, 16:].all()
+    assert out.shape == (1, 1, H, W)
+    assert out[:, :, :16, :16].all()
+    assert out[:, :, :16, 16:].logical_not().all()
+    assert out[:, :, 16:, :16].logical_not().all()
+    assert out[:, :, 16:, 16:].all()
