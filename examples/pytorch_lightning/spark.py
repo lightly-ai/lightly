8b2eca5b6bef46b0b8ca32e0b2cb9d4ac56e1762
Lionel <lionel@lightly.ai>
2026-03-02 18:10:03 +0100

    typing issues

--- examples/pytorch_lightning/spark.py
+++ examples/pytorch_lightning/spark.py
@@ -1,5 +1,5 @@
 # This example requires the following dependencies to be installed:
-# pip install lightly
+# pip install "lightly[timm]"
 
 # Note: The model and training settings do not follow the reference settings
 # from the paper. The settings are chosen such that the example can easily be
@@ -9,33 +9,43 @@ import pytorch_lightning as pl
 import timm
 import torch
 import torchvision
-from pytorch_lightning.callbacks import ModelCheckpoint, RichProgressBar
-from torch import nn
+from pytorch_lightning import LightningModule
+from torch import Tensor
+from torch.nn import Module
 from torchvision.transforms import v2
 
+import lightly.models.utils as model_utils
 from lightly.loss.sparse_spark import SparKPatchReconLoss
+from lightly.models.modules import sparse_spark
 
 ## The global projection head is the same as the Barlow Twins one
 from lightly.models.modules.sparse_spark import (
     SparKDensifier,
     SparKMasker,
-    SparKMaskingOuptut,
+    SparKMaskingOutput,
     SparKOutputDecoder,
     UNetDecoder,
-    dense_model_to_sparse,
+    sparse_layer_context,
 )
-from lightly.models.utils import patchify
 
 
-def get_downsample_ratio_from_timm_model(model: nn.Module) -> int:
+def _get_downsample_ratio_from_timm_model(model: Module) -> int:
+    if not hasattr(model, "feature_info"):
+        raise ValueError(
+            "The provided model does not have the required 'feature_info' attribute."
+        )
     return model.feature_info[-1]["reduction"]
 
 
-def get_enc_feat_map_chs_from_timm_model(model: nn.Module) -> list[int]:
+def _get_enc_feat_map_chs_from_timm_model(model: Module) -> list[int]:
+    if not hasattr(model, "feature_info"):
+        raise ValueError(
+            "The provided model does not have the required 'feature_info' attribute."
+        )
     return [fi["num_chs"] for fi in model.feature_info]
 
 
-class SparseSparK(pl.LightningModule):
+class SparseSparK(LightningModule):
     def __init__(
         self,
         input_size: int = 416,
@@ -45,15 +55,17 @@ class SparseSparK(pl.LightningModule):
     ):
         super().__init__()
         backbone = timm.create_model(
-            "resnet18", drop_path_rate=0.05, features_only=True
+            model_name="resnet18", drop_path_rate=0.05, features_only=True
         )
-        downsample_ratio = get_downsample_ratio_from_timm_model(backbone)
-        enc_feat_map_chs = get_enc_feat_map_chs_from_timm_model(backbone)
-        self.sparse_encoder = dense_model_to_sparse(backbone, sbn=sbn, verbose=True)
+        downsample_ratio = _get_downsample_ratio_from_timm_model(backbone)
+        enc_feat_map_chs = _get_enc_feat_map_chs_from_timm_model(backbone)
+        self.sparse_encoder = sparse_spark.dense_model_to_sparse(
+            m=backbone, sbn=sbn, verbose=True
+        )
         self.fmap_h = input_size // downsample_ratio
         self.fmap_w = input_size // downsample_ratio
         self.dense_decoder = UNetDecoder(
-            downsample_ratio,
+            up_sample_ratio=downsample_ratio,
             width=enc_feat_map_chs[-1],
         )
         self.masker = SparKMasker(
@@ -72,41 +84,51 @@ class SparseSparK(pl.LightningModule):
         self.recon_loss_fn = SparKPatchReconLoss()
         # output decoder for visualization (pass minimal spatial props)
         self.output_decoder = SparKOutputDecoder(
-            self.fmap_h,
-            self.fmap_w,
-            downsample_ratio,
+            fmap_h=self.fmap_h,
+            fmap_w=self.fmap_w,
+            downsample_ratio=downsample_ratio,
         )
 
     def forward(
         self,
-        inp_bchw: torch.Tensor,
+        inp_bchw: Tensor,
         vis=False,
     ):
         # step1. Mask
-        mask_out: SparKMaskingOuptut = self.masker(inp_bchw)
+        mask_out: SparKMaskingOutput = self.masker(inp_bchw)
         masked_bchw, per_level_mask = mask_out
         active_b1fHfW = per_level_mask[0]
         active_b1hw = per_level_mask[-1]
         # step2. Encode: get hierarchical encoded sparse features (a list containing 4 feature maps at 4 scales)
-        fea_bcffs: list[torch.Tensor] = self.sparse_encoder(masked_bchw)
-        # step3. Densify: get hierarchical dense features for decoding
-        to_dec = self.densifier(fea_bcffs)
+        # Use sparse_layer_context to provide the mask to the sparse encoder and densifier.
+        with sparse_layer_context(active_mask=active_b1fHfW):
+            fea_bcffs: list[Tensor] = self.sparse_encoder(masked_bchw)
+            # step3. Densify: get hierarchical dense features for decoding
+            to_dec = self.densifier(fea_bcffs)
         # step4. Decode and reconstruct
         rec_bchw = self.dense_decoder(to_dec)
         inp, rec = (
-            patchify(inp_bchw, self.downsample_ratio),
-            patchify(rec_bchw, self.downsample_ratio),
-        )  # inp and rec: (B, L = f*f, N = C*downsample_raito**2)
+            model_utils.patchify(inp_bchw, self.downsample_ratio),
+            model_utils.patchify(rec_bchw, self.downsample_ratio),
+        )  # inp and rec: (B, L = f*f, N = C*downsample_ratio**2)
 
-        recon_loss, mean, var = self.recon_loss_fn(inp, rec, active_b1fHfW)
+        recon_loss, mean, var = self.recon_loss_fn(
+            inp_patches=inp, rec_patches=rec, active_mask=active_b1fHfW
+        )
 
         if vis:
-            return self.output_decoder(rec, mean, var, inp_bchw, active_b1hw)
+            return self.output_decoder(
+                rec_patches=rec,
+                mean=mean,
+                var=var,
+                inp_bchw=inp_bchw,
+                active_mask_full=active_b1hw,
+            )
         else:
             return recon_loss
 
-    def training_step(self, batch, batch_index) -> torch.Tensor:
-        img, target = batch
+    def training_step(self, batch: tuple[Tensor, Tensor], batch_index: int) -> Tensor:
+        img, _ = batch
         recon_loss = self.forward(img)
         # Log the training loss to logger and progress bar (per-step and per-epoch)
         self.log(
@@ -164,9 +186,6 @@ trainer = pl.Trainer(
     max_epochs=30,
     devices=1,
     accelerator=accelerator,
-    callbacks=[
-        RichProgressBar(),
-    ],
 )
 trainer.fit(
     model=model,
