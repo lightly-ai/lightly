edbfdcf69195a6b839f473b39ec8c0cc1043ea6d
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-04 17:44:38 -0300

    fix: tests now uses context manager for sparse mask

--- tests/models/modules/test_sparse_spark.py
+++ tests/models/modules/test_sparse_spark.py
@@ -1,21 +1,11 @@
-from typing import Generator
-
 import pytest
 import torch
 
 import lightly.models.modules.sparse_spark as sparse_spark
 
 
-@pytest.fixture(autouse=True)
-def _cleanup_cur_active() -> Generator[None, None, None]:
-    sparse_spark._cur_active = None
-    yield
-    sparse_spark._cur_active = None
-
-
-def test__get_active_ex_or_ii_expands_mask() -> None:
-    H, W = 32, 32
-    sparse_spark._cur_active = torch.tensor(
+def _create_mask() -> torch.Tensor:
+    return torch.tensor(
         [
             [
                 [
@@ -27,87 +17,68 @@ def test__get_active_ex_or_ii_expands_mask() -> None:
         dtype=torch.bool,
     )
 
-    active = sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=True)
-    assert not isinstance(active, tuple)
 
-    assert active.shape == (1, 1, H, W)
-    assert active[:, :, :16, :16].all()
-    assert active[:, :, :16, 16:].logical_not().all()
-    assert active[:, :, 16:, :16].logical_not().all()
-    assert active[:, :, 16:, 16:].all()
+def test__get_active_ex_or_ii_expands_mask() -> None:
+    H, W = 32, 32
+    with sparse_spark.sparse_layer_context(_create_mask()):
+        active = sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=True)
+        assert not isinstance(active, tuple)
+
+        assert active.shape == (1, 1, H, W)
+        assert active[:, :, :16, :16].all()
+        assert active[:, :, :16, 16:].logical_not().all()
+        assert active[:, :, 16:, :16].logical_not().all()
+        assert active[:, :, 16:, 16:].all()
 
 
 def test__get_active_ex_or_ii_dont_shrink_mask() -> None:
     H, W = 4, 4
-    sparse_spark._cur_active = torch.ones(1, 1, 32, 32)
-    with pytest.raises(AssertionError):
-        sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
+    with sparse_spark.sparse_layer_context(torch.ones(1, 1, 32, 32)):
+        with pytest.raises(AssertionError):
+            sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
 
 
 def test__get_active_ex_or_ii_raise_on_non_active_mask() -> None:
     H, W = 32, 32
-    sparse_spark._cur_active = None
-    with pytest.raises(
-        AssertionError,
-    ):
+    with pytest.raises(RuntimeError):
         sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=False)
 
 
 def test__get_active_ex_or_ii_returning_ex_false_correct_values() -> None:
     H, W = 32, 32
-    sparse_spark._cur_active = torch.tensor(
-        [
-            [
-                [
-                    [1, 0],
-                    [0, 1],
-                ]
-            ]
-        ],
-        dtype=torch.bool,
-    )
+    with sparse_spark.sparse_layer_context(_create_mask()):
+        active_b, active_h, active_w = sparse_spark._get_active_ex_or_ii(
+            H=H, W=W, returning_active_ex=False
+        )
+        active_ex = sparse_spark._get_active_ex_or_ii(
+            H=H, W=W, returning_active_ex=True
+        )
+        assert not isinstance(active_ex, tuple)
 
-    active_b, active_h, active_w = sparse_spark._get_active_ex_or_ii(
-        H=H, W=W, returning_active_ex=False
-    )
-    active_ex = sparse_spark._get_active_ex_or_ii(H=H, W=W, returning_active_ex=True)
-    assert not isinstance(active_ex, tuple)
+        active_ex_scattered = torch.zeros_like(active_ex)
+        active_ex_scattered[active_b, :, active_h, active_w] = 1
 
-    active_ex_scattered = torch.zeros_like(active_ex)
-    active_ex_scattered[active_b, :, active_h, active_w] = 1
-
-    assert torch.equal(active_ex, active_ex_scattered)
+        assert torch.equal(active_ex, active_ex_scattered)
 
 
 def test_sp_conv_forward() -> None:
     H, W = 32, 32
-    sparse_spark._cur_active = torch.tensor(
-        [
-            [
-                [
-                    [1, 0],
-                    [0, 1],
-                ]
-            ]
-        ],
-        dtype=torch.bool,
-    )
+    with sparse_spark.sparse_layer_context(_create_mask()):
+        conv = sparse_spark.SparseConv2d(
+            in_channels=1,
+            out_channels=1,
+            kernel_size=3,
+            padding=1,
+        )
+        conv.weight.data.fill_(1)
+        assert conv.bias is not None
+        conv.bias.data.fill_(0)
 
-    conv = sparse_spark.SparseConv2d(
-        in_channels=1,
-        out_channels=1,
-        kernel_size=3,
-        padding=1,
-    )
-    conv.weight.data.fill_(1)
-    assert conv.bias is not None
-    conv.bias.data.fill_(0)
+        x = torch.ones(1, 1, H, W)
+        out = conv(x)
 
-    x = torch.ones(1, 1, H, W)
-    out = conv(x)
-
-    assert out.shape == (1, 1, H, W)
-    assert out[:, :, :16, :16].all()
-    assert out[:, :, :16, 16:].logical_not().all()
-    assert out[:, :, 16:, :16].logical_not().all()
-    assert out[:, :, 16:, 16:].all()
+        assert out.shape == (1, 1, H, W)
+        assert out[:, :, :16, :16].all()
+        assert out[:, :, :16, 16:].logical_not().all()
+        assert out[:, :, 16:, :16].logical_not().all()
+        assert out[:, :, 16:, 16:].all()
